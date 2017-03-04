e6e166629d7fe0cd124eeae537795eb49c1deac4
Openarl <dayveplusplus@gmail.com>
2017-03-04 11:04:09 +1000

    Sneaking in some new uniques

--- Data/New.lua
+++ Data/New.lua
@@ -9,10 +9,10 @@ local itemBases = data.itemBases
 
 
 data.uniques.new = {
+-- Equippable items
 [[
 Tidebreaker
 Imperial Maul
-Unreleased: true
 Requires Level 65, 212 Str
 20% increased Stun Duration on Enemies
 Socketed Gems are Supported by Level 20 Endurance Charge on Melee Stun
@@ -23,7 +23,6 @@ Adds 63 to 346 Physical Damage
 ]],[[
 Martyr of Innocence
 Highborn Staff
-Unreleased: true
 Requires Level 52, 89 Str, 89 Int
 18% Chance to Block
 15% Chance to Block
@@ -36,7 +35,6 @@ Grants level 15 Vengeance Skill
 ]],[[
 Garukhan's Flight
 Stealth Boots
-Unreleased: true
 Requires Level 62, 117 Dex
 115% increased Evasion Rating
 30% increased Movement Speed
@@ -46,7 +44,6 @@ Regenerate 100 Life per second while moving
 ]],[[
 Gruthkul's Pelt
 Wyrmscale Doublet
-Unreleased: true
 Requiress Level 38, 57 Str, 57 Dex
 69% increased Physical Damage
 +131 to maximum Life
@@ -55,5 +52,127 @@ Requiress Level 38, 57 Str, 57 Dex
 15% increased Character Size
 Spell Skills deal no Damage
 Your Spells are disabled
+]],[[
+The Baron
+Close Helmet
+Requires Level 26, 58 Str
++2 to Level of Socketed Minion Gems
++28 to Strength
+Minions have 20% increase maximum Life
+Your Strenght is added to your Minions
++1 to maximum number of Zombies per 300 Strength
+With 1000 or more Strength 2% of Damage dealt by your Zombies is Leeched to you as Life
+]],[[
+Bisco's Collar
+Gold Amulet
+Requires Level 30
+(12-20)% increased Rarity of Items found
+150% increased Rarity of Items Dropped by Slain Magic Enemies
+100% increased Quantity of Items Dropped by Slain Normal Enemies
+]],[[
+Bisco's Leash
+Heavy Belt
+Requires Level 30
++(25-35) to Strength
+5% increased Quantity of Items found
++34% to Cold Resistance
+Rampage
+1% increased Rarity of Items found per 15 Rampage Kills
+]],[[
+The Wise Oak
+Bismuth Flask
+Requires Level 8
+Implicits: 0
+During Flask Effect, 10% reduced Damage taken of each Element for which your Uncapped
+Elemental Resistance is lowest
+During Flask Effect, Damage Penetrates 20% of Resistance of each Element for which your
+Uncapped Elemental Resistance is highest
+]],[[
+Ahn's Heritage
+Colossal Tower Shield
+Requires Level 67, 159 Str
+69% increased Armour
++69 to maximum Life
+-1 to maximum Endurance Charges
+-10% to maximum Block Chance
++6% Chance to Block
++3% to all maximum Resistances while you have no Endurance Charges
+You have Onslaught while at maximum Endurance Charges
+]],[[
+Haemophilia
+Serpentscale Gauntlets
+Requires Level 43, 34 Str, 34 Dex
+25% increased Damage over Time
++27 to Strength
+Attacks have 25% chance to cause Bleeding
+31% increased Attack Damage against Bleeding Enemies
+Bleeding Enemies you Kill Explode, dealing 5% of
+their Maximum Life as Physical Damage
+25% reduced Bleed Duration
+]],
+-- Theshold jewels
+[[
+Fight for Survival
+Viridian Jewel
+Limited to: 2
+Radius: Medium
+11% increased Cold Damage
+With at least 40 Dexterity in Radius, Frost
+Blades Melee Damage Penetrates 15% Cold Resistance
+With at least 40 Dexterity in Radius, Frost Blades has 25% increased Projectile Speed
+]],[[
+Omen on the Winds
+Viridian Jewel
+Limited to: 2
+Radius: Medium
+16% increased Damage against Chilled Enemies
+With at least 40 Dexterity in Radius, Ice Shot has 25% increased Area of Effect
+With at least 40 Dexterity in Radius, Ice Shot has 50% chance of Projectiles Piercing
+]],[[
+Frozen Trail
+Cobalt Jewel
+Limited to: 2
+Radius: Medium
+9% increased Projectile Damage
+With at least 40 Intelligence in Radius, Frostbolt fires 2 additional Projectiles
+With at least 40 Intelligence in Radius, Frostbolt Projectiles gain 40% increased Projectile
+Speed per second
+]],[[
+Inevitability
+Cobalt Jewel
+Limited to: 2
+Radius: Medium
+13% increased Fire Damage
+With at least 40 Intelligence in Radius, Magma Orb fires an additional Projectile
+With at least 40 Intelligence in Radius, Magma Orb
+has 10% increased Area of Effect per Chain
+]],[[
+Overwhelming Odds
+Crimson Jewel
+Limited to: 2
+Radius: Medium
+11% increased Physical Damage
+With at least 40 Strength in Radius, Cleave grants Fortify on Hit
+With at least 40 Strength in Radius, Cleave has 3% increased Area of
+Effect per Nearby Enemy
+]],[[
+Collateral Damage
+Viridian Jewel
+Limited to: 2
+Radius: Medium
+15% increased Physical Damage
+With at least 40 Dexterity in Radius, Shrapnel Shot has 25% increased Area of Effect
+With at least 40 Dexterity in Radius, Shrapnel Shot's
+cone has a 50% chance to deal Double Damage
+]],[[
+Might and Influence
+Viridian Jewel
+Limited to: 1
+Radius: Medium
+12% increased Physical Damage
+With at least 40 Dexterity in Radius, Dual Strike has a 20% chance
+to deal Double Damage with the Main-Hand Weapon
+With at least 40 Dexterity in Radius, Dual Strike deals Off-Hand Splash Damage
+to surrounding targets
 ]],
 }
