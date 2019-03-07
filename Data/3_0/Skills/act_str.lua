7be751a466eac8bcd709efe053aeec761f06f468
zmike <michael.blumenkrantz@gmail.com>
2019-03-06 21:32:45 -0500

    skills: add radius to vaal earthquake

--- Data/3_0/Skills/act_str.lua
+++ Data/3_0/Skills/act_str.lua
@@ -1243,6 +1243,8 @@ skills["VaalEarthquake"] = {
 	},
 	baseMods = {
 		skill("showAverage", true, { type = "SkillPart", skillPart = 2 }),
+		skill("radius", 18, { type = "SkillPart", skillPart = 1 }),
+		skill("radius", 28, { type = "SkillPart", skillPart = 2 }),
 	},
 	qualityStats = {
 		{ "physical_damage_+%", 1 },
