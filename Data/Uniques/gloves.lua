a93ef6e455b7c236d18e66ea0487a62e3e6e1193
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2019-11-24 21:51:21 +0100

    Updated Blasphemer's Grasp to 3.8.0
    Blasphemer's Grasp has no new variant.

--- Data/Uniques/gloves.lua
+++ Data/Uniques/gloves.lua
@@ -116,8 +116,8 @@ Requires Level 58
 +(40-50) to Dexterity
 (150-200)% increased Evasion and Energy Shield
 +(50-60) to maximum Life
-15% increased Damage with Ailments per Elder Item Equipped
 +6 to Maximum Life per Elder Item Equipped
++4% to Damage over Time Multiplier for Ailments per Elder Item Equipped
 8% increased Effect of non-Damaging Ailments per Elder Item Equipped
 Remove an Ailment when you use a Flask if all Equipped Items are Elder Items
 Elder Item
