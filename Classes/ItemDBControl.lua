79a23731c4a7ffa9f3586e47532e805f5c81bee1
Sascha Scherrer <ddragbone@gmail.com>
2019-12-09 18:43:07 +0100

    Improved item sorting

--- Classes/ItemDBControl.lua
+++ Classes/ItemDBControl.lua
@@ -182,19 +182,16 @@ function ItemDBClass:ListBuilder()
 	if self.sortDetail and self.sortDetail.stat then -- stat-based
 		local start = GetTime()
 		local calcFunc, calcBase = self.itemsTab.build.calcsTab:GetMiscCalculator(self.build)
-		local originalValue = calcBase[self.sortMode]
 		for itemIndex, item in ipairs(list) do
 			item.measuredPower = 0
 			for slotName, slot in pairs(self.itemsTab.slots) do
 				if self.itemsTab:IsItemValidForSlot(item, slotName) and not slot.inactive and (not slot.weaponSet or slot.weaponSet == (self.itemsTab.activeItemSet.useSecondWeaponSet and 2 or 1)) then
 					local output = calcFunc(item.base.flask and { toggleFlask = item } or { repSlotName = slotName, repItem = item })
-					if calcBase.Minion and output.Minion then
-						output = output.Minion
-					end
-					item.measuredPower = output[self.sortMode] or 0
+					local measuredPower = output.Minion and output.Minion[self.sortMode] or output[self.sortMode] or 0
 					if self.sortDetail.transform then
-						item.measuredPower = self.sortDetail.transform(item.measuredPower)
-					end			
+						measuredPower = self.sortDetail.transform(measuredPower)
+					end
+					item.measuredPower = m_max(item.measuredPower, measuredPower)
 				end
 			end
 			local now = GetTime()
