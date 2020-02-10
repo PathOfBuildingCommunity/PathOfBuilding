a704c887b22888945f89cdefaa20b1c4d049f1a4
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2020-02-10 16:32:50 +0100

    Converted tabs to spaces

--- updatemanifest.py
+++ updatemanifest.py
@@ -4,26 +4,26 @@ import xml.etree.ElementTree
 
 
 def update_manifest():
-	manifest = xml.etree.ElementTree.parse("manifest.xml")
-	root = manifest.getroot()
+    manifest = xml.etree.ElementTree.parse("manifest.xml")
+    root = manifest.getroot()
 
-	for file in root.iter("File"):
-		path = file.get('name')
-		extension = os.path.splitext(path)[1]
-		if extension not in [".lua", ".txt"]:
-			print(f"Skipping file type {extension}")
-			continue
-		try:
-			with open(path, 'rb') as f:
-				data = f.read()
-			sha1_hash = hashlib.sha1(data).hexdigest()
-			file.set("sha1", sha1_hash)
-			print(f"Path: {path} hash: {sha1_hash}")
-		except FileNotFoundError:
-			print(f"File not found, skipping {path}")
+    for file in root.iter("File"):
+        path = file.get('name')
+        extension = os.path.splitext(path)[1]
+        if extension not in [".lua", ".txt"]:
+            print(f"Skipping file type {extension}")
+            continue
+        try:
+            with open(path, 'rb') as f:
+                data = f.read()
+            sha1_hash = hashlib.sha1(data).hexdigest()
+            file.set("sha1", sha1_hash)
+            print(f"Path: {path} hash: {sha1_hash}")
+        except FileNotFoundError:
+            print(f"File not found, skipping {path}")
 
-	manifest.write("manifest-updated.xml")
+    manifest.write("manifest-updated.xml")
 
 
 if __name__ == "__main__":
-	update_manifest()
+    update_manifest()
