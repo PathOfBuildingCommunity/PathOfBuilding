c41e3b808b7b9ae0ec30cf8b090bf7aa1eb7113e
Nostrademous <nostrademous@hotmail.com>
2020-03-06 08:22:22 -0500

    adds Profane Proxy Unique

--- Data/New.lua
+++ Data/New.lua
@@ -25,7 +25,19 @@ Banners you are carrying gain 1 Stage on Melee Hit, up to 5 per second
 War Banner has 147% increased Adrenaline duration
 Dread Banner has 141% increased Fortify duration
 ]],
-
+[[
+Profane Proxy
+Unset Ring
+Sockets: G
+LevelReq: 52
+Implicits: 1
+Has 1 Socket
++3 to Level of Socketed Curse Gems
++28% To Cold Resistance
++20% To Lightning Resistance
+Left Ring Slot: Your Chilling Skitterbot's Aura Applies Socketed Curse Instead
+Right Ring Slot: Your Shocking Skitterbot's Aura Applies Socketed Curse Instead
+]]
 
 -- Reworked
 
