31c24cccd9be5accda80b061675fb7aa35b98c9a
Nostrademous <nostrademous@hotmail.com>
2020-03-14 11:56:21 -0400

    fixed leading whitespace

--- Modules/ModParser-3_0.lua
+++ Modules/ModParser-3_0.lua
@@ -1036,7 +1036,7 @@ local modTagList = {
 	["per poison affecting enemy, up to %+([%d%.]+)%%"] = function(num) return { tag = { type = "Multiplier", actor = "enemy", var = "PoisonStack", limit = num, limitTotal = true } } end,
 	["for each spider's web on the enemy"] = { tag = { type = "Multiplier", actor = "enemy", var = "Spider's WebStack" } },
 	-- Skill conditions
-	[" of banner skills"] = { tag = { type = "SkillName", skillNameList = { "Dread Banner", "War Banner" } } },
+	["of banner skills"] = { tag = { type = "SkillName", skillNameList = { "Dread Banner", "War Banner" } } },
 }
 
 local mod = modLib.createMod
