43ddff822475f2e98bc088839fd0b68c81a8b187
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2020-03-15 00:06:51 +0100

    Finalise rolls on Profane Proxy

--- Data/New.lua
+++ Data/New.lua
@@ -4,19 +4,6 @@
 
 data.uniques.new = {
 -- New
-[[
-Profane Proxy
-Unset Ring
-Sockets: G
-LevelReq: 52
-Implicits: 1
-Has 1 Socket
-+3 to Level of Socketed Curse Gems
-+28% To Cold Resistance
-+20% To Lightning Resistance
-Left Ring Slot: Your Chilling Skitterbot's Aura Applies Socketed Curse Instead
-Right Ring Slot: Your Shocking Skitterbot's Aura Applies Socketed Curse Instead
-]],
 
 -- Reworked
 
