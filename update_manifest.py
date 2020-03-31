9db2991bb7247125bffd46013595f715544f3561
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2020-03-31 23:47:32 +0200

    Re-add supported file extensions to manifest updater

--- update_manifest.py
+++ update_manifest.py
@@ -10,7 +10,7 @@ def update_manifest():
     for file in root.iter("File"):
         path = file.get('name')
         extension = os.path.splitext(path)[1]
-        if extension not in [".lua", ".txt"]:
+        if extension not in (".lua", ".txt", ".png", ".jpg"):
             print(f"Skipping file type {extension}")
             continue
         try:
