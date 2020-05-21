8757c11eeae8ee56e41ac414f9594cea0a6eb9e9
Carlos Vinicius <contato-carloscastro@hotmail.com>
2020-05-21 12:04:01 -0300

    Fix: appearance of other tables in calcs tab

--- Classes/CalcSectionControl.lua
+++ Classes/CalcSectionControl.lua
@@ -171,7 +171,7 @@ function CalcSectionClass:FormatStr(str, actor, colData)
 			return actor.output[c] or ""
 		end
 	end)
-	str = str:gsub("{(%d+):output:([%a%.:]+)}", function(p, c)
+	str = str:gsub("{s(%d+):output:([%a%.:]+)}", function(p, c)
 		local ns, var = c:match("^(%a+)%.(%a+)$")
 		if ns then
 			local s = self:FormatVal(actor.output[ns] and actor.output[ns][var] or 0, tonumber(p))
@@ -187,6 +187,14 @@ function CalcSectionClass:FormatStr(str, actor, colData)
 			else
 				return "+"..s
 			end
+		end
+	end)
+	str = str:gsub("{(%d+):output:([%a%.:]+)}", function(p, c)
+		local ns, var = c:match("^(%a+)%.(%a+)$")
+		if ns then
+			return self:FormatVal(actor.output[ns] and actor.output[ns][var] or 0, tonumber(p))
+		else
+			return self:FormatVal(actor.output[c] or 0, tonumber(p))
 		end
 	end)
 	str = str:gsub("{(%d+):mod:([%d,]+)}", function(p, n)
