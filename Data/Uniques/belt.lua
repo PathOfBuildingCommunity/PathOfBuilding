c8dfeb88d3e8944cf36e2eda4cdca5a20a244323
Trevor Lund <Wires77@users.noreply.github.com>
2020-06-30 15:09:17 -0500

    Adding catalyst tags

--- Data/Uniques/belt.lua
+++ Data/Uniques/belt.lua
@@ -62,8 +62,8 @@ Implicits: 1
 Bear's Girdle
 Leather Belt
 Requires Level 68
-+(25-40) to maximum Life
-Adds (5-7) to (11-12) Physical Damage to Attacks
+{tags:life}+(25-40) to maximum Life
+{tags:attack}Adds (5-7) to (11-12) Physical Damage to Attacks
 (20-30)% increased Stun Duration on Enemies
 Nearby Enemies are Crushed while you have least 25 Rage
 (4-6)% increased Physical Damage per 10 Rage
