6e94f2350dccef90a0c45c6e8317fc266bb2a37f
Regisle <Regisle.godform@gmail.com>
2020-07-14 10:47:37 +0930

    fixed vaal molten shell config

--- Modules/ConfigOptions.lua
+++ Modules/ConfigOptions.lua
@@ -242,7 +242,7 @@ return {
 	end },
 	{ label = "Vaal Molten Shell:", ifSkill = "Vaal Molten Shell" },
 	{ var = "VaalMoltenShellDamageMitigated", type = "count", label = "Damage Mitigated:", tooltip = "The amount of damage mitigated by vaal molten shell in the last second", ifSkill = "Vaal Molten Shell", apply = function(val, modList, enemyModList)
-		modList:NewMod("SkillData", "LIST", { key = "VaalMoltenShellDamageMitigated", value = val }, "Config", { type = "SkillName", skillName = "Vaal Molten Shell" })
+		modList:NewMod("SkillData", "LIST", { key = "VaalMoltenShellDamageMitigated", value = val }, "Config", { type = "SkillName", skillName = "Molten Shell" })
 	end },
 
 	-- Section: Map modifiers/curses
