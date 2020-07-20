664e64451dffb5aa522fc46624e2925d06391299
Nostrademous <nostrademous@hotmail.com>
2020-07-20 15:39:54 -0400

    added crit chance calculation

--- Modules/CombatSimulator.lua
+++ Modules/CombatSimulator.lua
@@ -25,21 +25,55 @@ end
 local function getPlayerData()
     cs.player = { }
     if global_env then
-        cs.player.aps, cs.player.minDmg, cs.player.maxDmg, cs.player.critChance = getPlayerWeaponInfo(global_env)
+        cs.player.aps, cs.player.minDmg, cs.player.maxDmg, cs.player.baseCriticalStrikeChance = getPlayerWeaponInfo(global_env)
         ConPrintf("PhysDmg Min: " .. cs.player.minDmg)
         ConPrintf("PhysDmg Max: " .. cs.player.maxDmg)
         ConPrintf("APS: " .. cs.player.aps)
-        ConPrintf("Crit Chance: " .. cs.player.critChance)
+        ConPrintf("Crit Chance: " .. cs.player.baseCriticalStrikeChance)
         cs.player.attackInterval = 1/cs.player.aps
     end
 end
 
+local function getCriticalStrikeChance()
+    local base = cs.player.baseCriticalStrikeChance
+    local moreCriticalStrikeChance = 0
+    local incCriticalStrikeChance = 0
+    local chance = (base + moreCriticalStrikeChance) * (1 + incCriticalStrikeChance)
+    return chance
+end
+
+local function getCriticalStrikeMultiplier()
+    local base = 1.5
+    local additionalCriticalStrikeMultiplier = 0
+    local multiplier = base + additionalCriticalStrikeMultiplier
+    return multiplier
+end
+
+local function isCrit()
+    local critChance = getCriticalStrikeChance() * 100
+    local randValue = math.random(1, 10000)
+    if randValue <= critChance then
+        cs.simData.numCrits = cs.simData.numCrits + 1
+        return true
+    end
+    return false
+end
+
 local function getDmg()
-    local flat_dmg = math.random(cs.player.minDmg, cs.player.maxDmg)
-    return flat_dmg
+    cs.simData.numAttacks = cs.simData.numAttacks + 1
+    local dmg = math.random(cs.player.minDmg, cs.player.maxDmg)
+    if isCrit() then
+        dmg = dmg * getCriticalStrikeMultiplier()
+    end
+    return dmg
 end
 
 local function runSingleSim(numSec, player)
+    cs.simData = {
+        numAttacks = 0,
+        numCrits = 0,
+        numMisses = 0,
+    }
     local t = 0.0
     local t_tenth = 0.1
     local t_next_attack = cs.player.attackInterval
@@ -73,12 +107,18 @@ function cs.runSimulation()
     getPlayerData()
 
     local avg_sim_dmg = 0
+    local avg_sim_attacks = 0
+    local avg_sim_crits = 0
     for i = 1, numSims do
         local ret = runSingleSim(numSecsPerSim)
-        --ConPrintf("DPS [" .. i .. "]: " .. ret)
+        --ConPrintf("Num Attacks: " .. cs.simData.numAttacks .. ", Num Crits: " .. cs.simData.numCrits)
         avg_sim_dmg = avg_sim_dmg + ret
+        avg_sim_attacks = avg_sim_attacks + cs.simData.numAttacks
+        avg_sim_crits = avg_sim_crits + cs.simData.numCrits
     end
-    ConPrintf("Avg DPS: " .. avg_sim_dmg/numSims)
+    ConPrintf("Avg DPS: " .. avg_sim_dmg / numSims)
+    ConPrintf("Avg Num of Attacks: " .. avg_sim_attacks / numSims)
+    ConPrintf("Avg Num of Crits: " .. avg_sim_crits / numSims .. " --> (" .. avg_sim_crits * 100 / avg_sim_attacks .. "%%)")
 end
 
 return cs
