043bc97bcb11e4cc69e73d386f308cd7e0052c5e
Regisle <Regisle.godform@gmail.com>
2020-08-19 20:51:34 +0930

    fixed net es regen and added total net regen

--- Modules/CalcSections-3_0.lua
+++ Modules/CalcSections-3_0.lua
@@ -1542,9 +1542,12 @@ return {
 		{ breakdown = "TotalDegen" },
 		{ label = "Sources", modName = { "PhysicalDegen", "FireDegen", "ColdDegen", "LightningDegen", "ChaosDegen" }, modType = "BASE" },
 	}, },
+	{ label = "Total Net Regen", haveOutput = "TotalNetRegen", { format = "{1:output:TotalNetRegen}", 
+		{ breakdown = "TotalNetRegen" },
+	}, },
 	{ label = "Net Life Regen", haveOutput = "NetLifeRegen", { format = "{1:output:NetLifeRegen}", { breakdown = "NetLifeRegen" }, }, },
 	{ label = "Net Mana Regen", haveOutput = "NetManaRegen", { format = "{1:output:NetManaRegen}", { breakdown = "NetManaRegen" }, }, },
-	{ label = "Net Energy Shield Regen", haveOutput = "NetEnergyShieldRegen", { format = "{1:output:NetEnergyShieldRegen}", { breakdown = "NetEnergyShieldRegen" }, }, },
+	{ label = "Net ES. Regen", haveOutput = "NetEnergyShieldRegen", { format = "{1:output:NetEnergyShieldRegen}", { breakdown = "NetEnergyShieldRegen" }, }, },
 } }
 } },
 }
