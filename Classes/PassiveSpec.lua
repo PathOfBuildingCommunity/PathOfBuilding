5c69649df4ba324c5bc9343ed8d3557edafc28df
John Hargrove <john.g.hargrove@gmail.com>
2020-09-13 12:00:02 -0400

    provide name when generating cluster jewel subgraphs

--- Classes/PassiveSpec.lua
+++ Classes/PassiveSpec.lua
@@ -688,6 +688,7 @@ function PassiveSpecClass:BuildSubgraph(jewel, parentSocket, id, upSize)
 		local node = {
 			type = "Keystone",
 			id = nodeId,
+			name = keystoneNode.name,
 			dn = keystoneNode.dn,
 			sd = keystoneNode.sd,
 			icon = keystoneNode.icon,
@@ -850,6 +851,7 @@ function PassiveSpecClass:BuildSubgraph(jewel, parentSocket, id, upSize)
 		local node = {
 			type = "Notable",
 			id = nodeId + nodeIndex,
+			name = baseNode.name,
 			dn = baseNode.dn,
 			sd = baseNode.sd,
 			icon = baseNode.icon,
