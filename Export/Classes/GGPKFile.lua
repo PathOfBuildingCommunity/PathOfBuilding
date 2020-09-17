49044e3835aa7c785d7bfd909b5c6524eb9044ad
Trevor Lund <Wires77@users.noreply.github.com>
2020-09-17 16:46:50 -0500

    Legion files for exporter (#6)
    * Adding files needed for Legion passives
    * Adding spec.lua changes for legion
    * Adding alternateTreeVersions

--- Export/Classes/GGPKFile.lua
+++ Export/Classes/GGPKFile.lua
@@ -247,6 +247,9 @@ function GGPKClass:GetNeededFiles()
 		"Data/AchievementItems.dat",
 		"Data/MultiPartAchievements.dat",
 		"Data/PantheonPanelLayout.dat",
+		"Data/AlternatePassiveAdditions.dat",
+		"Data/AlternatePassiveSkills.dat",
+		"Data/AlternateTreeVersions.dat",
 	}
 	local txtFiles = {
 		"Metadata/StatDescriptions/passive_skill_aura_stat_descriptions.txt",
