5149f0f2f80df0e968c73ea4e503e246fead80fc
Nightblade <Nightblade@users.noreply.github.com>
2020-09-18 12:11:41 +1000

    Add 4 of the new 3.12 uniques
    Includes corrected Corpsewalker from #1345

--- Data/New.lua
+++ Data/New.lua
@@ -1,10 +1,57 @@
-﻿--
+--
 -- Upcoming uniques will live here until their mods/rolls are finalised
 --
 
 data.uniques.new = {
 
 -- New
+[[
+Actum
+Butcher Axe
+League: Heist
+Requires Level 63, 149 Str, 76 Dex
+Implicits: 0
+Has no Sockets
+235% increased Physical Damage
+You have no Intelligence
+Critical Strike Chance is 28% for Hits with this Weapon
+]],[[
+Chains of Emancipation
+Chain Belt
+League: Heist
+Requires Level 61
+Implicits: 1
++(9-20) to maximum Energy Shield
++77 to maximum Life
++20% to Chaos Resistance
+Enemy Hits inflict Temporal Chains on you
+When you lose Temporal Chains you gain maximum Rage
+Immune to Curses while you have at least 25 Rage
+]],[[
+Corpsewalker
+Carnal Boots
+League: Heist
+Requires Level 55, 52 Dex, 52 Int
+Implicits: 0
+Triggers Level 20 Corpse Walk when Equipped
+94% increased Evasion and Energy Shield
+25% increased Movement Speed
+40% increased Damage if you have Consumed a corpse Recently
+For each nearby corpse, Regenerate 0.25% Life per second, up to 3%
+]],[[
+The Iron Mass
+Gladius
+League: Heist
+Requires Level 60, 113 Str, 113 Dex
+Implicits: 1
+40% increased Global Accuracy Rating
+152% increased Physical Damage
+17% increased Attack Speed
+Unholy Might
+21% chance to inflict Withered for 2 seconds on Hit with this Weapon
+Summoned Skeleton Warriors wield a copy of this Weapon while in your Main Hand
+Summoned Skeleton Warriors deal Triple Damage with this Weapon if you've Hit with this Weapon recently
+]],
 
 -- Reworked
 
