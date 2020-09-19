d0e06deb5e72feb0f97a03976d9b5b3dc19ccbaf
Nostrademous <nostrademous@hotmail.com>
2020-09-19 00:01:51 -0400

    fix skill alternate quality values

--- Export/Scripts/skills.lua
+++ Export/Scripts/skills.lua
@@ -301,11 +301,14 @@ directiveTable.skill = function(state, args, out)
 	end
 	if not skill.qualityStats then
 		skill.qualityStats = { }
+		local divisor = nil
 		for i, qualityStatsRow in ipairs(dat("GrantedEffectQualityStats"):GetRowList("GrantedEffect", granted)) do
-			ConPrintf('i: %d', i)
 			skill.qualityStats[i] = { }
+			if not divisor then
+				divisor = qualityStatsRow.Divisor
+			end
 			for j, stat in ipairs(qualityStatsRow.GrantedStats) do
-				table.insert(skill.qualityStats[i], { stat.Id, qualityStatsRow.StatValues[j] / qualityStatsRow.Divisor })
+				table.insert(skill.qualityStats[i], { stat.Id, qualityStatsRow.StatValues[j] / divisor })
 				--ConPrintf("[%d] %s %s", i, granted.ActiveSkill.DisplayName, stat.Id)
 			end
 		end
