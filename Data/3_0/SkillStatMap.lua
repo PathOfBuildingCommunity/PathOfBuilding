25b9887cfdff6f53cb7b6eb651b1572e1322d08e
LocalIdentity <localidentity2@gmail.com>
2020-10-01 03:36:48 +1000

    Fix Divergent Blind Crit Chance

--- Data/3_0/SkillStatMap.lua
+++ Data/3_0/SkillStatMap.lua
@@ -439,7 +439,7 @@ return {
 	mod("CritChance", "BASE", nil, 0, 0, { type = "ActorCondition", actor = "enemy", var = "FullLife" })
 },
 ["critical_strike_chance_+%_vs_blinded_enemies"] = {
-	mod("CritChance", "BASE", nil, 0, 0, { type = "ActorCondition", actor = "enemy", var = "Blinded"})
+	mod("CritChance", "INC", nil, 0, 0, { type = "ActorCondition", actor = "enemy", var = "Blinded"})
 },
 -- Duration
 ["buff_effect_duration_+%_per_removable_endurance_charge"] = {
