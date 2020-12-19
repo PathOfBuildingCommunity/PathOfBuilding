812bef107a8923eee78fc4ed399a3adee9a676f4
ppoelzl <33464174+ppoelzl@users.noreply.github.com>
2020-12-19 03:04:22 +0100

    Fix formatting

--- Classes/ItemsTab.lua
+++ Classes/ItemsTab.lua
@@ -2279,9 +2279,9 @@ function ItemsTabClass:AddItemTooltip(tooltip, item, slot, dbMode)
 		end
 		if flaskData.lifeTotal then
 			if flaskData.lifeGradual ~= 0 then
-				tooltip:AddLine(16, s_format("^x7F7F7FRecovers %s%d ^x7F7F7FLife over %s%.1f0 ^x7F7F7FSeconds", 
+				tooltip:AddLine(16, s_format("^x7F7F7FRecovers %s%d ^x7F7F7FLife over %s%.1f0 ^x7F7F7FSeconds",
 					main:StatColor(flaskData.lifeTotal, base.flask.life), flaskData.lifeGradual,
-					main:StatColor(flaskData.duration, base.flask.duration), flaskData.duration,
+					main:StatColor(flaskData.duration, base.flask.duration), flaskData.duration
 					))
 			end
 			if flaskData.lifeInstant ~= 0 then
@@ -2290,9 +2290,9 @@ function ItemsTabClass:AddItemTooltip(tooltip, item, slot, dbMode)
 		end
 		if flaskData.manaTotal then
 			if flaskData.manaGradual then
-				tooltip:AddLine(16, s_format("^x7F7F7FRecovers %s%d ^x7F7F7FMana over %s%.1f0 ^x7F7F7FSeconds", 
+				tooltip:AddLine(16, s_format("^x7F7F7FRecovers %s%d ^x7F7F7FMana over %s%.1f0 ^x7F7F7FSeconds",
 					main:StatColor(flaskData.manaTotal, base.flask.mana), flaskData.manaGradual,
-					main:StatColor(flaskData.duration, base.flask.duration), flaskData.duration,
+					main:StatColor(flaskData.duration, base.flask.duration), flaskData.duration
 					))
 			end
 			if flaskData.manaInstant then
@@ -2451,7 +2451,7 @@ function ItemsTabClass:AddItemTooltip(tooltip, item, slot, dbMode)
 					elseif grad > 0 then
 						t_insert(stats, s_format("^8Life recovered: ^7%d ^8over ^7%.2fs", grad, lifeDur))
 					end
-				end				
+				end
 				if modDB:Flag(nil, "LifeFlaskAppliesToEnergyShield") then
 					if inst > 0 and grad > 0 then
 						t_insert(stats, s_format("^8Energy Shield recovered: ^7%d ^8(^7%d^8 instantly, plus ^7%d ^8over^7 %.2fs^8)", inst + grad, inst, grad, lifeDur))
