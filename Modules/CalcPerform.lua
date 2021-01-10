58ee72d43ba0bf3e3e08e34d55a948c6e82e11df
mu <max+github@ullinger.ch>
2021-01-10 07:53:57 +0100

    Only activate mod if the player has an active spectre skill

--- Modules/CalcPerform.lua
+++ Modules/CalcPerform.lua
@@ -426,8 +426,17 @@ local function doActorMisc(env, actor)
 			modDB.multipliers["BuffOnSelf"] = (modDB.multipliers["BuffOnSelf"] or 0) + 1
 		end
 		if modDB:Sum("BASE", nil, "DamageFromHitsTakenFromSpectres") > 0 then
-			local effect = modDB:Sum("BASE", nil, "DamageFromHitsTakenFromSpectres")
-			modDB:NewMod("DamageTakenWhenHit", "MORE", -effect, "Damage From Hits Taken From Spectres")
+			local isActiveSpecter = false
+			for _, activeSkill in ipairs(env.player.activeSkillList) do
+				if activeSkill.skillFlags.spectre then
+					isActiveSpecter = true
+				end
+			end
+
+			if isActiveSpecter then
+				local effect = modDB:Sum("BASE", nil, "DamageFromHitsTakenFromSpectres")
+				modDB:NewMod("DamageTakenWhenHit", "MORE", -effect, "Damage From Hits Taken From Spectres")
+			end
 		end
 		if modDB:Flag(nil, "Onslaught") then
 			local effect = m_floor(20 * (1 + modDB:Sum("INC", nil, "OnslaughtEffect", "BuffEffectOnSelf") / 100))
