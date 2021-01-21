85b73ce84984e5207cb613383e9e6e1897af34d5
Trevor Lund <Wires77@users.noreply.github.com>
2021-01-21 11:19:11 -0600

    Adding example of XML loading test

--- Test/Scenario/TestBuilds.lua
+++
@@ -1,9 +0,0 @@
-TestBuilds = {}
-
-function TestBuilds:testBuilds()
-    local testBuild = LoadModule("Test/TestBuilds/3.13/OccVortex")
-    loadBuildFromXML(testBuild.xml)
-    for key, value in pairs(testBuild.output) do
-        lu.assertEquals(build.calcsTab.mainOutput[key], value)
-    end
-end
