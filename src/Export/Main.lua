b2efb19624ddff16889c5531d54b764b4e87002c
Wires77 <Wires77@users.noreply.github.com>
2021-04-16 12:39:14 -0500

    Removing UIntOffset

--- src/Export/Main.lua
+++ src/Export/Main.lua
@@ -141,7 +141,7 @@ function main:Init()
 		return self.ggpk.txt[name]
 	end
 
-	self.typeDrop = { "Bool", "Int", "UInt", "Interval", "Float", "String", "UIntOffset", "Enum", "Key" }
+	self.typeDrop = { "Bool", "Int", "UInt", "Interval", "Float", "String", "Enum", "Key" }
 
 	self.colList = { }
 
