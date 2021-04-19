05b0d735477f0940ee846817b5a66cf0974eba00
Nostrademous <nostrademous@hotmail.com>
2021-04-19 01:21:44 -0400

    added radius to Blood Sacrament

--- src/Data/Skills/other.lua
+++ src/Data/Skills/other.lua
@@ -3294,12 +3294,6 @@ skills["BloodSacramentUnique"] = {
 		activeSkill.skillModList:NewMod("Multiplier:ChannelledLifeReservedPerStage", "BASE", lifeReserved, "Blood Sacrament")
 	end,
 	statMap = {
-		["spell_minimum_base_physical_damage"] = {
-			skill("PhysicalMin", nil),
-		},
-		["spell_maximum_base_physical_damage"] = {
-			skill("PhysicalMax", nil),
-		},
 		["flameblast_hundred_times_radius_+_per_1%_life_reserved"] = {
 			skill("radiusExtra", nil, { type = "Multiplier", var = "ChannelledLifeReservedPercentPerStage", div = 100 }, { type = "Multiplier", var = "BloodSacramentStage" }),
 		},
@@ -3312,7 +3306,8 @@ skills["BloodSacramentUnique"] = {
 		area = true,
 	},
 	baseMods = {
-		mod("Multiplier:BloodSacramentMaxStages", "BASE", 33)
+		skill("radius", 5),
+		mod("Multiplier:BloodSacramentMaxStages", "BASE", 33),
 	},
 	qualityStats = {
 	},
