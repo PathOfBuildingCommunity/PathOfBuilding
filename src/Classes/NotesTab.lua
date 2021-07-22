eda669337563f65cfe19eaa3a9e7d73b12288064
Wires77 <Wires77@users.noreply.github.com>
2021-07-22 10:37:00 -0500

    Fixes #2946 - Add documentation for color codes to Notes tab

--- src/Classes/NotesTab.lua
+++ src/Classes/NotesTab.lua
@@ -13,12 +13,17 @@ local NotesTabClass = newClass("NotesTab", "ControlHost", "Control", function(se
 
 	self.lastContent = ""
 
-	self.controls.edit = new("EditControl", {"TOPLEFT",self,"TOPLEFT"}, 8, 8, 0, 0, "", nil, "^%C\t\n", nil, nil, 16)
+	local colorDesc = [[^7This field supports different colors.  Using the caret symbol (^) followed by a Hex code or a number will set the color.  Below are some common color codes PoB uses
+^7xC8C8C8: ^xC8C8C8Normal    ^7x8888FF: ^x8888FFMagic    ^7xFFFF77: ^xFFFF77Rare    ^7xAF6025: ^xAF6025Unique
+^7xB97123: ^xB97123Fire     ^7x3F6DB3: ^x3F6DB3Cold    ^7xADAA47: ^xADAA47Lightning     ^7xD02090: ^xD02090Chaos
+	]]
+	self.controls.colorDoc = new("LabelControl", {"TOPLEFT",self,"TOPLEFT"}, 8, 8, 200, 16, colorDesc)
+	self.controls.edit = new("EditControl", {"TOPLEFT",self.controls.colorDoc,"TOPLEFT"}, 0, 48, 0, 0, "", nil, "^%C\t\n", nil, nil, 16)
 	self.controls.edit.width = function()
 		return self.width - 16
 	end
 	self.controls.edit.height = function()
-		return self.height - 16
+		return self.height - 64
 	end
 	self:SelectControl(self.controls.edit)
 end)
