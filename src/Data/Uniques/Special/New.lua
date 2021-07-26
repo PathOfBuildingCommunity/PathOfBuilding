e8b3c1617e3506c0bbee0537c4527cbe4e871d3f
Wires77 <Wires77@users.noreply.github.com>
2021-07-26 02:12:28 -0500

    Added new uniques

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -10,10 +10,10 @@ Faithguard
 Runic Helm
 League: Expedition
 Requires Level 25, 30 Str, 30 Dex, 30 Int
-+30 to Intelligence
-25% increased Ward
-26% faster Restoration of Ward
-23% increased light radius
++(20-30) to Intelligence
+(25-35)% increased Ward
+(20-30)% faster Restoration of Ward
+(15-25)% increased light radius
 Increases and Reductions to maximum Energy Shield instead apply to Ward
 ]],[[
 Cadigan's Crown
@@ -21,7 +21,82 @@ Runic Crown
 League: Expedition
 Requires Level 68, 66 Str, 66 Dex, 66 Int
 Never deal Critical Strikes
+Nearby Enemies cannot deal Critical Strikes
 Battlemage
+]],[[
+Elixir of the Unbroken Circle
+Iron Flask
+League: Expedition
+Requires Level 40
+(20–40)% increased Duration
+0% reduced effect
+Recover 4% of Life per Endurance Charge on use
+Lose all Endurance Charges on use
+Gain 1 Endurance Charge per Second during Flask Effect
+]],[[
+Medved's Challenge
+Runic Gauntlets
+Requires Level 69, 38 Str, 38 Dex, 38 Int
+League: Expedition
+800% increased Attribute Requirements
+(30–50)% increased Ward
++(15–25)% to all Elemental Resistances
+80% less Flask Charges gained from Kills
+Flasks gain 1 Charge per second if you've Hit a Unique Enemy Recently
+]],[[
+Nightgrip
+Runic Gages
+League: Expedition
+Requires Level 48, 31 Str, 31 Dex, 31 Int
+(33–48)% increased Ward
++(17–23)% to Chaos Resistance
+Gain Added Chaos Damage equal to 25% of Ward
+75% of Damage bypasses Ward
+]],[[
+Olroth's Charge
+Runic Sollerets
+League: Expedition
+Requires Level 48, 37 Str, 37 Dex, 37 Int
+(50–80)% increased Ward
+(30–50)% slower Restoration of Ward
+20% increased Movement Speed
+Gain Adrenaline for 3 seconds when Ward Breaks
+]],[[
+Olroth's Resolve
+Iron Flask
+League: Expedition
+Requires Level 40
+(40–50)% increased Charges used
+Ward does not Break during Flask effect
+70% less Ward during Flask Effect
+]],[[
+Starlight Chalice
+Iron Flask
+League: Expedition
+Requires Level 40
+(20–30)% increased Charge Recovery
+Inflict Fire, Cold and Lightning Exposure on nearby Enemies when used
+(20–30)% increased Effect of Non-Damaging Ailments you inflict during Flask Effect
+]],[[
+Vorana's March
+Runic Sabatons
+League: Expedition
+Requires Level 69, 46 Str, 46 Dex, 46 Int
+Has no Sockets
+Triggers Level 20 Summon Arbalists when Equipped
+25% increased Movement Speed
+Summoned Arbalists have (10–20)% chance to inflict Cold Exposure on Hit
+Summoned Arbalists have (10–20)% chance to Intimidate for 4 seconds on Hit
+Summoned Arbalists Convert (30–50)% of Physical Damage to Fire Damage
+]],[[
+Vorana's Preparation
+Iron Flask
+League: Expedition
+Requires Level 40
+(-10–10)% reduced Charges used
+Debilitate nearby Enemies for 2 Seconds when Flask Effect ends
+Flask Effect is removed when Ward Breaks
+Culling Strike during Flask Effect
 ]],
 -- Reworked
 
