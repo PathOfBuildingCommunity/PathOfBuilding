6aac30a986050bbd472d0a37390712049e641a65
Lexy <learn2draw@live.nl>
2021-08-05 16:59:43 +0200

    Modify Elusive effect (#3061)
    * Add config option to overwrite elusive effect value
    
    * dont actually delete configs :-1:
    
    * clean up tooltip
    
    * Add cap to elusive effect
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Modules/CalcPerform.lua
+++ src/Modules/CalcPerform.lua
@@ -807,6 +807,10 @@ local function doActorMisc(env, actor)
 		end
 		if modDB:Flag(nil, "Elusive") then
 			local effect = 1 + modDB:Sum("INC", nil, "ElusiveEffect", "BuffEffectOnSelf") / 100
+			-- Override elusive effect if set.			
+			if modDB:Override(nil, "ElusiveEffect") then 
+				effect = m_min(modDB:Override(nil, "ElusiveEffect") / 100, effect)
+			end
 			condList["Elusive"] = true
 			modDB:NewMod("AttackDodgeChance", "BASE", m_floor(15 * effect), "Elusive")
 			modDB:NewMod("SpellDodgeChance", "BASE", m_floor(15 * effect), "Elusive")
