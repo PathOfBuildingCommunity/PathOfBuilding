0445d59a59cd849dacd9b3dbd527b6247d2a5885
David Allen <davidrallen1@gmail.com>
2021-09-16 00:03:17 -0700

    Added text to the instructions on the item tab that explains how to clone items with copy paste.

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -237,13 +237,16 @@ local ItemsTabClass = newClass("ItemsTab", "UndoHandler", "ControlHost", "Contro
 [[^7Double-click an item from one of the lists,
 or copy and paste an item from in game
 (hover over the item and Ctrl+C) to view or edit
-the item and add it to your build.
-You can Control + Click an item to equip it,
-or drag it onto the slot.  This will also add it to your
-build if it's from the unique/template list.
-If there's 2 slots an item can go in,
+the item and add it to your build. You can 
+also clone an item within Path of Building by 
+copying and pasting it with Ctl+C and Ctl+V.
+
+You can Control + Click an item to equip it, or 
+drag it onto the slot.  This will also add it to 
+your build if it's from the unique/template list.
+If there's 2 slots an item can go in, 
 holding Shift will put it in the second.]])
-	self.controls.sharedItemList = new("SharedItemListControl", {"TOPLEFT",self.controls.craftDisplayItem, "BOTTOMLEFT"}, 0, 194, 340, 308, self)
+	self.controls.sharedItemList = new("SharedItemListControl", {"TOPLEFT",self.controls.craftDisplayItem, "BOTTOMLEFT"}, 0, 232, 340, 308, self)
 
 	-- Display item
 	self.displayItemTooltip = new("Tooltip")
