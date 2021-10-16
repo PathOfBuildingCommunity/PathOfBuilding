f8ad9d9abd76e6ad7d01a7097c7e6d132269f38b
Mykhailo Boiev <michaelboyev@gmail.com>
2021-10-16 02:14:39 +0200

    Set enemy level now produces default enemy corpse life value if it is not set

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -1165,10 +1165,21 @@ function calcs.offence(env, actor, activeSkill)
 	runSkillFunc("preDamageFunc")
 
 	-- Handle corpse explosions
-	if skillData.explodeCorpse and skillData.corpseLife then
+	if skillData.explodeCorpse and (skillData.corpseLife or env.configInput.enemyLevel) then
+		local normalizedEnemyLevel
+		if env.configInput.enemyLevel then
+			if env.configInput.enemyLevel < 1 then
+				normalizedEnemyLevel = 1
+			elseif  env.configInput.enemyLevel > 100 then
+				normalizedEnemyLevel = 100
+			else
+				normalizedEnemyLevel = env.configInput.enemyLevel
+			end
+		end
+		local localCorpseLife = skillData.corpseLife or data.monsterLifeTable[normalizedEnemyLevel];
 		local damageType = skillData.corpseExplosionDamageType or "Fire"
-		skillData[damageType.."BonusMin"] = skillData.corpseLife * ( skillData.corpseExplosionLifeMultiplier or skillData.selfFireExplosionLifeMultiplier )
-		skillData[damageType.."BonusMax"] = skillData.corpseLife * ( skillData.corpseExplosionLifeMultiplier or skillData.selfFireExplosionLifeMultiplier )
+		skillData[damageType.."BonusMin"] = localCorpseLife * ( skillData.corpseExplosionLifeMultiplier or skillData.selfFireExplosionLifeMultiplier )
+		skillData[damageType.."BonusMax"] = localCorpseLife * ( skillData.corpseExplosionLifeMultiplier or skillData.selfFireExplosionLifeMultiplier )
 	end
 
 	-- Cache global damage disabling flags
