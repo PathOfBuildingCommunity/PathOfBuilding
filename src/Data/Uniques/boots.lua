f9097e4cba783bb9e83dfc9ff02f36a1bb9f6a21
SaloEater <danil.lazarev@gmail.com>
2021-10-18 20:50:27 +0300

    Additional darkray mod

--- src/Data/Uniques/boots.lua
+++ src/Data/Uniques/boots.lua
@@ -583,7 +583,7 @@ Requires Level 65, 62 Str, 62 Dex
 {variant:1}50% reduced Frenzy Charge Duration
 {variant:2}40% reduced Frenzy Charge Duration
 25% reduced Light Radius
-{variant:2}2% chance to Suppress Spell Damage per Frenzy Charge
+{variant:1,2}2% chance to Suppress Spell Damage per Frenzy Charge
 {variant:3}10% increased Evasion Rating per Frenzy Charge
 ]],[[
 Dusktoe
