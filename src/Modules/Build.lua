8716c98b58221e890cbe369679e039333c7e5d64
Alex <Alex@ALEX-PC>
2021-10-19 13:49:36 +0100

    Fix: Changed string.format to s_format

--- src/Modules/Build.lua
+++ src/Modules/Build.lua
@@ -1177,7 +1177,7 @@ function buildMode:FormatStat(statData, statVal, overCapStatVal)
 	valStr = color .. formatNumSep(valStr)
 
 	if overCapStatVal and overCapStatVal > 0 then
-		valStr = valStr .. "^x808080" .. " (+" .. string.format("%d", overCapStatVal) .. "%)"
+		valStr = valStr .. "^x808080" .. " (+" .. s_format("%d", overCapStatVal) .. "%)"
 	end
 	self.lastShowThousandsSeparators = main.showThousandsSeparators
 	self.lastShowThousandsSeparator = main.thousandsSeparator
