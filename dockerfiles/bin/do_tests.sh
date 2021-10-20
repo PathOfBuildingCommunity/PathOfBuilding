5ddad3cd5eb67000dcd9c5f92a1ecae9167a3365
Lazarev <danil.lazarev@gmail.com>
2021-10-20 17:08:35 +0300

    Optimize dockerfiles

--- dockerfiles/bin/do_tests.sh
+++ dockerfiles/bin/do_tests.sh
@@ -7,3 +7,5 @@ done
 LOGS_LENGTH=$(docker logs -f busted | wc -l)
 LAST_START=$(docker logs -f busted | grep -n "Loading main script..." | tail -1 | cut -d : -f 1)
 docker logs --tail $((LOGS_LENGTH - LAST_START + 1)) -f busted > spec/test_results.log
+echo Finished with results:
+echo $(grep "[0-9] successes" spec/test_results.log)
--- dockerfiles/bin/generate_builds.sh
+++ dockerfiles/bin/generate_builds.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd dockerfiles
-docker build -t busted:generation -f Dockerfile_generation .
+docker build -t busted:generation -f Dockerfile .
 cd ..
 docker run -v $(pwd):/root --rm -i busted:generation busted --lua=luajit -r generate
