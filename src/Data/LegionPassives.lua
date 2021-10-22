cefc668a39ef1ad6c24af359caf0df829aea09bd
Wires77 <Wires77@users.noreply.github.com>
2021-10-22 16:52:40 -0500

    Fixing bad export data

--- src/Data/LegionPassives.lua
+++ src/Data/LegionPassives.lua
@@ -3908,6 +3908,11 @@ return {
 			}, 
 			["not"] = false, 
 			["sd"] = {
+				[1] = "Energy Shield starts at zero",
+				[2] = "Cannot Recharge or Regenerate Energy Shield",
+				[3] = "Lose 5% of Energy Shield per second",
+				[4] = "Life Leech effects are not removed at Full Life",
+				[5] = "Life Leech effects Recover Energy Shield instead while on Full Life",
 			}, 
 			["dn"] = "Immortal Ambition", 
 			["isJewelSocket"] = false, 
