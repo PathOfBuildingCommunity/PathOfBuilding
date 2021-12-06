3876fd1442488f58279852a6a1e0659c42085cc7
Eyal <killermze@gmail.com>
2021-12-06 11:50:47 +0200

    Revert "launch"
    This reverts commit 2dd23762a268339b0396f8de32b91680f5b52ba3.

--- src/Launch.lua
+++ src/Launch.lua
@@ -15,14 +15,6 @@ launch = { }
 SetMainObject(launch)
 
 function launch:OnInit()
-	-- This is the path to emmy_core.dll. The ?.dll at the end is intentional.
-	package.cpath = package.cpath .. ";C:/Users/Eyal/.vscode/extensions/tangzx.emmylua-0.3.49/debugger/emmy/windows/x86/?.dll"
-	local dbg = require("emmy_core")
-	-- This port must match the Visual Studio Code configuration. Default is 9966.
-	dbg.tcpListen("localhost", 9966)
-	-- Uncomment the next line if you want Path of Building to block until the debugger is attached
-	dbg.waitIDE()
-
 	self.devMode = false
 	self.installedMode = false
 	self.versionNumber = "?"
