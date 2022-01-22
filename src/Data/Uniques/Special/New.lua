2718aa4df3174edda0c898224efc310090a7a514
Nostrademous <nostrademous@hotmail.com>
2022-01-21 21:46:00 -0500

    feat: added first two teased uniques

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -5,6 +5,23 @@
 data.uniques.new = {
 
 -- New
+[[
+Melding of the Flesh
+Cobalt Jewel
+Limited to: 1
+-76% to all Elemental Resistances
+Elemental Resistances are capped by your highest Maximum Elemental Resistance instead
+]],[[
+Crystallised Omniscience
+Onyx Amulet
+Requires Level 61
+Implicits: 1
+{tags:jewellery_attribute}+(10-16) to all Attributes
+Modifiers to Attributes instead apply to Omniscience
++1% to all Elemental Resistance per 10 Omniscience
+Penetrate 1% Elemental Resistance per 10 Omniscience
+Attribute Requirements can be satisfied by 23% of Omniscience
+]]
 
 -- Reworked
 
