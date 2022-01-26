f9f4ac7734b46dab0871ceebe9d742fd8ecd4752
Nostrademous <nostrademous@hotmail.com>
2022-01-25 20:20:24 -0500

    feat: added unique warstaff

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -12,6 +12,14 @@ Cobalt Jewel
 Limited to: 1
 -(75-76)% to All Elemental Resistances
 Elemental Resistances are capped by your highest Maximum Elemental Resistance instead
+]],[[
+The Annihilating Light
+Quarterstaff
+Requires Level 68, 78 Str, 78 Int
+Implicits: 1
++18% Chance to Block Attack Damage while wielding a Staff
+63% reduced Elemental Resistances
+Elemental Skills deal Triple Damage
 ]]
 
 -- Reworked
