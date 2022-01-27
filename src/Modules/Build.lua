8e99b4da0edf04532b0cb1e2a23513c445d9cf9c
JonAloof <jonathan@unboxthepattern.com>
2022-01-26 18:55:59 -0800

    Updated dropbox to auto-resize

--- src/Modules/Build.lua
+++ src/Modules/Build.lua
@@ -15,7 +15,7 @@ local s_format = string.format
 local banditDropList = {
 	{ label = "2 Passive Points", id = "None" },
 	{ label = "Oak (Life Regen, Phys.Dmg. Reduction, Phys.Dmg)", id = "Oak" },
-	{ label = "Kraityn (Attack/Cast Speed, Avoid.Ele.Ailments, Move Speed)", id = "Kraityn" },
+	{ label = "Kraityn (Attack/Cast Speed, Avoid Elemental Ailments, Move Speed)", id = "Kraityn" },
 	{ label = "Alira (Mana Regen, Crit Multiplier, Resists)", id = "Alira" },
 }
 
@@ -501,6 +501,9 @@ function buildMode:Init(dbFileName, buildName, buildXML, convertBuild)
 		self.modFlag = true
 		self.buildFlag = true
 	end)
+	self.controls.bandit.maxDroppedWidth = 500
+	self.controls.bandit.enableDroppedWidth = true
+	self.controls.bandit:CheckDroppedWidth(true)
 	self.controls.banditLabel = new("LabelControl", {"BOTTOMLEFT",self.controls.bandit,"TOPLEFT"}, 0, 0, 0, 14, "^7Bandit:")
 	-- The Pantheon
 	local function applyPantheonDescription(tooltip, mode, index, value)
