ddb641f57b9d20dfd926c3255c54168f6d659566
Wires77 <Wires77@users.noreply.github.com>
2022-01-28 11:23:17 -0600

    partial revert of a misunderstanding of which stacks were being used

--- src/Data/Skills/act_dex.lua
+++ src/Data/Skills/act_dex.lua
@@ -1765,14 +1765,17 @@ skills["ChargedDash"] = {
 	castTime = 1,
 	parts = {
 		{
-			name = "Channelling",
+			name = "Channelling, No Stages",
+		},
+		{
+			name = "Channelling, Max Stages",
 		},
 		{
 			name = "Release",
 		},
 	},
 	preDamageFunc = function(activeSkill, output)
-		   if activeSkill.skillPart == 2 then
+		   if activeSkill.skillPart == 3 then
 			   local finalWaveDamageModifier = activeSkill.skillModList:Sum("INC", activeSkill.skillCfg, "chargedDashFinalDamageModifier")
 			   activeSkill.skillModList:NewMod("Damage", "MORE", finalWaveDamageModifier, "Skill:ChargedDash", ModFlag.Attack, { type = "Release Damage", skillPart = 3 })
 		   end
@@ -1781,13 +1784,13 @@ skills["ChargedDash"] = {
 		["base_skill_show_average_damage_instead_of_dps"] = {
 		},
 		["charged_dash_damage_+%_final"] = {
-			mod("chargedDashFinalDamageModifier", "INC", nil, 0, 0, { type="BaseReleaseDamage", skillPart = 2 }),
+			mod("chargedDashFinalDamageModifier", "INC", nil, 0, 0, { type="BaseReleaseDamage", skillPart = 3 }),
 		},
 		["charged_dash_damage_+%_final_per_stack"] = {
-			mod("chargedDashFinalDamageModifier", "INC", nil, 0, 0, { type="Multiplier", skillPart = 2, var = "ChargedDashStage" }),
+			mod("chargedDashFinalDamageModifier", "INC", nil, 0, 0, { type="Multiplier", skillPart = 3, var = "ChargedDashStage" }),
 		},
 		["charged_dash_channelling_damage_at_full_stacks_+%_final"] = {
-			mod("Damage", "MORE", nil, 0, 0, { type= "MultiplierThreshold", var= "ChargedDashStage", threshold = 15 }, { type = "SkillPart", skillPart = 1 }),
+			mod("Damage", "MORE", nil, 0, 0, { type = "SkillPart", skillPart = 2 }),
 		},
 	},
 	baseFlags = {
@@ -1800,9 +1803,9 @@ skills["ChargedDash"] = {
 		skill("radiusLabel", "Start of Dash:"),
 		skill("radiusSecondary", 26),
 		skill("radiusSecondaryLabel", "End of Dash:"),
-		skill("hitTimeMultiplier", 2, { type = "Skill", skillPart = 1 }),
+		skill("hitTimeMultiplier", 2, { type = "Skill", skillPartList = { 1, 2 } }),
 		mod("Multiplier:ChargedDashMaxStages", "BASE", 15),
-		skill("showAverage", true, { type = "SkillPart", skillPart = 2 }),
+		skill("showAverage", true, { type = "SkillPart", skillPart = 3 }),
 	},
 	qualityStats = {
 		Default = {
