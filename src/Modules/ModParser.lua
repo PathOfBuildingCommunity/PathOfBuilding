4ca3ec5d2619f4059efa6c9b8d2f8208eaf40ca1
Tomas Slusny <slusnucky@gmail.com>
2022-01-30 18:23:54 +0100

    Add support for Strength of Blood less damage taken (#3983)
    Fixes #3620
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/Modules/ModParser.lua
+++ src/Modules/ModParser.lua
@@ -1469,6 +1469,9 @@ end
 -- List of special modifiers
 local specialModList = {
 	-- Keystones
+	["(%d+)%% less damage taken for every (%d+)%% life recovery per second from leech"] = function(num, _, div)
+		return {  mod("DamageTaken", "MORE", -num, { type = "PerStat", stat = "MaxLifeLeechRatePercent", div = tonumber(div) }) }
+	end,
 	["modifiers to chance to suppress spell damage instead apply to chance to dodge spell hits at 50%% of their value"] = {
 		flag("ConvertSpellSuppressionToSpellDodge"),
 		mod("SpellSuppressionChance", "OVERRIDE", 0, "Acrobatics"),
