a4c59fc8e3c8b807488871722a426f7472083426
Tomas Slusny <slusnucky@gmail.com>
2022-04-17 02:40:40 +0200

    Remove import popup and allow both code and URLs in import
    With import type autodetection the popup is no longer necessary and this
    can be done in a lot more elegant way.
    
    Supersedes #4246
    Fixes #4163
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/Modules/Build.lua
+++ src/Modules/Build.lua
@@ -921,11 +921,8 @@ function buildMode:OnFrame(inputEvents)
 				end
 		elseif IsKeyDown("CTRL") then
 				if event.key == "i" then
-					if self.viewMode == "IMPORT" then
-						self.importTab.controls.importCodePastebin:Click()
-					else
 						self.viewMode = "IMPORT"
-					end
+					self.importTab:SelectControl(self.importTab.controls.importCodeIn)
 				elseif event.key == "s" then
 					self:SaveDBFile()
 					inputEvents[id] = nil
