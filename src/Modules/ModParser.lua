8d14ab32cd15b57ef48f1fdbbe912e8074822eee
Tomas Slusny <slusnucky@gmail.com>
2022-05-16 01:47:38 +0200

    Fix supreme ego more mana reservation of skills to only affects auras (#4293)
    This is easily verifiable in game and I assume whatever sources left
    that apply to all reservation of skills also follow same pattern so I
    just edited "mana reservation of skills" directly.
    
    Signed-off-by: Tomas Slusny <slusnucky@gmail.com>

--- src/Modules/ModParser.lua
+++ src/Modules/ModParser.lua
@@ -128,7 +128,7 @@ local modNameList = {
 	["cost of skills"] = "Cost",
 	["mana reserved"] = "ManaReserved",
 	["mana reservation"] = "ManaReserved",
-	["mana reservation of skills"] = "ManaReserved",
+	["mana reservation of skills"] = { "ManaReserved", tag = { type = "SkillType", skillType = SkillType.Aura } },
 	["mana reservation efficiency of skills"] = "ManaReservationEfficiency",
 	["life reservation efficiency of skills"] = "LifeReservationEfficiency",
 	["reservation of skills"] = "Reserved",
