fbe78efb0f0a12f995da9c76cdd57530259a88e7
LocalIdentity <localidentity2@gmail.com>
2022-05-16 05:36:13 +1000

    Update uniques with in game values

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -11,10 +11,10 @@ Crystal Belt
 Requires Level 79
 Implicits: 1
 {tags:jewellery_defense}+(60-80) to maximum Energy Shield
-{tags:life}30% increased Life Recovery from Flasks
+{tags:life}(30-40)% increased Life Recovery from Flasks
 33% of Chaos Damage taken does not bypass Energy Shield
 33% of Non-Chaos Damage taken bypasses Energy Shield
-{tags:jewellery_defense}Gain 6% of Maximum Life as Extra Maximum Energy Shield
+{tags:jewellery_defense}Gain (5-10)% of Maximum Life as Extra Maximum Energy Shield
 Supreme Decadence
 ]],[[
 Echoes of Creation
@@ -22,11 +22,11 @@ Royal Burgonet
 Shaper Item
 Requires Level 65, 148 Str
 Socketed Warcry Skills have +1 Cooldown Use
-116% increased Armour
-+63 to maximum Life
-When you Attack, take 15% of Life as Physical Damage for
+(80-120))% increased Armour
++(50-70)) to maximum Life
+When you Attack, take (15-20)% of Life as Physical Damage for
 each Warcry Exerting the Attack
-Skills deal 12% more Damage for each Warcry Exerting them
+Skills deal (10-15))% more Damage for each Warcry Exerting them
 ]],[[
 The Eternal Struggle
 Onyx Amulet
@@ -36,10 +36,10 @@ Requires Level 61
 Implicits: 2
 9% increased Mana Reservation Efficiency of Skills
 {tags:speed}6% increased Movement Speed
-{tags:jewellery_attribute}+31 to Strength
-{tags:jewellery_attribute}+22 to Dexterity
-{tags:jewellery_attribute}+50 to Intelligence
-{tags:jewellery_defense}15% increased Global Defences
+{tags:jewellery_attribute}+(20-50) to Strength
+{tags:jewellery_attribute}+(20-50) to Dexterity
+{tags:jewellery_attribute}+(20-50) to Intelligence
+{tags:jewellery_defense}(10-15)% increased Global Defences
 Critical Strikes inflict Malignant Madness if The Eater of Worlds is dominant
 Kill Enemies that have 15% or lower Life on Hit if The Searing Exarch is dominant
 ]],[[
@@ -50,7 +50,7 @@ Elder Item
 Requires Level 16
 Implicits: 1
 {tags:jewellery_resistance}+(20-30)% to Cold Resistance
-{tags:jewellery_resistance}+25% to Cold Resistance
+{tags:jewellery_resistance}+(20-30)% to Cold Resistance
 All Damage with Hits can Chill
 All Damage Taken from Hits can Chill you
 Enemies Chilled by your Hits can be Shattered as though Frozen
