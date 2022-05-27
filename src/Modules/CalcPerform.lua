b8a0e449caf38ac5880a3166cc1ecbd3f0aae391
LocalIdentity <localidentity2@gmail.com>
2022-05-27 18:29:58 +1000

    Fix Exposure mastery stacking incorrectly

--- src/Modules/CalcPerform.lua
+++ src/Modules/CalcPerform.lua
@@ -3046,7 +3046,7 @@ function calcs.perform(env, avoidCache)
 			if min ~= math.huge then
 				-- Modify the magnitude of all exposures
 				for _, mod in ipairs(modDB:Tabulate("BASE", nil, "ExtraExposure", "Extra"..element.."Exposure")) do
-					min = m_min(min, modDB:Override(nil, "ExposureMin")) + mod.value
+					min = min + mod.value
 				end
 				enemyDB:NewMod(element.."Resist", "BASE", m_min(min, modDB:Override(nil, "ExposureMin")), source)
 				modDB:NewMod("Condition:AppliedExposureRecently", "FLAG", true, "")
