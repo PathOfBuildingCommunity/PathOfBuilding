a23c682ac1d9b7981e0d9cffe8f516d79e87edc2
Lothrik <maximius@gmail.com>
2022-07-13 11:54:05 -0700

    Check placeholder type directly instead of by value

--- src/Modules/CalcSetup.lua
+++ src/Modules/CalcSetup.lua
@@ -293,7 +293,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 		if env.configInput.enemyLevel then
 			env.enemyLevel = m_min(data.misc.MaxEnemyLevel, env.configInput.enemyLevel)
 		elseif env.configPlaceholder["enemyLevel"] then
-			if env.configPlaceholder["enemyLevel"] == 82 then
+			if env.configInput.enemyIsBoss == "None" then
 				env.enemyLevel = m_min(data.misc.MaxEnemyLevel, env.build.characterLevel, env.configPlaceholder["enemyLevel"])
 			else
 				env.enemyLevel = m_min(data.misc.MaxEnemyLevel, env.configPlaceholder["enemyLevel"])
