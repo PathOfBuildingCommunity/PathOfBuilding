95892f758b56b568bb8c7c22e24df4cc72357042
Nostrademous <nostrademous@hotmail.com>
2022-08-08 21:50:42 -0400

    Feat: add support for Non-Vaal gems

--- src/Modules/CalcTools.lua
+++ src/Modules/CalcTools.lua
@@ -100,11 +100,13 @@ end
 
 -- Check if given gem is of the given type ("all", "strength", "melee", etc)
 function calcLib.gemIsType(gem, type)
+	print(type)
 	return (type == "all" or 
 			(type == "elemental" and (gem.tags.fire or gem.tags.cold or gem.tags.lightning)) or 
 			(type == "aoe" and gem.tags.area) or
 			(type == "trap or mine" and (gem.tags.trap or gem.tags.mine)) or
 			(type == "active skill" and gem.tags.active_skill) or
+			(type == "non-vaal" and not gem.tags.vaal) or
 			(type == gem.name:lower()) or
 			gem.tags[type])
 end
