da0b7561b70db2c590341edc7d413b311cfab516
Sinured <githubmm@use.startmail.com>
2022-08-11 12:13:58 +0200

    damage and atk increase only applies to melee weapons and unarmed

--- src/Data/Skills/sup_str.lua
+++ src/Data/Skills/sup_str.lua
@@ -3207,15 +3207,18 @@ skills["SupportMultistrike"] = {
 	statDescriptionScope = "gem_stat_descriptions",
 	statMap = {
 		["multistrike_damage_+%_final_on_first_repeat"] = {
-			mod("Damage", "MORE", nil),
+			mod("Damage", "MORE", nil, ModFlag.WeaponMelee),
+			mod("Damage", "MORE", nil, ModFlag.Unarmed),
 		},
 		["multistrike_damage_+%_final_on_second_repeat"] = {
 		},
 		["support_multiple_attack_damage_+%_final"] = {
-			mod("Damage", "MORE", nil, ModFlag.Attack),
+			mod("Damage", "MORE", nil, ModFlag.WeaponMelee),
+			mod("Damage", "MORE", nil, ModFlag.Unarmed),
 		},
 		["support_multiple_attacks_melee_attack_speed_+%_final"] = {
-			mod("Speed", "MORE", nil, ModFlag.Attack),
+			mod("Speed", "MORE", nil, ModFlag.WeaponMelee),
+			mod("Speed", "MORE", nil, ModFlag.Unarmed),
 		},
 		["multistrike_area_of_effect_+%_per_repeat"] = {
 			mod("AreaOfEffect", "INC", nil)
@@ -3305,14 +3308,17 @@ skills["SupportMultistrikePlus"] = {
 		["multistrike_damage_+%_final_on_second_repeat"] = {
 		},
 		["multistrike_damage_+%_final_on_third_repeat"] = {
-			mod("Damage", "MORE", nil),
+			mod("Damage", "MORE", nil, ModFlag.WeaponMelee),
+			mod("Damage", "MORE", nil, ModFlag.Unarmed),
 			div = 2,
 		},
 		["support_multiple_attack_damage_+%_final"] = {
-			mod("Damage", "MORE", nil, ModFlag.Attack),
+			mod("Damage", "MORE", nil, ModFlag.WeaponMelee),
+			mod("Damage", "MORE", nil, ModFlag.Unarmed),
 		},
 		["support_multiple_attacks_melee_attack_speed_+%_final"] = {
-			mod("Speed", "MORE", nil, ModFlag.Attack),
+			mod("Speed", "MORE", nil, ModFlag.WeaponMelee),
+			mod("Speed", "MORE", nil, ModFlag.Unarmed),
 		},
 	},
 	qualityStats = {
