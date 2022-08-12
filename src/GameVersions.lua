c8555063ac026a4e8ce73e879e4c64e8e38ab29c
Regisle <49933620+Regisle@users.noreply.github.com>
2022-08-13 07:31:06 +0930

    Add 3.19 Tree (#4744)
    * add Initial support for adding non-Tree nodes to Tree (Nine lives)
    
    * update positions and add other dummy nodes
    
    * separate stats and reminder text and add new nodes
    
    * rest of nodes
    
    * Fix comma
    
    * fix spelling of sapped
    
    * update spec.lua for passiveSkills
    
    * remove unrelated spec
    
    * update file to hardcode group and node IDs, and correct ascendency
    
    * update to new tree
    
    * add initial 3_19 tree
    
    * spelling fix
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/GameVersions.lua
+++ src/GameVersions.lua
@@ -7,7 +7,7 @@ liveTargetVersion = "3_0"
 -- Skill tree versions
 ---Added for convenient indexing of skill tree versions.
 ---@type string[]
-treeVersionList = { "2_6", "3_6", "3_7", "3_8", "3_9", "3_10", "3_11", "3_12", "3_13", "3_14", "3_15", "3_16", "3_17", "3_18", }
+treeVersionList = { "2_6", "3_6", "3_7", "3_8", "3_9", "3_10", "3_11", "3_12", "3_13", "3_14", "3_15", "3_16", "3_17", "3_18", "3_19", }
 --- Always points to the latest skill tree version.
 latestTreeVersion = treeVersionList[#treeVersionList]
 ---Tree version where multiple skill trees per build were introduced to PoBC.
@@ -84,5 +84,10 @@ treeVersions = {
 		display = "3.18",
 		num = 3.18,
 		url = "https://www.pathofexile.com/passive-skill-tree/3.18.0/",
+	},
+	["3_19"] = {
+		display = "3.19",
+		num = 3.19,
+		url = "https://www.pathofexile.com/passive-skill-tree/3.19.0/",
 	},
 }
