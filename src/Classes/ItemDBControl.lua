d96880ddf5d9cb10d2eac4e1c7908cb895b15dae
QuickStick <31533893+QuickStick123@users.noreply.github.com>
2022-08-19 21:02:11 +1000

    add

--- src/Classes/ItemDBControl.lua
+++ src/Classes/ItemDBControl.lua
@@ -69,6 +69,9 @@ local ItemDBClass = newClass("ItemDBControl", "ListControl", function(self, anch
 		self.controls.requirement = new("DropDownControl", {"LEFT",self.controls.sort,"BOTTOMLEFT"}, 0, 11, 179, 18, { "Any requirements", "Current level", "Current attributes", "Current useable" }, function(index, value)
 			self.listBuildFlag = true
 		end)
+		self.controls.obtainable = new("DropDownControl", {"LEFT",self.controls.requirement,"RIGHT"}, 2, 0, 179, 18, { "Any item", "Obtainable", "Unobtainable"}, function(index, value)
+			self.listBuildFlag = true
+		end)
 	end
 	self.controls.search = new("EditControl", {"BOTTOMLEFT",self,"TOPLEFT"}, 0, -2, 258, 18, "", "Search", "%c", 100, function()
 		self.listBuildFlag = true
@@ -108,6 +111,12 @@ function ItemDBClass:DoesItemMatchFilters(item)
 	end
 	if self.dbType == "UNIQUE" and self.controls.league.selIndex > 1 then
 		if (self.controls.league.selIndex == 2 and item.league) or (self.controls.league.selIndex > 2 and (not item.league or not item.league:match(self.leagueList[self.controls.league.selIndex]))) then
+			return false
+		end
+	end
+	if self.dbType == "UNIQUE" and self.controls.obtainable.selIndex > 1 then
+		local obtainable = not (item.source == "No longer obtainable")
+		if (self.controls.obtainable.selIndex == 2 and not obtainable) or (self.controls.obtainable.selIndex == 3 and obtainable) then
 			return false
 		end
 	end
