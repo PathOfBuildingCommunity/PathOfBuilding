a88e638a3dafd5e39ac91577f40a646b2265aa68
QuickStick <31533893+QuickStick123@users.noreply.github.com>
2022-08-23 14:52:59 +1000

    merge essentia sanguis

--- src/Data/Uniques/claw.lua
+++ src/Data/Uniques/claw.lua
@@ -121,43 +121,30 @@ Implicits: 3
 {variant:2,3,4}8% increased Spell Damage per 5% Chance to Block Attack Damage
 ]],[[
 Essentia Sanguis
-Eye Gouger
+{variant:1,2,3}Eye Gouger
+{variant:4,5}Vaal Claw
 Variant: Pre 1.3.0
 Variant: Pre 2.0.0
 Variant: Pre 2.6.0
+Variant: Pre 3.11.0
 Variant: Current
-Requires Level 64, 113 Dex, 113 Int
 Implicits: 3
 {variant:1,2}0.6% of Physical Attack Damage Leeched as Life
 {variant:3}Gain 31 Life per Enemy Hit by Attacks
-{variant:4}Gain 50 Life per Enemy Hit by Attacks
-{variant:1}10% additional Chance to Block while Dual Wielding Claws
-{variant:2,3,4}8% additional Chance to Block while Dual Wielding Claws
+{variant:4,5}2% of Physical Attack Damage Leeched as Life
+{variant:1}+10% Chance to Block Attack Damage while Dual Wielding Claws
+{variant:2,3,4,5}+8% Chance to Block Attack Damage while Dual Wielding Claws
 {variant:1}(80-120)% increased Physical Damage
-{variant:2,3,4}(140-180)% increased Physical Damage
+{variant:2,3,4,5}(140-180)% increased Physical Damage
 {variant:1}Adds 1 to 50 Lightning Damage
 {variant:2,3}Adds 1 to 80 Lightning Damage
-{variant:4}Adds 1 to 200 Lightning Damage
+{variant:4,5}Adds 1 to 200 Lightning Damage
 (20-30)% increased Attack Speed
 +(30-40) to maximum Energy Shield
-Leech Energy Shield instead of Life
-]],[[
-Essentia Sanguis
-Vaal Claw
-Variant: Pre 3.11.0
-Variant: Current
-Requires Level 66, 95 Dex, 131 Int
-Implicits: 1
-2% of Physical Attack Damage Leeched as Life
-+8% Chance to Block Attack Damage while Dual Wielding Claws
-(140-180)% increased Physical Damage
-Adds 1 to 200 Lightning Damage
-(20-30)% increased Attack Speed
-+(30-40) to maximum Energy Shield
-{variant:1}Leech Energy Shield instead of Life
-{variant:2}50% reduced Maximum Recovery per Energy Shield Leech
-{variant:2}Energy Shield Leech Effects from Attacks are not removed at Full Energy Shield
-{variant:2}Ghost Reaver
+{variant:1,2,3,4}Leech Energy Shield instead of Life
+{variant:5}50% reduced Maximum Recovery per Energy Shield Leech
+{variant:5}Energy Shield Leech Effects from Attacks are not removed at Full Energy Shield
+{variant:5}Ghost Reaver
 ]],[[
 Hand of Thought and Motion
 Blinder
