595a3c6ed7fac332065cdd0c79fac8d1d04325c4
TPlant <PJacek@users.noreply.github.com>
2022-10-20 07:57:10 +0200

    Adjust sample Brittle effects (#5136)

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -3711,7 +3711,7 @@ function calcs.offence(env, actor, activeSkill)
 				ramping = true,
 			},
 			["Brittle"] = {
-				effList = { 5, 10 },
+				effList = { 2, 4 },
 				effect = function(damage, effectMod) return 10 * ((damage / enemyThreshold) ^ 0.4) * effectMod end,
 				thresh = function(damage, value, effectMod) return damage * ((10 * effectMod / value) ^ 2.5) end,
 				ramping = true,
