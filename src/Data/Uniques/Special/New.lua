8b06755f5434f7255b4db9b0344c0d9e88be2610
QuickStick <31533893+QuickStick123@users.noreply.github.com>
2022-12-02 15:18:00 +1100

    Add items
    Update New.lua

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -53,6 +53,52 @@ Wolf Pelt
 85% increased Evasion Rating
 22% chance to Avoid Elemental Ailments
 Recover 176 Life when you Suppress Spell Damage
+]],[[
+Anathema
+Moonstone Ring
+LevelReq: 49
+Implicits: 1
+{tags:jewellery_defense}+(15-25) to maximum Energy Shield
+{tags:jewellery_attribute}+37 to Intelligence
+{tags:caster}13% increased Cast Speed
+16% chance to gain a Power Charge when you Cast a Curse Spell
+Your Curse Limit is equal to your maximum Power Charges
+]],[[
+Eternal Damnation
+Agate Amulet
+LevelReq: 52
+Implicits: 1
+{tags:jewellery_attribute}+(16-24) to Strength and Intelligence
+{tags:mana}+45 to maximum Mana
+{tags:jewellery_resistance}+9% to Chaos Resistance
+{tags:jewellery_resistance}-5% to all maximum Resistances
+Gain additional Elemental Damage Reduction equal to half your Chaos Resistance
+]],[[
+Progenesis
+Amethyst Flask
+LevelReq: 60
+17% reduced Charges per use
+14% increased Duration
+When Hit during effect, 25% of Life loss from Damage taken occurs over 4 seconds instead
+]],[[
+Rational Doctrine
+Cobalt Jewel
+You have Consecrated Ground around you while stationary if Strength is your highest Attribute
+25% chance to create Profane Ground on Critical Strike if Intelligence is your highest Attribute
+Effects of Consecrated Ground you create Linger for 4 seconds 
+Effects of Profane Ground you create Linger for 4 seconds
+]],[[
+Entropic Devastation
+Assassin's Mitts
+Grants Call of Steel
+124%  increased Evasion and Energy Shield
+50% increased effects of impale inflicted by Spells
+Critical Strikes with Spell inflict Impale
+]],[[
+Firesong
+Crimson Jewel
++13% to Fire Resistance
+Modifiers to Ignite Duration on you apply to all Elemental Ailments
 ]]
 -- Reworked
 
