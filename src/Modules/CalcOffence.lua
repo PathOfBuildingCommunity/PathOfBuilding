d1bfd705c58102fe6144e123de4162a4b07e0fc8
Paliak <91493239+Paliak@users.noreply.github.com>
2022-12-05 13:03:51 +0100

    FIX: Apply QuickStick's arcanist brand formula.

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -1554,11 +1554,11 @@ function calcs.offence(env, actor, activeSkill)
 			output.Speed = skillData.triggerRate
 			skillData.showAverage = false
 		elseif skillData.triggeredByBrand and skillData.triggered then
-			local activationFreqInc = 100 / (100 + skillModList:Sum("INC", cfg, "Speed", "BrandActivationFrequency"))
+			local activationFreqInc = (100 + skillModList:Sum("INC", cfg, "Speed", "BrandActivationFrequency")) / 100
 			local activationFreqMore = skillModList:More(cfg, "BrandActivationFrequency")
 			local linkedSpellsCount = skillModList:Sum("BASE", cfg, "ArcanistSpellsLinked") or 1
 			
-			output.Speed = activationFreqMore * linkedSpellsCount * activationFreqInc
+			output.Speed = activationFreqInc * activationFreqMore / linkedSpellsCount
 			output.Time = 1 / output.Speed
 			output.TriggerTime = output.Time
 			
@@ -1566,8 +1566,8 @@ function calcs.offence(env, actor, activeSkill)
 				breakdown.Speed = {
 					"1.00 ^8(base activation cooldown)",
 					s_format("/ %.2f ^8(more activation frequency)", activationFreqMore),
-					s_format("/ %d ^8(number of linked spells)", linkedSpellsCount),
 					s_format("/ %.2f ^8(increased activation frequency)", activationFreqInc),
+					s_format("* %d ^8(number of linked spells)", linkedSpellsCount),
 					s_format("= %.2f ^8(trigger cooldown)", output.Time),
 					"",
 					s_format("1 / %.2f ", output.Time),
