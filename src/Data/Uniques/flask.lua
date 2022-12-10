197aa8d75b9449950560c50b4755e6b00b94cb9f
QuickStick <31533893+QuickStick123@users.noreply.github.com>
2022-12-10 22:20:21 +1100

    Update some more

--- src/Data/Uniques/flask.lua
+++ src/Data/Uniques/flask.lua
@@ -119,6 +119,7 @@ Source: Drops from unique{Synthete Nightmare} in normal{The Cortex}
 Variant: Pre 3.15.0
 Variant: Pre 3.16.0
 Variant: Current
+Implicits: 1
 Creates Consecrated Ground on Use
 {variant:1}(30-50)% increased Duration
 {variant:2}(20-40)% increased Duration
@@ -272,7 +273,7 @@ Variant: Pre 1.3.0
 Variant: Pre 2.5.0
 Variant: Pre 3.15.0
 Variant: Current
-Requires Level 68
+LevelReq: 68
 {variant:1}(30-40)% Chance to Block Attack Damage during Effect
 {variant:2}(20-30)% Chance to Block Attack Damage during Effect
 {variant:3}(14-20)% Chance to Block Attack Damage during Effect
@@ -286,7 +287,7 @@ Replica Rumi's Concoction
 Granite Flask
 League: Heist
 Source: Steal from a unique{Curio Display} during a Grand Heist
-Requires Level 68
+LevelReq: 68
 90% reduced Duration
 Gain 1 Endurance Charge on use
 Petrified during Effect
@@ -295,33 +296,32 @@ Petrified during Effect
 ]],[[
 Sin's Rebirth
 Stibnite Flask
-Requires Level 14
 Implicits: 1
 Creates a Smoke Cloud on Use
-Gain Unholy Might during Effect
-Immunity to Ignite during Effect
-Removes Burning on use
+Grants Immunity to Ignite for 4 seconds if used while Ignited
+Removes all Burning when used
+Unholy Might during Effect
 ]],[[
 The Sorrow of the Divine
 Sulphur Flask
+Variant: Pre 3.7.0
+Variant: Current
 League: Legion
-Requires Level 35
 Implicits: 1
 Creates Consecrated Ground on Use
+{variant:2}Life Recovery from Flasks also applies to Energy Shield during Effect
 (25-50)% increased Duration
 Zealot's Oath during Effect
-Life Recovery from Flasks also applies to Energy Shield during Effect
 ]],[[
 Replica Sorrow of the Divine
 Sulphur Flask
 League: Heist
 Source: Steal from a unique{Curio Display} during a Grand Heist
-Requires Level 35
 Implicits: 1
 Creates Consecrated Ground on Use
+Life Recovery from Flasks also applies to Energy Shield during Effect
 (25-50)% increased Duration
 Eldritch Battery during Effect
-Life Recovery from Flasks also applies to Energy Shield during Effect
 ]],[[
 Soul Catcher
 Quartz Flask
@@ -331,14 +331,13 @@ Upgrade: Upgrades to unique{Soul Ripper} via currency{Vial of the Ghost}
 Variant: Pre 3.10.0
 Variant: Pre 3.15.0
 Variant: Current
-Requires Level 27
 Cannot gain Mana during effect
-{variant:2}Vaal Skills have (80-120)% increased Critical Strike Chance during effect
-{variant:3}Vaal Skills have (60-80)% increased Critical Strike Chance during effect
-{variant:1}Vaal Skills deal (60-100)% increased Damage during effect
-{variant:2}Vaal Skills deal (80-120)% increased Damage during effect
-{variant:3}Vaal Skills deal (60-80)% increased Damage during effect
-{variant:1}Vaal Skills have 25% reduced Soul Cost during effect
+{variant:2}(80-120)% increased Critical Strike Chance with Vaal Skills during effect
+{variant:3}(60-80)% increased Critical Strike Chance with Vaal Skills during effect
+{variant:1}(60-100)% increased Damage with Vaal Skills during effect
+{variant:2}(80-120)% increased Damage with Vaal Skills during effect
+{variant:3}(60-80)% increased Damage with Vaal Skills during effect
+{variant:1}Non-Aura Vaal Skills require 25% reduced Souls Per Use during Effect
 {variant:2}Vaal Skills used during effect have (20-40)% reduced Soul Gain Prevention Duration
 {variant:3}Vaal Skills used during effect have 10% reduced Soul Gain Prevention Duration
 ]],[[
@@ -348,11 +347,10 @@ League: Incursion
 Source: Upgraded from unique{Soul Catcher} via currency{Vial of the Ghost}
 Variant: Pre 3.10.0
 Variant: Current
-Requires Level 27
 {variant:1}100% increased Charges per use
-{variant:1}Vaal Skills deal (30-40)% more Damage during effect
+{variant:1}Vaal Skills deal (30-40)% more Damage during Effect
 {variant:1}Vaal Skills used during effect do not apply Soul Gain Prevention
-{variant:1}Gains no Charges During effect of any Soul Ripper Flask
+{variant:1}Gains no Charges during Effect of any Soul Ripper Flask
 {variant:2}+(-40-90) maximum Charges
 {variant:2}Loses all Charges when you enter a new area
 {variant:2}Consumes Maximum Charges to use
@@ -364,7 +362,6 @@ Variant: Pre 2.2.0
 Variant: Pre 3.0.0
 Variant: Pre 3.15.0
 Variant: Current
-Requires Level 18
 {variant:1}30% of Physical Damage from Hits taken as Cold Damage during Effect
 {variant:2,3}20% of Physical Damage from Hits taken as Cold Damage during Effect
 {variant:4}(10-15)% of Physical Damage from Hits taken as Cold Damage during Effect
@@ -376,7 +373,6 @@ Requires Level 18
 ]],[[
 The Overflowing Chalice
 Sulphur Flask
-Requires Level 35
 Variant: Pre 3.15.0
 Variant: Current
 Implicits: 1
@@ -384,9 +380,9 @@ Creates Consecrated Ground on Use
 {variant:1}100% increased Charge Recovery
 {variant:2}(40-60)% increased Charge Recovery
 {variant:1}(10-20)% increased Duration
-Gains no Charges during Effect of any Overflowing Chalice Flask
 {variant:1}100% increased Charges gained by Other Flasks during Effect
 {variant:2}(50-100)% increased Charges gained by Other Flasks during Effect
+Gains no Charges during Effect of any Overflowing Chalice Flask
 ]],[[
 Vessel of Vinktar
 Topaz Flask
@@ -409,36 +405,33 @@ Variant: Current (Proliferation)
 Variant: Current (Penetration)
 Variant: Current (Spells)
 Variant: Current (Attacks)
-Requires Level 68
+LevelReq: 68
 {variant:5,6,7,8,9,10,11,12,13,14,15}(100-80)% increased Charges per use
 {variant:16,17,18}(150-125)% increased Charges per use
-{variant:1,2,3,4,5,6,7,8}Shocks nearby Enemies during Effect
-{variant:9,10,11,12,13,14,15,16,17,18}Shocks nearby Enemies during Effect, causing 10% increased Damage taken
-{variant:1,2,3,4,5,6,7,8}You are Shocked during Effect
-{variant:9,10,11,12,13,14,15,16,17,18}You are Shocked during Effect, causing 50% increased Damage taken
-{variant:1,2,3,4}30% of Lightning Damage Leeched as Life during Effect
-{variant:5,6,7,8,9,10,11,12,13,14,15,16,17,18}20% of Lightning Damage Leeched as Life during Effect
-{variant:1,2,3,4}30% of Lightning Damage Leeched as Mana during Effect
-{variant:5,6,7,8}20% of Lightning Damage Leeched as Mana during Effect
-{variant:1,2,3,4}Life and Mana Leech are instant during effect
+Shocks nearby Enemies during Effect, causing 10% increased Damage taken
+You are Shocked during Effect, causing 50% increased Damage taken
 {variant:1,5,11}Damage Penetrates 10% Lightning Resistance during Effect
 {variant:16}Damage Penetrates 6% Lightning Resistance during Effect
 {variant:2,6,9}Adds (15-25) to (70-90) Lightning Damage to Spells during Effect
 {variant:12}Adds (25-35) to (110-130) Lightning Damage to Spells during Effect
 {variant:17}Adds (10-15) to (55-65) Lightning Damage to Spells during Effect
-{variant:3,7,13}Adds (25-35) to (110-130) Lightning Damage to Attacks during Effect
+{variant:3,7,13}Adds (25-35) to (110-130) Lightning Damage to Spells during Effect
 {variant:18}Adds (10-15) to (55-65) Lightning Damage to Attacks during Effect
 {variant:4,8,10}20% of Physical Damage Converted to Lightning during Effect
 {variant:14}50% of Physical Damage Converted to Lightning during Effect
-{variant:15}(25-40)% increased Effect of Shock during Effect
-{variant:15}Shocks you inflict during Effect spread to other Enemies within a Radius of 20
+{variant:15}(25-40)% increased Effect of Shocks you inflict during Effect
+{variant:15}Shocks you inflict during Effect spread to other Enemies in a radius of 20
+{variant:1,2,3,4}30% of Lightning Damage Leeched as Life during Effect
+{variant:5,6,7,8,9,10,11,12,13,14,15,16,17,18}20% of Lightning Damage Leeched as Life during Effect
+{variant:1,2,3,4}30% of Lightning Damage Leeched as Mana during Effect
+{variant:5,6,7,8}20% of Lightning Damage Leeched as Mana during Effect
+{variant:1,2,3,4}Life and Mana Leech are instant during effect
 ]],[[
 The Wise Oak
 Bismuth Flask
 Variant: Pre 3.0.0
 Variant: Pre 3.15.0
 Variant: Current
-Requires Level 8
 {variant:1,2}During Effect, 10% reduced Damage taken of each Element for which your Uncapped Elemental Resistance is lowest
 {variant:3}During Effect, 6% reduced Damage taken of each Element for which your Uncapped Elemental Resistance is lowest
 {variant:1}During Effect, Damage Penetrates 20% Resistance of each Element for which your Uncapped Elemental Resistance is highest
@@ -450,14 +443,14 @@ Stibnite Flask
 Variant: Pre 3.0.0
 Variant: Pre 3.15.0
 Variant: Current
-Requires Level 48
+LevelReq: 48
 Implicits: 1
 Creates a Smoke Cloud on Use
 {variant:1,2}50% increased Charges per use
 {variant:3}(-10-10)% reduced Charges per use
 {variant:1}(50-70)% increased Damage Over Time during Effect
 {variant:2}(25-40)% increased Damage Over Time during Effect
-Grants level 21 Despair Curse Aura during Effect
+Grants Level 21 Despair Curse Aura during Effect
 ]],
 -- Flask: Ward
 [[
