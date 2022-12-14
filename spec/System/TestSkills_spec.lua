1c3656288f8606c44185eb9a4b148ac43e7f9361
QuickStick <31533893+QuickStick123@users.noreply.github.com>
2022-12-14 17:58:25 +1100

    try adding tests

--- spec/System/TestSkills_spec.lua
+++ spec/System/TestSkills_spec.lua
@@ -22,4 +22,38 @@ describe("TestAttacks", function()
         -- No Envy level increase, so base should still be 205
         assert.are.equals(round(205 * 1.44), build.calcsTab.mainEnv.minion.modDB:Sum("BASE", build.calcsTab.mainEnv.minion.mainSkill.skillCfg, "ChaosMin"))
     end)
+
+    it("adds divergent arcane cloak, ensures round to nearest percent", function()
+        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nArcane Cloak 20/20 Alternate2  1\n")
+        runCallback("OnFrame")
+
+        assert.are.equals(floor(50 * floor(14 * 1.1)), build.calcsTab.mainEnv.player.modDB:Sum("BASE", build.calcsTab.mainEnv.player.mainSkill.skillCfg, "LightningMin"))
+
+        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nArcane Cloak 25/50 Alternate2  1\n")
+        runCallback("OnFrame")
+
+        assert.are.equals(floor(50 * floor(16 * 1.25)), build.calcsTab.mainEnv.player.modDB:Sum("BASE", build.calcsTab.mainEnv.player.mainSkill.skillCfg, "LightningMin"))
+
+        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nArcane Cloak 25/60 Alternate2  1\n")
+        runCallback("OnFrame")
+
+        assert.are.equals(floor(50 * floor(16 * 1.3)), build.calcsTab.mainEnv.player.modDB:Sum("BASE", build.calcsTab.mainEnv.player.mainSkill.skillCfg, "LightningMin"))
+    end)
+
+    it("adds assassin's mark and ensures round to nearest percent and scales correctly", function()
+        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nAssassin's Mark 20/50 Alternate2  1\n")
+        runCallback("OnFrame")
+
+        assert.are.equals(floor(1.5 + 0.5, 2), build.calcsTab.mainEnv.enemy.modDB:Sum("BASE", build.calcsTab.mainEnv.player.mainSkill.skillCfg, "SelfCritChance"))
+
+        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nAssassin's Mark 20/50 Alternate2  1\nMark On Hit 20/20 Default  1\n")
+        runCallback("OnFrame")
+
+        assert.are.equals(floor((1.5 + 0.5) * 0.78, 2), build.calcsTab.mainEnv.enemy.modDB:Sum("BASE", build.calcsTab.player.mainEnv.mainSkill.skillCfg, "SelfCritChance"))
+
+        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nAssassin's Mark 20/20 Alternate2  1\nMark On Hit 20/20 Default  1\n")
+        runCallback("OnFrame")
+
+        assert.are.equals(floor((1.5 + 0.2) * 0.78, 2), build.calcsTab.mainEnv.enemy.modDB:Sum("BASE", build.calcsTab.mainEnv.player.mainSkill.skillCfg, "SelfCritChance"))
+    end)
 end)
