12afac315e96e4392414bfbbf1e320b8a2f315c8
Regisle <Regisle.godform@gmail.com>
2022-12-25 23:11:32 +1030

    Militant Faith update skill cost to mana cost

--- src/Data/Uniques/jewel.lua
+++ src/Data/Uniques/jewel.lua
@@ -1994,7 +1994,7 @@ Implicits: 0
 {variant:14}1% increased Minion Attack and Cast Speed per 10 Devotion
 {variant:15}Minions have +60 to Accuracy Rating per 10 Devotion
 {variant:16}Regenerate 0.6 Mana per Second per 10 Devotion
-{variant:17}1% reduced Cost of Skills per 10 Devotion
+{variant:17}1% reduced Mana Cost of Skills per 10 Devotion
 {variant:18}1% increased effect of Non-Curse Auras per 10 Devotion
 {variant:19}3% increased Defences from Equipped Shield per 10 Devotion
 Passives in radius are Conquered by the Templars
