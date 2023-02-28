77a3ccabe61d434d88e843af7b588e5ea8493ddb
Paliak <91493239+Paliak@users.noreply.github.com>
2023-02-28 13:10:04 +0100

    FIX: hide trigger supports if not compatible

--- src/Modules/CalcTools.lua
+++ src/Modules/CalcTools.lua
@@ -92,6 +92,9 @@ function calcLib.canGrantedEffectSupportActiveSkill(grantedEffect, activeSkill)
 	if grantedEffect.excludeSkillTypes[1] and calcLib.doesTypeExpressionMatch(grantedEffect.excludeSkillTypes, activeSkill.skillTypes) then
 		return false
 	end
+	if grantedEffect.isTrigger and activeSkill.triggeredBy then
+		return false
+	end
 	return not grantedEffect.requireSkillTypes[1] or calcLib.doesTypeExpressionMatch(grantedEffect.requireSkillTypes, activeSkill.skillTypes, not grantedEffect.ignoreMinionTypes and activeSkill.minionSkillTypes)
 end
 
