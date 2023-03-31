98e61180bb52bb6e9e419bd34a83d62bbf6a907f
Paliak <91493239+Paliak@users.noreply.github.com>
2023-03-31 09:46:11 +0200

    Implement Blood Price, El'Abin's Visage uniques (#5809)
    * FEAT: impl Blood Price, El'Abin'S Visage
    
    * FIX: spelling, formatting
    
    ---------
    
    Co-authored-by: LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -6,6 +6,23 @@ data.uniques.new = {
 
 -- New
 [[
+El'Abin's Visage
+Fencer Helm
++(20-30) to Strength
++(20-30) to Dexterity
+(100-120)% increased Armour and Evasion
+(20-30)% increased Rarity of Items found
+Can have a Crucible Passive Skill Tree
+]],
+[[
+Blood Price
+Reaver Helmet
+(20-30)% reduced maximum Life
+Regenerate (227.2-227.9) Life per second
+100% increased Stun and Block Recovery
+Reserves 8% of Nearby Enemy Monsters' Life
+]],
+[[
 Widowhail
 Crude Bow
 (183-225)% increased bonuses gained from Equipped Quiver
