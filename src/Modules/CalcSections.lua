37f000a9190dc97a1460ee5952b91d06808b6c89
Tim <62115140+cardbeard@users.noreply.github.com>
2023-03-31 10:55:49 -0400

    % of Armour also applies to chaos damage from hits (#5818)
    * Added support for the new mastery - % of armour also applies to chaos damage from hits
    
    * Removed unnecessary . and added SelfIgnoreChaosResistance for future-proofing

--- src/Modules/CalcSections.lua
+++ src/Modules/CalcSections.lua
@@ -1478,6 +1478,10 @@ return {
 		{ breakdown = "LightningDamageReduction" },
 		{ modName = { "ArmourAppliesToLightningDamageTaken", "SelfIgnoreLightningResistance", "DamageReductionMax" } },
 	}, },
+	{ label = "Chaos Dmg. Reduct", haveOutput = "ChaosDamageReduction", { format = "{0:output:ChaosDamageReduction}%",
+		{ breakdown = "ChaosDamageReduction" },
+		{ modName = { "ArmourAppliesToChaosDamageTaken", "SelfIgnoreChaosResistance", "DamageReductionMax" } },
+	}, },
 } }
 } },
 { 1, "Evasion", 3, colorCodes.EVASION, {{ defaultCollapsed = false, label = "Evasion", data = {
