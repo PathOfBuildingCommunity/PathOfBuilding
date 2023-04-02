f5157acf5257fb79dfd4c54843ecb3c7859275c3
Regisle <Regisle.godform@gmail.com>
2023-04-02 15:07:50 +0930

    revert comments

--- src/Classes/PartyTab.lua
+++ src/Classes/PartyTab.lua
@@ -528,8 +528,6 @@ function PartyTabClass:ParseBuffs(list, buf, buffType)
 			mode = "Stats"
 		elseif line == "---" then
 			mode = "Name"
-		elseif line:match("---") then -- comment line, just ignore it
-			
 		else
 			if line:find("|") then
 				local modStrings = {}
@@ -588,7 +586,7 @@ function PartyTabClass:exportBuffs(buffType)
 	if self.buffExports[buffType].ConvertedToText then
 		return self.buffExports[buffType].string
 	end
-	local buf = ((buffType == "Curse") and ("--- Curse Limit ---\n" .. tostring(self.buffExports["CurseLimit"]))) or ""
+	local buf = ((buffType == "Curse") and (tostring(self.buffExports["CurseLimit"]))) or ""
 	for buffName, buff in pairs(self.buffExports[buffType]) do
 		if #buf > 0 then
 			buf = buf.."\n"
