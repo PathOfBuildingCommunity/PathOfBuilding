fbef40760044546dacd3fdfd3fca983251459e24
Regisle <49933620+Regisle@users.noreply.github.com>
2023-04-21 13:54:40 +0930

    Implement vaal firestorm burning ground and aoe (#6080)
    * implement vaal firestorm burning ground and aoe
    
    * update radii for firestorm and vaal firestorm
    
    * Fix AoE to only apply to secondary radius
    
    ---------
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Data/Skills/act_int.lua
+++ src/Data/Skills/act_int.lua
@@ -3577,9 +3577,9 @@ skills["Firestorm"] = {
 	},
 	baseMods = {
 		skill("showAverage", false, { type = "SkillPart", skillPart = 1 }),
-		skill("radius", 22),
+		skill("radius", 25),
 		skill("radiusLabel", "Area in which fireballs fall:"),
-		skill("radiusSecondary", 16),
+		skill("radiusSecondary", 13),
 		skill("radiusSecondaryLabel", "Area of fireball explosion:"),
 	},
 	qualityStats = {
@@ -3659,10 +3659,23 @@ skills["VaalFirestorm"] = {
 	skillTypes = { [SkillType.Spell] = true, [SkillType.Damage] = true, [SkillType.Trappable] = true, [SkillType.Totemable] = true, [SkillType.Mineable] = true, [SkillType.Area] = true, [SkillType.Duration] = true, [SkillType.Fire] = true, [SkillType.AreaSpell] = true, [SkillType.Vaal] = true, },
 	statDescriptionScope = "skill_stat_descriptions",
 	castTime = 0.75,
+	statMap = {
+		["vaal_firestorm_gem_explosion_area_of_effect_+%_final"] = {
+			mod("AreaOfEffectSecondary", "MORE", nil, 0, 0)
+		},
+	},
 	baseFlags = {
 		spell = true,
 		area = true,
 		duration = true,
+	},
+	baseMods = {
+		skill("dotIsArea", true),
+		flag("dotIsBurningGround"),
+		skill("radius", 20),
+		skill("radiusLabel", "Area in which fireballs fall:"),
+		skill("radiusSecondary", 13),
+		skill("radiusSecondaryLabel", "Area of fireball explosion:"),
 	},
 	qualityStats = {
 		Default = {
