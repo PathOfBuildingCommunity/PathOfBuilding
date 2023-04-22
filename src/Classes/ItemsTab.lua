703925b6dae55c1bbefcb0a44d6763609fe6f191
Peechey <92683202+Peechey@users.noreply.github.com>
2023-04-22 00:28:24 -0700

    move list to data

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -47,50 +47,6 @@ local catalystQualityFormat = {
 	"^x7F7F7FQuality (Critical Modifiers): "..colorCodes.MAGIC.."+%d%% (augmented)",
 }
 
-local casterTagCrucibleUniques = {
-	["Atziri's Rule"] = true,
-	["Cane of Kulemak"] = true,
-	["Cane of Unravelling"] = true,
-	["Cospri's Malice"] = true,
-	["Cybil's Paw"] = true,
-	["Disintegrator"] = true,
-	["Duskdawn"] = true,
-	["Geofri's Devotion"] = true,
-	["Mjolner"] = true,
-	["Pledge of Hands"] = true,
-	["Soulwrest"] = true,
-	["Taryn's Shiver"] = true,
-	["The Rippling Thoughts"] = true,
-	["The Surging Thoughts"] = true,
-	["The Whispering Ice"] = true,
-	["Tremor Rod"] = true,
-	["Xirgil's Crank"] = true,
-}
-local minionTagCrucibleUniques = {
-	["Arakaali's Fang"] = true,
-	["Ashcaller"] = true,
-	["Chaber Cairn"] = true,
-	["Chober Chaber"] = true,
-	["Clayshaper"] = true,
-	["Earendel's Embrace"] = true,
-	["Femurs of the Saints"] = true,
-	["Jorrhast's Blacksteel"] = true,
-	["Law of the Wilds"] = true,
-	["Midnight Bargain"] = true,
-	["Mon'tregul's Grasp"] = true,
-	["Null's Inclination"] = true,
-	["Queen's Decree"] = true,
-	["Queen's Escape"] = true,
-	["Replica Earendel's Embrace"] = true,
-	["Replica Midnight Bargain"] = true,
-	["Severed in Sleep"] = true,
-	["Soulwrest"] = true,
-	["The Black Cane"] = true,
-	["The Iron Mass"] = true,
-	["The Scourge"] = true,
-	["United in Dream"] = true,
-}
-
 local ItemsTabClass = newClass("ItemsTab", "UndoHandler", "ControlHost", "Control", function(self, build)
 	self.UndoHandler()
 	self.ControlHost()
@@ -2659,10 +2615,10 @@ function ItemsTabClass:AddCrucibleModifierToDisplayItem()
 			keyMap[key] = index
 		end
 		-- check for uniques with off-tag mods
-		if casterTagCrucibleUniques[self.displayItem.title] then
+		if self.build.data.casterTagCrucibleUniques[self.displayItem.title] then
 			includeTags["caster_unique_weapon"] = true
 		end
-		if minionTagCrucibleUniques[self.displayItem.title] then
+		if self.build.data.minionTagCrucibleUniques[self.displayItem.title] then
 			includeTags["minion_unique_weapon"] = true
 		end
 
