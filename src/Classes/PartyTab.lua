1861a3e755f3c4b202fafa9f2cfdd5922c10eae9
Regisle <Regisle.godform@gmail.com>
2023-04-26 20:02:55 +0930

    prevent lower aura effect mods from adding stats

--- src/Classes/PartyTab.lua
+++ src/Classes/PartyTab.lua
@@ -667,7 +667,7 @@ function PartyTabClass:ParseBuffs(list, buf, buffType, label)
 			elseif line == "---" then
 				mode = "Name"
 			else
-				if line:find("|") then
+				if line:find("|") and currentName ~= "SKIP" then
 					local modType, mod = modLib.parseFormattedSourceMod(line, currentModType)
 					if mod then
 						currentModType = modType
@@ -680,12 +680,23 @@ function PartyTabClass:ParseBuffs(list, buf, buffType, label)
 							if isMark then
 								list[modType][currentName].isMark = true
 							end
+						elseif list[modType][currentName].effectMult ~= currentEffect then
+							if list[modType][currentName].effectMult < currentEffect then
+								list[modType][currentName] = {
+									modList = new("ModList"),
+									effectMult = currentEffect
+								}
+							else
+								currentName = "SKIP"
+							end
 						end
-						if mod.source:match("Item") then
-							_, mod.source = mod.source:match("Item:(%d+):(.+)")
-							mod.source = "Party, "..mod.source
+						if currentName ~= "SKIP" then
+							if mod.source:match("Item") then
+								_, mod.source = mod.source:match("Item:(%d+):(.+)")
+								mod.source = "Party, "..mod.source
+							end
+							list[modType][currentName].modList:AddMod(mod)
 						end
-						list[modType][currentName].modList:AddMod(mod)
 					end
 				end
 			end
