c51f19cb33d71a11e127000e42dbc9ed5e56f10e
Paliak <91493239+Paliak@users.noreply.github.com>
2023-05-30 09:48:12 +0200

    WIP: inital work on moving trigger code

--- src/Modules/Calcs.lua
+++ src/Modules/Calcs.lua
@@ -16,6 +16,7 @@ LoadModule("Modules/CalcPerform", calcs)
 LoadModule("Modules/CalcActiveSkill", calcs)
 LoadModule("Modules/CalcDefence", calcs)
 LoadModule("Modules/CalcOffence", calcs)
+LoadModule("Modules/CalcTriggers", calcs)
 
 -- Get the average value of a table -- note this is unused
 function math.average(t)
