289ef984766a8ce8cf962c0328f498d2df27071e
Paliak <91493239+Paliak@users.noreply.github.com>
2023-08-07 03:29:05 +0200

    FEAT: add new uniques

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -5,7 +5,37 @@
 data.uniques.new = {
 
 -- New
-
+[[
+Kitava's Hunger
+Majestic Plate
+88% increased Armour
+Recover 2% of Life on Kill
+37% Increased Stun and Block Recovery
++910 to maximum Life if there are no Life Modifiers on other Equipped items
+]],
+[[
+Arohongui's Tending
+Coral Amulet
+Regenerate 4.4 Mana per second
+37% increased Life Recovery from Flasks
+26% increased Mana Recovery from Flasks
+Life Flasks used while on Low Life apply Recovery Instantly
+Mana Flasks used while on Low Mana apply Recovery Instantly
+]],
+[[
+Bound Fate
+Cloth Belt
++28 to Dexterity
++26 to Intelligence
++64 to Maximum Life
+Every 5 seconds, gain one of the following for 5 seconds:
+Your Hits are always Critical Strikes
+Hits against you always are Critical Strikes
+Attacks cannot Hit you
+Attacks against you always Hit
+Your Damage with Hits is Lucky
+Damage of Hits against you is Lucky
+]],
 -- Reworked
 
 }
