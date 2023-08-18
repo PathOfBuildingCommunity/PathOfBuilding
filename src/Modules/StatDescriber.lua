8b08c5e6da0408668c5fa439e3fea563fd87b44f
Nostrademous <nostrademous@hotmail.com>
2023-08-18 14:22:55 -0400

    bug fix

--- src/Modules/StatDescriber.lua
+++ src/Modules/StatDescriber.lua
@@ -81,7 +81,7 @@ local function applySpecial(val, spec)
 	elseif spec.k == "divide_by_twenty" then
 		val[spec.v].min = val[spec.v].min / 20
 		val[spec.v].max = val[spec.v].max / 20
-		val[spec.v].fmt = "g
+		val[spec.v].fmt = "g"
 	elseif spec.k == "divide_by_one_hundred" then
 		val[spec.v].min = val[spec.v].min / 100
 		val[spec.v].max = val[spec.v].max / 100
