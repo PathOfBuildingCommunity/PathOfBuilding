ae41c35dba03a9dc7ee8ff85a97faedd96b50469
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2023-08-18 17:54:01 +1000

    Update mod ranges

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -8,26 +8,26 @@ data.uniques.new = {
 [[
 Kitava's Hunger
 Majestic Plate
-88% increased Armour
-Recover 2% of Life on Kill
-37% Increased Stun and Block Recovery
-+910 to maximum Life if there are no Life Modifiers on other Equipped items
+(60-100)% increased Armour
+Recover (1-3)% of Life on Kill
+(30-40)% Increased Stun and Block Recovery
++(700-1000) to maximum Life if there are no Life Modifiers on other Equipped items
 ]],
 [[
 Arohongui's Tending
 Coral Amulet
-Regenerate 4.4 Mana per second
-37% increased Life Recovery from Flasks
-26% increased Mana Recovery from Flasks
+{tags:life}Regenerate (2-4) Life per second
+{tags:life}(15-30)% increased Life Recovery from Flasks
+{tags:mana}(15-30)% increased Mana Recovery from Flasks
 Life Flasks used while on Low Life apply Recovery Instantly
 Mana Flasks used while on Low Mana apply Recovery Instantly
 ]],
 [[
 Bound Fate
 Cloth Belt
-+28 to Dexterity
-+26 to Intelligence
-+64 to Maximum Life
+{tags:jewellery_attribute}+(20-30) to Dexterity
+{tags:jewellery_attribute}+(20-30) to Intelligence
+{tags:life}+(60-80) to Maximum Life
 Every 5 seconds, gain one of the following for 5 seconds:
 Your Hits are always Critical Strikes
 Hits against you always are Critical Strikes
@@ -38,8 +38,8 @@ Damage of Hits against you is Lucky
 ]],[[
 Maata's Teaching
 Karui Sceptre
-+37 to Intelligence
-42% increased Critical Strike Chance
++(30-40) to Intelligence
+(25-50)% increased Critical Strike Chance
 +2 to Level of all Minion Skill Gems
 Minions' Base Attack Critical Strike Chance is equal to the Critical Strike Chance of your Main Hand Weapon"
 ]],
