cfc1246944790a21954b24ebf39b717c8ce630df
Wires77 <Wires77@users.noreply.github.com>
2023-08-21 20:03:49 -0500

    No need for legacy mod
    Signed-off-by: Wires77 <Wires77@users.noreply.github.com>

--- src/Modules/ModParser.lua
+++ src/Modules/ModParser.lua
@@ -2132,7 +2132,7 @@ local specialModList = {
 		mod("Damage", "MORE", num, { type = "Multiplier", var = "EnduranceChargesLostRecently", limit = tonumber(limit), limitTotal = true }),
 	} end,
 	["(%d+)%% more damage if you've lost an endurance charge in the past 8 seconds"] = function(num) return { mod("Damage", "MORE", num, { type = "Condition", var = "LostEnduranceChargeInPast8Sec" })	} end,
-	["trigger level (%d+) (.+) when you attack with a non%-vaal slam? ?o?r? ?s?t?r?i?k?e? skill near an enemy"] = function(num, _, skill) return triggerExtraSkill(skill, num) end,
+	["trigger level (%d+) (.+) when you attack with a non%-vaal slam or strike skill near an enemy"] = function(num, _, skill) return triggerExtraSkill(skill, num) end,
 	-- Deadeye
 	["projectiles pierce all nearby targets"] = { flag("PierceAllTargets") },
 	["gain %+(%d+) life when you hit a bleeding enemy"] = function(num) return { mod("LifeOnHit", "BASE", num, nil, ModFlag.Hit, { type = "ActorCondition", actor = "enemy", var = "Bleeding" }) } end,
