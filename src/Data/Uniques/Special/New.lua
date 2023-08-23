35bb8c8a42ed54e9a8fd3dc38a2390e98cf953d6
LocalIdentity <localidentity2@gmail.com>
2023-08-24 03:55:32 +1000

    Fix Level requirements and add Replica Dragonfang's Flight

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -9,41 +9,29 @@ data.uniques.new = {
 Ahuana's Bite
 Sharktooth Arrow Quiver
 League: Ancestor
+LevelReq: 56
+Implicits: 1
 Gain (6–8) Life per Enemy Hit with Attacks
 Adds (30–40) to (60–70) Cold Damage to Attacks
 +(20–30)% to Cold Resistance
 Enemies Chilled by your Hits have Damage taken increased by Chill Effect
 Chill Enemies as though dealing (60–100)% more Damage
 ]],[[
-Utula's Hunger
-Majestic Plate
-League: Ancestor
-(60-100)% increased Armour
-Recover (1-3)% of Life on Kill
-(30-40)% Increased Stun and Block Recovery
-+(700-1000) to maximum Life if there are no Life Modifiers on other Equipped items
-]],[[
 Akoya's Gaze
 Regicide Mask
 League: Ancestor
+Requires Level 52, 58 Dex, 58 Int
 +100 Strength Requirement
 (20–25)% increased Warcry Speed
 Life Leech from Exerted Attacks is instant
 Non-Exerted Attacks deal no Damage
 ]],[[
-Ikiaho's Promise
-Coral Amulet
-League: Ancestor
-{tags:life}Regenerate (2–4) Life per second
-{tags:mana}Regenerate (3–5) Mana per second
-{tags:life}(30–40)% increased Life Recovery from Flasks
-{tags:mana}(15–30)% increased Mana Recovery from Flasks
-Life Flasks used while on Low Life apply Recovery Instantly
-Mana Flasks used while on Low Mana apply Recovery Instantly
-]],[[
 Bound Fate
 Cloth Belt
 League: Ancestor
+LevelReq: 16
+Implicits: 1
+(15-25)% increased Stun and Block Recovery
 {tags:jewellery_attribute}+(20-30) to Dexterity
 {tags:jewellery_attribute}+(20-30) to Intelligence
 {tags:life}+(60-80) to Maximum Life
@@ -58,6 +46,8 @@ Damage of Hits against you is Lucky
 Defiance of Destiny
 Paua Amulet
 League: Ancestor
+LevelReq: 49
+Implicits: 1
 {tags:mana}(20–30)% increased Mana Regeneration Rate
 {tags:life}+(60–80) to maximum Life
 {tags:jewellery_resistance}+(10–40)% to Fire Resistance
@@ -68,15 +58,30 @@ League: Ancestor
 Honoured Alliance
 Coral Ring
 League: Ancestor
+LevelReq: 49
+Implicits: 1
 {tags:life}+(20–30) to maximum Life
 10% chance to Trigger Summon Spirit of Akoya on Kill
 {tags:jewellery_attribute}+(10–20) to all Attributes
 {tags:mana}(30–50)% increased Mana Regeneration Rate
 {tags:life}(10–15)% of Damage taken Recouped as Life
 ]],[[
+Ikiaho's Promise
+Coral Amulet
+League: Ancestor
+LevelReq: 30
+Implicits: 1
+{tags:life}Regenerate (2–4) Life per second
+{tags:mana}Regenerate (3–5) Mana per second
+{tags:life}(30–40)% increased Life Recovery from Flasks
+{tags:mana}(15–30)% increased Mana Recovery from Flasks
+Life Flasks used while on Low Life apply Recovery Instantly
+Mana Flasks used while on Low Mana apply Recovery Instantly
+]],[[
 Kahuturoa's Certainty
 Ancient Greaves
 League: Ancestor
+Requires Level 46, 82 Str
 (100–150)% increased Armour
 5% reduced Movement Speed
 (100–200)% increased Stun and Block Recovery
@@ -85,6 +90,8 @@ Unaffected by Damaging Ailments
 Kaom's Binding
 Heavy Belt
 League: Ancestor
+LevelReq: 56
+Implicits: 1
 {tags:jewellery_attribute}+(30–40) to Strength
 {tags:jewellery_defense}+(300–500) to Armour
 Take no Burning Damage if you've stopped taking Burning Damage Recently
@@ -93,6 +100,8 @@ Nearby Enemies Convert 25% of their Physical Damage to Fire
 Kiloava's Bluster
 Ironwood Buckler
 League: Ancestor
+Requires Level 57, 137 Dex
+Implicits: 1
 3% increased Movement Speed
 (100–150)% increased Evasion Rating
 (15–25)% chance to Avoid Elemental Ailments
@@ -102,19 +111,33 @@ League: Ancestor
 Maata's Teaching
 Karui Sceptre
 League: Ancestor
+Requires Level 56, 96 Str, 96 Int
+Implicits: 1
 +(30-40) to Intelligence
 (25-50)% increased Critical Strike Chance
-+2 to Level of all Minion Skill Gems
++(1-2) to Level of all Minion Skill Gems
 Minions' Base Attack Critical Strike Chance is equal to the Critical Strike Chance of your Main Hand Weapon
 ]],[[
 Rakiata's Dance
 Engraved Greatsword
 League: Ancestor
+Requires Level 48, 91 Str, 76 Dex
+Implicits: 1
 60% increased Global Accuracy Rating
 Adds (150–200) to (300–350) Cold Damage
 Adds 1 to (550–600) Lightning Damage
 (15–20)% increased Attack Speed
 Treats Enemy Monster Elemental Resistance values as inverted
+]],[[
+Replica Dragonfang's Flight
+Onyx Amulet
+LevelReq: 56
+Implicits: 1
++(10–16) to all Attributes
++(10–15)% to all Elemental Resistances
++3 to Level of all Spark Gems
+(10–15)% increased Reservation Efficiency of Skills
+Items and Gems have (10–15)% reduced Attribute Requirements
 ]],[[
 Tawhanuku's Timing
 Moonstone Ring
@@ -125,9 +148,19 @@ League: Ancestor
 (5–10)% chance to Freeze, Shock and Ignite
 {tags:jewellery_defense,caster}Spells cause you to gain Energy Shield equal to their Upfront Cost every fifth time you Pay it
 ]],[[
+Utula's Hunger
+Majestic Plate
+League: Ancestor
+Requires Level 53, 145 Str
+(60-100)% increased Armour
+Recover (1-3)% of Life on Kill
+(30-40)% Increased Stun and Block Recovery
++(700-1000) to maximum Life if there are no Life Modifiers on other Equipped items
+]],[[
 Warrior's Tale
 Crimson Jewel
 League: Ancestor
+Limited to: 1
 100% increased effect of Tattoos in Radius
 ]]
 -- Reworked
