95f8eb1c847169fca5b61d4e07a7d45c60731266
Lily <Lilylicious@users.noreply.github.com>
2023-09-08 16:37:30 +0200

    Don't try to flip ranges that aren't ranges (#6621)

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -633,6 +633,11 @@ holding Shift will put it in the second.]])
 
 				local minA, maxA = getMinMax(modA)
 				local minB, maxB = getMinMax(modB)
+
+				if not minA or not minB or not maxA or not maxB then
+					return false
+				end
+
 				local allInts = minA == m_floor(minA) and maxA == m_floor(maxA) and minB == m_floor(minB) and maxB == m_floor(maxB) -- if the mod goes in steps that aren't 1, then the code below this doesn't work
 				if (minA and minB and maxA and maxB and allInts) then
 					if (minA < minB) then -- ascending
