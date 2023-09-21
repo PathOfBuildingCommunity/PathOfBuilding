129a9a0968ece17665037bd8ef503f2f6bf5966f
Paliak <91493239+Paliak@users.noreply.github.com>
2023-09-21 19:01:00 +0200

    FIX: only disable if item is dialla's

--- src/Classes/ImportTab.lua
+++ src/Classes/ImportTab.lua
@@ -1025,7 +1025,12 @@ function ImportTabClass:ImportSocketedItems(item, socketedItems, slotName)
 					itemSocketGroupList[groupID] = { label = "", enabled = true, gemList = { }, slot = slotName }
 				end
 				local socketGroup = itemSocketGroupList[groupID]
-				t_insert(socketGroup.gemList, gemInstance)
+				if not socketedItem.support and socketGroup.gemList[1] and socketGroup.gemList[1].support and item.title ~= "Dialla's Malefaction" then
+					-- If the first gemInstance is a support gemInstance, put the first active gemInstance before it
+					t_insert(socketGroup.gemList, 1, gemInstance)
+				else
+					t_insert(socketGroup.gemList, gemInstance)
+				end
 			end
 		end
 	end
