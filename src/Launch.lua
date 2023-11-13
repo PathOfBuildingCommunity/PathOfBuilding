69451165a4d32f7f76309429627932121be2db74
admSla <adam.slaninka@gmail.com>
2023-11-13 22:37:36 +0100

    add color changer in settings for highlighting nodes in Passive tree

--- src/Launch.lua
+++ src/Launch.lua
@@ -16,6 +16,13 @@ launch = { }
 SetMainObject(launch)
 
 function launch:OnInit()
+	-- This is the path to emmy_core.dll. The ?.dll at the end is intentional.
+	package.cpath = package.cpath .. ";C:/Users/Adam/.vscode/extensions/tangzx.emmylua-0.5.19/debugger/emmy/windows/x86/?.dll"
+	local dbg = require("emmy_core")
+	-- This port must match the Visual Studio Code configuration. Default is 9966.
+	dbg.tcpListen("localhost", 9966)
+	-- Uncomment the next line if you want Path of Building to block until the debugger is attached
+	--dbg.waitIDE()
 	self.devMode = false
 	self.installedMode = false
 	self.versionNumber = "?"
