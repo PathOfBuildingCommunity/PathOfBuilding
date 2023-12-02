351cd55491b34f4cac7ad5afe7ae4fe9118b5064
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2023-12-02 12:16:23 +1100

    Add Replica Badge of the Brotherhood 3.23

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -8,12 +8,27 @@ data.uniques.new = {
 [[
 Replica Covenant
 Spidersilk Robe
+League: Heist
+Source: Steal from a unique{Curio Display} during a Grand Heist
 Implicits: 0
 Socketed Gems are Supported by Level 29 Added Cold Damage
 +(20-30) to Intelligence
 (210-250)% increased Energy Shield
 10% increased maximum Life
 Skills gain a Base Energy Shield Cost equal to 200% of Base Mana Cost
+]],[[
+Replica Badge of the Brotherhood
+Turquoise Amulet
+League: Heist
+Source: Steal from a unique{Curio Display} during a Grand Heist
+Requires Level: 60
+Implicits: 1
+{tags:jewellery_attribute}+(16-24) to Dexterity and Intelligence
+Your Maximum Endurance Charges is equal to your Maximum Frenzy Charges
++1 to Maximum Fortification per Endurance Charge
+(7-10)% increased Cooldown Recovery of Travel Skills per Frenzy Charge
+(20-25)% chance to lose a Frenzy Charge when you use a Travel Skill
+(20-25)% chance to lose an Endurance Charge when you gain Fortification
 ]]
 
 -- Reworked
