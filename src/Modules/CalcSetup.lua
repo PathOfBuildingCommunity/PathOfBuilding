710b14f6d4fd8904a1f7548c470b49360cbce023
Sida Wang <42928828+sida-wang@users.noreply.github.com>
2023-12-06 20:01:30 +1100

    fix: exclude item provided skill groups from socket calc

--- src/Modules/CalcSetup.lua
+++ src/Modules/CalcSetup.lua
@@ -994,7 +994,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 					local socketedGems = 0
 					-- Loop through socket groups to calculate number of socketed gems
 					for _, socketGroup in pairs(env.build.skillsTab.socketGroupList) do
-						if (socketGroup.enabled and socketGroup.slot and socketGroup.slot == slotName and socketGroup.gemList) then
+						if (not socketGroup.source and socketGroup.enabled and socketGroup.slot and socketGroup.slot == slotName and socketGroup.gemList) then
 							for _, gem in pairs(socketGroup.gemList) do
 								if (gem.gemData and gem.enabled) then
 									socketedGems = socketedGems + 1
