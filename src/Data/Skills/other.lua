bdaf8de35c294e8b6fa0acd1b020b029576b6806
Paliak <91493239+Paliak@users.noreply.github.com>
2023-12-15 11:02:56 +0100

    FIX: hidden blade not using main weapon and its supports

--- src/Data/Skills/other.lua
+++ src/Data/Skills/other.lua
@@ -3358,9 +3358,11 @@ skills["HiddenBlade"] = {
 	statDescriptionScope = "skill_stat_descriptions",
 	castTime = 1,
 	fromItem = true,
+	preferredSlotName = "Weapon 1",
 	baseFlags = {
 		attack = true,
 		projectile = true,
+		forceMainHand = true,
 	},
 	stats = {
 		"skill_has_trigger_from_unique_item",
