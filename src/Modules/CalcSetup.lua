abc993ceefcf9551d58fc6976c5b3c44062e0152
Paliak <91493239+Paliak@users.noreply.github.com>
2023-12-22 00:34:26 +0100

    FIX: missing actor for extraJewelFunc list

--- src/Modules/CalcSetup.lua
+++ src/Modules/CalcSetup.lua
@@ -1088,6 +1088,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 	if not override or (override and not override.extraJewelFuncs) then
 		override = override or {}
 		override.extraJewelFuncs = new("ModList")
+		override.extraJewelFuncs.actor = env.player
 		for _, mod in ipairs(env.modDB:Tabulate("LIST", nil, "ExtraJewelFunc")) do
 			override.extraJewelFuncs:AddMod(mod.mod)
 		end
