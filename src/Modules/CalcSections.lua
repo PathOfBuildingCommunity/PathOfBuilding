d602bddb48abbc0a0f6968f2a206057bc61a2641
LocalIdentity <localidentity2@gmail.com>
2024-01-12 02:19:22 +1100

    Add colours and extra breakdowns

--- src/Modules/CalcSections.lua
+++ src/Modules/CalcSections.lua
@@ -1641,23 +1641,26 @@ return {
 	}, },
 } }
 } },
-{ 1, "Charges", 3, colorCodes.NORMAL, {
-	{ defaultCollapsed = true, label = "Charges", data = { } },
+{ 1, "Charges", 3, colorCodes.NORMAL, {{ defaultCollapsed = true, label = "Charges", data = {
+	extra = colorCodes.RAGE.."{0:output:EnduranceCharges}^7, "..colorCodes.EVASION.."{0:output:FrenzyCharges}^7, "..colorCodes.MANA.."{0:output:PowerCharges}",} },
 	{ defaultCollapsed = true, label = "Endurance", haveOutput="UseEnduranceCharges", data = {
-		{ label = "Max", { format = "{0:output:EnduranceChargesMax}", { modName = { "EnduranceChargesMax" } } }, },
-		{ label = "Current", { format = "{0:output:EnduranceCharges}", }, },
+	extra = colorCodes.RAGE.."{0:output:EnduranceCharges} ^7/ "..colorCodes.RAGE.."{0:output:EnduranceChargesMax}",
+		{ label = "Max", { format = "{0:output:EnduranceChargesMax}", { modName = { "EnduranceChargesMax", "PartyMemberMaximumEnduranceChargesEqualToYours" } } }, },
+		{ label = "Current", { format = "{0:output:EnduranceCharges}", { modName = { "EnduranceChargesMin", "HaveMaximumEnduranceCharges" } } }, },
 		{ label = "Spendable", { format = "{0:output:RemovableEnduranceCharges}", }, },
 		{ label = "Duration", { format = "{0:output:EnduranceChargesDuration}s", { modName = { "EnduranceChargesDuration", "ChargeDuration" } } }, },
 	} },
 	{ defaultCollapsed = true, label = "Frenzy", haveOutput="UseFrenzyCharges", data = {
+	extra = colorCodes.EVASION.."{0:output:FrenzyCharges} ^7/ "..colorCodes.EVASION.."{0:output:FrenzyChargesMax}",
 		{ label = "Max", { format = "{0:output:FrenzyChargesMax}", { modName = { "FrenzyChargesMax" } } }, },
-		{ label = "Current", { format = "{0:output:FrenzyCharges}", }, },
+		{ label = "Current", { format = "{0:output:FrenzyCharges}", { modName = { "FrenzyChargesMin", "HaveMaximumFrenzyCharges" } } }, },
 		{ label = "Spendable", { format = "{0:output:RemovableFrenzyCharges}", }, },
 		{ label = "Duration", { format = "{0:output:FrenzyChargesDuration}s", { modName = { "FrenzyChargesDuration", "ChargeDuration" } } }, },
 	} },
 	{ defaultCollapsed = true, label = "Power", haveOutput="UsePowerCharges", data = {
+	extra = colorCodes.MANA.."{0:output:PowerCharges} ^7/ "..colorCodes.MANA.."{0:output:PowerChargesMax}",
 		{ label = "Max", { format = "{0:output:PowerChargesMax}", { modName = { "PowerChargesMax" } } }, },
-		{ label = "Current", { format = "{0:output:PowerCharges}", }, },
+		{ label = "Current", { format = "{0:output:PowerCharges}", { modName = { "PowerChargesMin", "HaveMaximumPowerCharges" } } }, },
 		{ label = "Spendable", { format = "{0:output:RemovablePowerCharges}", }, },
 		{ label = "Duration", { format = "{0:output:PowerChargesDuration}s", { modName = { "PowerChargesDuration", "ChargeDuration" } } }, },
 	} },
