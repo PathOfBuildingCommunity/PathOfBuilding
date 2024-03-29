e0694c43c58e8a1801de5faf8b761741f6cb7c8d
Nostrademous <nostrademous@hotmail.com>
2024-03-29 14:43:07 -0400

    Drop Level Min & Max became a DropLevel list (#7515)

--- src/Export/spec.lua
+++ src/Export/spec.lua
@@ -4019,53 +4019,53 @@ return {
 			width=100
 		},
 		[25]={
-			list=false,
-			name="DropLevelMinimum",
+			list=true,
+			name="DropLevel",
 			refTo="",
 			type="Int",
 			width=100
 		},
 		[26]={
-			list=false,
-			name="DropLevelMaximum",
-			refTo="",
-			type="Int",
-			width=50
-		},
-		[27]={
-			list=false,
-			name="",
-			refTo="",
-			type="String",
-			width=300
-		},
-		[28]={
 			list=true,
 			name="MonsterMods",
 			refTo="Mods",
 			type="Key",
 			width=300
 		},
-		[29]={
+		[27]={
 			list=false,
 			name="Type",
 			refTo="EssenceType",
 			type="Key",
 			width=150
 		},
-		[30]={
+		[28]={
 			list=false,
 			name="Tier",
 			refTo="",
 			type="Int",
 			width=80
 		},
-		[31]={
+		[29]={
 			list=true,
 			name="",
 			refTo="",
 			type="Int",
 			width=120
+		},
+		[30]={
+			list=false,
+			name="",
+			refTo="Mods",
+			type="Key",
+			width=150
+		},
+		[31]={
+			list=false,
+			name="",
+			refTo="Mods",
+			type="Key",
+			width=150
 		},
 		[32]={
 			list=false,
@@ -4104,187 +4104,173 @@ return {
 		},
 		[37]={
 			list=false,
-			name="",
+			name="HelmetMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[38]={
 			list=false,
-			name="",
+			name="BodyArmourMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[39]={
 			list=false,
-			name="HelmetMod",
+			name="BootsMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[40]={
 			list=false,
-			name="BodyArmourMod",
+			name="GlovesMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[41]={
 			list=false,
-			name="BootsMod",
+			name="BowMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[42]={
 			list=false,
-			name="GlovesMod",
+			name="WandMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[43]={
 			list=false,
-			name="BowMod",
+			name="StaffMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[44]={
 			list=false,
-			name="WandMod",
+			name="TwoHandSwordMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[45]={
 			list=false,
-			name="StaffMod",
+			name="TwoHandAxeMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[46]={
 			list=false,
-			name="TwoHandSwordMod",
+			name="TwoHandMaceMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[47]={
 			list=false,
-			name="TwoHandAxeMod",
+			name="ClawMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[48]={
 			list=false,
-			name="TwoHandMaceMod",
+			name="DaggerMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[49]={
 			list=false,
-			name="ClawMod",
+			name="OneHandSwordMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[50]={
 			list=false,
-			name="DaggerMod",
+			name="ThrustingOneHandSwordMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[51]={
 			list=false,
-			name="OneHandSwordMod",
+			name="OneHandAxeMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[52]={
 			list=false,
-			name="ThrustingOneHandSwordMod",
+			name="OneHandMaceMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[53]={
 			list=false,
-			name="OneHandAxeMod",
+			name="SceptreMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[54]={
 			list=false,
-			name="OneHandMaceMod",
+			name="",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
 		[55]={
 			list=false,
-			name="SceptreMod",
-			refTo="Mods",
-			type="Key",
-			width=150
-		},
-		[56]={
-			list=false,
-			name="",
-			refTo="Mods",
-			type="Key",
-			width=150
-		},
-		[57]={
-			list=false,
 			name="",
 			refTo="Mods",
 			type="Int",
 			width=50
 		},
-		[58]={
+		[56]={
 			list=false,
 			name="BeltMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
-		[59]={
+		[57]={
 			list=false,
 			name="AmuletMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
-		[60]={
+		[58]={
 			list=false,
 			name="RingMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
-		[61]={
+		[59]={
 			list=false,
 			name="",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
-		[62]={
+		[60]={
 			list=false,
 			name="ShieldMod",
 			refTo="Mods",
 			type="Key",
 			width=150
 		},
-		[63]={
+		[61]={
 			list=false,
 			name="",
 			refTo="Mods",
