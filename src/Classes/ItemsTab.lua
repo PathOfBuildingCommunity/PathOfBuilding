fff71d50fc6cb718a58fdd2c1c72700aca4ddca3
LocalIdentity <localidentity2@gmail.com>
2024-03-30 09:40:37 +1100

    Remove unneeded check

--- src/Classes/ItemsTab.lua
+++ src/Classes/ItemsTab.lua
@@ -2640,7 +2640,7 @@ function ItemsTabClass:AddCustomModifierToDisplayItem()
 		t_insert(sourceList, { label = "Essence", sourceId = "ESSENCE" })
 		t_insert(sourceList, { label = "Veiled", sourceId = "VEILED"})
 	end
-	if (self.displayItem.rarity ~= "UNIQUE" and self.displayItem.rarity ~= "RELIC") and (self.displayItem.type == "Helmet" or self.displayItem.type == "Body Armour" or self.displayItem.type == "Gloves" or self.displayItem.type == "Boots") then
+	if self.displayItem.type == "Helmet" or self.displayItem.type == "Body Armour" or self.displayItem.type == "Gloves" or self.displayItem.type == "Boots" then
 		t_insert(sourceList, { label = "Necropolis", sourceId = "NECROPOLIS"})
 	end
 	if not self.displayItem.clusterJewel and self.displayItem.type ~= "Flask" then
