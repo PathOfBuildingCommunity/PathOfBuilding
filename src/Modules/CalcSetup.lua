c860b9097b8560c1054490583181db3e5477b9ea
Paliak <91493239+Paliak@users.noreply.github.com>
2024-04-09 21:46:03 +0200

    FIX: minor optimization. Only normalize if normal level compare fails

--- src/Modules/CalcSetup.lua
+++ src/Modules/CalcSetup.lua
@@ -1147,9 +1147,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 
 	if not accelerate.skills then
 		if env.mode == "MAIN" then
-			-- Process extra skills granted by items or tree nodes
-			local markList = wipeTable(tempTable1)
-			for _, grantedSkill in ipairs(env.grantedSkills) do
+			local function getNormalizedSkillLevel(grantedSkill)
 				-- Levels in socketGroup.gemList[1].level are normalized
 				-- grantedSkill.level is not causing group match miss which causes all things that rely on group order to fail
 				local normalizedGrantedSkill = {
@@ -1157,12 +1155,17 @@ function calcs.initEnv(build, mode, override, specEnv)
 					level = grantedSkill.level
 				}
 				calcLib.validateGemLevel(normalizedGrantedSkill)
+				return normalizedGrantedSkill.level
+			end
 
+			-- Process extra skills granted by items or tree nodes
+			local markList = wipeTable(tempTable1)
+			for _, grantedSkill in ipairs(env.grantedSkills) do
 				-- Check if a matching group already exists
 				local group
 				for index, socketGroup in pairs(build.skillsTab.socketGroupList) do
 					if socketGroup.source == grantedSkill.source and socketGroup.slot == grantedSkill.slotName then
-						if socketGroup.gemList[1] and socketGroup.gemList[1].skillId == grantedSkill.skillId and socketGroup.gemList[1].level == normalizedGrantedSkill.level then
+						if socketGroup.gemList[1] and socketGroup.gemList[1].skillId == grantedSkill.skillId and (socketGroup.gemList[1].level == grantedSkill.level or socketGroup.gemList[1].level == getNormalizedSkillLevel(grantedSkill)) then
 							group = socketGroup
 							markList[socketGroup] = true
 							break
