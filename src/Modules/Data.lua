15d0531d8a767192c046c19d9c81151fb1fef2d6
LocalIdentity <localidentity2@gmail.com>
2024-04-10 11:11:04 +1000

    Comment out Spectre skill printing to console

--- src/Modules/Data.lua
+++ src/Modules/Data.lua
@@ -954,7 +954,7 @@ data.printMissingMinionSkills = function()
 	for _, minion in pairs(data.minions) do
 		for _, skillId in ipairs(minion.skillList) do
 			if not data.skills[skillId] and not missing[skillId] then
-				ConPrintf("'%s' missing skill '%s'", minion.name, skillId)
+				--ConPrintf("'%s' missing skill '%s'", minion.name, skillId)
 				missing[skillId] = true
 			end
 		end
