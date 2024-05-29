e3b6baeb918841dc011fc7a8302ad598e7b4cde3
Paliak <91493239+Paliak@users.noreply.github.com>
2024-05-29 08:30:31 +0200

    FIX: manaforged trigger rate calculations

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -1512,7 +1512,7 @@ function calcs.offence(env, actor, activeSkill)
 
 	-- Calculate costs (may be slightly off due to rounding differences)
 	local costs = {
-		["Mana"] = { type = "Mana", upfront = true, percent = false, text = "mana", baseCost = 0, totalCost = 0, baseCostNoMult = 0, finalBaseCost = 0 },
+		["Mana"] = { type = "Mana", upfront = true, percent = false, text = "mana", baseCost = 0, baseCostRaw = 0, totalCost = 0, baseCostNoMult = 0, finalBaseCost = 0 },
 		["Life"] = { type = "Life", upfront = true, percent = false, text = "life", baseCost = 0, totalCost = 0, baseCostNoMult = 0, finalBaseCost = 0 },
 		["ES"] = { type = "ES", upfront = true, percent = false, text = "ES", baseCost = 0, totalCost = 0, baseCostNoMult = 0, finalBaseCost = 0 },
 		["Soul"] = { type = "Soul", upfront = true, percent = false, unaffectedByGenericCostMults = true, text = "soul", baseCost = 0, totalCost = 0, baseCostNoMult = 0, finalBaseCost = 0 },
@@ -1554,6 +1554,7 @@ function calcs.offence(env, actor, activeSkill)
 			val.baseCost = val.baseCost + baseCost
 			val.baseCostNoMult = val.baseCostNoMult + baseCostNoMult
 			val.finalBaseCost = (m_floor(val.baseCost * mult) + val.baseCostNoMult) + divineBlessingCorrection
+			val.baseCostRaw = val.baseCostRaw and (val.baseCost * mult + val.baseCostNoMult + divineBlessingCorrection)
 			if val.type == "Life" then
 				local manaType = resource:gsub("Life", "Mana")
 				if skillModList:Flag(skillCfg, "CostLifeInsteadOfMana") then -- Blood Magic / Lifetap
@@ -1589,32 +1590,42 @@ function calcs.offence(env, actor, activeSkill)
 			local hasCost = val.baseCost > 0 or val.totalCost > 0 or val.baseCostNoMult > 0 or val.finalBaseCost > 0
 			output[resource.."HasCost"] = hasCost
 			local costName = resource.."Cost"
+			local costNameRaw = costName.."Raw"
 			local moreType = 1
 			local moreCost = 1
 			local inc = 0
 			if not val.unaffectedByGenericCostMults then
 				output[costName] = val.finalBaseCost
+				output[costNameRaw] = val.baseCostRaw and val.baseCostRaw
 				moreType = skillModList:More(skillCfg, val.type.."Cost")
 				moreCost = skillModList:More(skillCfg, "Cost")
 				inc = skillModList:Sum("INC", skillCfg, val.type.."Cost", "Cost")
 				if inc < 0 then
 					output[costName] = m_max(0, m_ceil((1 + inc / 100) * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, (1 + inc / 100) * output[costNameRaw])
 				else
 					output[costName] = m_max(0, m_floor((1 + inc / 100) * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, (1 + inc / 100) * output[costNameRaw])
 				end
 				if moreType < 1 then
 					output[costName] = m_max(0, m_ceil(moreType * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, moreType * output[costNameRaw])
 				else
 					output[costName] = m_max(0, m_floor(moreType * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, moreType * output[costNameRaw])
 				end
 				if moreCost < 1 then
 					output[costName] = m_max(0, m_ceil(moreCost * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, moreCost * output[costNameRaw])
 				else
 					output[costName] = m_max(0, m_floor(moreCost * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, moreCost * output[costNameRaw])
 				end
 				output[costName] = m_max(0, output[costName] + val.totalCost)
+				output[costNameRaw] = output[costNameRaw] and m_max(0, output[costNameRaw] + val.totalCost)
 				if val.type == "Mana" and hybridLifeCost > 0 then -- Life/Mana Mastery
 					output[costName] = m_max(0, m_floor((1 - hybridLifeCost) * output[costName]))
+					output[costNameRaw] = output[costNameRaw] and m_max(0, (1 - hybridLifeCost) * output[costNameRaw])
 				end
 			else
 				moreType = skillModList:More(skillCfg, val.type.."Cost")
