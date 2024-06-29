0f1ea4bac73884d820989ef2439f0016f425edde
justjuangui <servicios@juacarvajal.com>
2024-06-28 20:33:43 -0500

    Trade Improvements Path, Fase 0: Enabled Download from Export lua code

--- src/Export/Launch.lua
+++ src/Export/Launch.lua
@@ -15,6 +15,24 @@ SetMainObject(launch)
 function launch:OnInit()
 	self.devMode = true
 	self.subScripts = { }
+	self.versionNumber = "?"
+	self.versionBranch = "?"
+	self.versionPlatform = "?"
+
+	local xml = require("xml")
+	local localManXML = xml.LoadXMLFile("manifest.xml") or xml.LoadXMLFile("../manifest.xml")
+	if localManXML and localManXML[1].elem == "PoBVersion" then
+		for _, node in ipairs(localManXML[1]) do
+			if type(node) == "table" then
+				if node.elem == "Version" then
+					self.versionNumber = node.attrib.number
+					self.versionBranch = node.attrib.branch
+					self.versionPlatform = node.attrib.platform
+				end
+			end
+		end
+	end
+
 	RenderInit()
 	ConPrintf("Loading main script...")
 	local errMsg
@@ -126,6 +144,12 @@ function launch:OnSubCall(func, ...)
 end
 
 function launch:OnSubError(id, errMsg)
+	if self.subScripts[id].type == "DOWNLOAD" then
+		local errMsg = PCall(self.subScripts[id].callback, nil, errMsg)
+		if errMsg then
+			self:ShowErrMsg("In download callback: %s", errMsg)
+		end
+	end
 	self.subScripts[id] = nil
 end
 
@@ -136,6 +160,11 @@ function launch:OnSubFinished(id, ...)
 			if errMsg then
 				self:ShowErrMsg("In subscript callback: %s", errMsg)
 			end
+		end
+	elseif self.subScripts[id].type == "DOWNLOAD" then
+		local errMsg = PCall(self.subScripts[id].callback, ...)
+		if errMsg then
+			self:ShowErrMsg("In download callback: %s", errMsg)
 		end
 	end
 	self.subScripts[id] = nil
@@ -195,4 +224,70 @@ function launch:DrawPopup(r, g, b, fmt, ...)
 	SetDrawColor(1, 1, 1)
 	DrawImage(nil, ox + 4, oy + 4, w - 8, h - 8)
 	DrawString(0, oy + 10, "CENTER", 20, "VAR", txt)
+end
+
+---Download the given page in the background, and calls the provided callback function when done:
+---@param url string
+---@param callback fun(response:table, errMsg:string) @ response = { header, body }
+---@param params table @ params = { header, body }
+function launch:DownloadPage(url, callback, params)
+	params = params or {}
+	local script = [[
+		local url, requestHeader, requestBody, connectionProtocol, proxyURL = ...
+		local responseHeader = ""
+		local responseBody = ""
+		ConPrintf("Downloading page at: %s", url)
+		local curl = require("lcurl.safe")
+		local easy = curl.easy()
+		if requestHeader then
+			local header = {}
+			for s in requestHeader:gmatch("[^\r\n]+") do
+				table.insert(header, s)
+			end
+			easy:setopt(curl.OPT_HTTPHEADER, header)
+		end
+		easy:setopt_url(url)
+		easy:setopt(curl.OPT_USERAGENT, "Path of Building/]]..self.versionNumber..[[")
+		easy:setopt(curl.OPT_ACCEPT_ENCODING, "")
+		if requestBody then
+			easy:setopt(curl.OPT_POST, true)
+			easy:setopt(curl.OPT_POSTFIELDS, requestBody)
+		end
+		if connectionProtocol then
+			easy:setopt(curl.OPT_IPRESOLVE, connectionProtocol)
+		end
+		if proxyURL then
+			easy:setopt(curl.OPT_PROXY, proxyURL)
+		end
+		easy:setopt_headerfunction(function(data)
+			responseHeader = responseHeader .. data
+			return true
+		end)
+		easy:setopt_writefunction(function(data)
+			responseBody = responseBody .. data
+			return true
+		end)
+		local _, error = easy:perform()
+		local code = easy:getinfo(curl.INFO_RESPONSE_CODE)
+		easy:close()
+		local errMsg
+		if error then
+			errMsg = error:msg()
+		elseif code ~= 200 then
+			errMsg = "Response code: "..code
+		elseif #responseBody == 0 then
+			errMsg = "No data returned"
+		end
+		ConPrintf("Download complete. Status: %s", errMsg or "OK")
+		return responseHeader, responseBody, errMsg
+	]]
+	local id = LaunchSubScript(script, "", "ConPrintf", url, params.header, params.body, self.connectionProtocol, self.proxyURL)
+	if id then
+		self.subScripts[id] = {
+			type = "DOWNLOAD",
+			callback = function(responseHeader, responseBody, errMsg)
+				callback({header=responseHeader, body=responseBody}, errMsg)
+			end
+		}
+	end
 end
