81ebc34b4a528e93ad0ba1ec67977359894433f8
LocalIdentity <localidentity2@gmail.com>
2024-07-21 23:59:16 +1000

    Update tincture bases

--- src/Data/Bases/tincture.lua
+++ src/Data/Bases/tincture.lua
@@ -5,63 +5,72 @@ local itemBases = ...
 itemBases["Ironwood Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "Damaging Hits with Melee Weapons always Stun Enemies that are on Full Life",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "Damaging Hits with Melee Weapons always Stun Enemies that are on Full Life" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Ashbark Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "(20-30)% Chance to Ignite with Melee Weapons\n(80-100)% Increased Damage with Ignite From Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "(20-30)% Chance to Ignite with Melee Weapons", "(80-100)% Increased Damage with Ignite From Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Fulgurite Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "(20-30)% Chance to Shock with Melee Weapons\n(35-40)% Increased Effect of Shock from Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "(20-30)% Chance to Shock with Melee Weapons", "(35-40)% Increased Effect of Shock from Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Poisonberry Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "(20-30)% Chance to Poison with Melee Weapons\n(80-100)% Increased Damage with Poison From Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "(20-30)% Chance to Poison with Melee Weapons", "(80-100)% Increased Damage with Poison From Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Rosethorn Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "+(100-150)% Increased Critical Strike Chance with Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "+(100-150)% Increased Critical Strike Chance with Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Oakbranch Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "Culling Strike with Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "Culling Strike with Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Borealwood Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "(20-30)% Chance to Freeze with Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "(20-30)% Chance to Freeze with Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Blood Sap Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "(20-30)% Chance to Bleed with Melee Weapons\n(80-100)% Increased Damage with Bleed From Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "(20-30)% Chance to Bleed with Melee Weapons", "(80-100)% Increased Damage with Bleed From Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
 itemBases["Prismatic Tincture"] = {
 	type = "Tincture",
 	tags = { tincture = true, default = true, },
+	implicit = "(80-100)% Increased Elemental Damage with Melee Weapons",
 	implicitModTypes = { },
-	tincture = { manaBurn = 0.6, cooldown = 7, buff = { "(80-100)% Increased Elemental Damage with Melee Weapons" }, },
+	tincture = { manaBurn = 0.6, cooldown = 7 },
 	req = { },
 }
