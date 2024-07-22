88ab950b49f2f56a10fadb41eae627e589e39069
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2024-07-23 05:36:08 +1000

    Fix Power Siphon not benefiting from Spell Damage (#7802)
    The mod was not in the stat map. Moved it to the SkillStatMap file as it's used on a bunch of gems now
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Data/SkillStatMap.lua
+++ src/Data/SkillStatMap.lua
@@ -839,6 +839,10 @@ return {
 	flag("MinionDamageAppliesToPlayer"),
 	mod("ImprovedMinionDamageAppliesToPlayer", "MAX", nil)
 },
+["active_skill_additive_spell_damage_modifiers_apply_to_attack_damage_at_%_value"] = {
+	flag("SpellDamageAppliesToAttacks"),
+	mod("ImprovedSpellDamageAppliesToAttacks", "MAX", nil),
+},
 ["active_skill_main_hand_weapon_damage_+%_final"] = {
 	mod("Damage", "MORE", nil, 0, 0, { type = "Condition", var = "MainHandAttack" }),
 },
