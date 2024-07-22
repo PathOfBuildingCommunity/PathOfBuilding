SrArkadiiz/SrArkadiuzz

initial Implementation of enemy damage over time skill type

src/Modules/CalcSections.lua
@@ -2034,103 +2034,6 @@ return {
 			{ breakdown = "ChaosMaximumHitTaken" }, 
 		},
 	}
-} }, { defaultCollapsed = false, label = "Dots and Degens", data = {
-	colWidth = 114,
-	{
-		{ format = colorCodes.PHYS.."Physical:" },
-		{ format = colorCodes.LIGHTNING.."Lightning:" },
-		{ format = colorCodes.COLD.."Cold:" },
-		{ format = colorCodes.FIRE.."Fire:" },
-		{ format = colorCodes.CHAOS.."Chaos:" },
-	},
-	{ label = "DoT taken",
-		{ format = "x {2:output:PhysicalTakenDotMult}", 
-			{ breakdown = "PhysicalTakenDotMult" }, 
-			{ modName = { "DamageTaken", "DamageTakenOverTime", "PhysicalDamageTaken", "PhysicalDamageTakenOverTime" } } 
-		},
-		{ format = "x {2:output:LightningTakenDotMult}",
-			{ breakdown = "LightningTakenDotMult" }, 
-			{ modName = { "DamageTaken", "DamageTakenOverTime", "LightningDamageTaken", "LightningDamageTakenOverTime", "ElementalDamageTaken", "ElementalDamageTakenOverTime", "SelfIgnoreLightningResistance" } }
-		},
-		{ format = "x {2:output:ColdTakenDotMult}",
-			{ breakdown = "ColdTakenDotMult" }, 
-			{ modName = { "DamageTaken", "DamageTakenOverTime", "ColdDamageTaken", "ColdDamageTakenOverTime", "ElementalDamageTaken", "ElementalDamageTakenOverTime", "SelfIgnoreColdResistance" } }
-		},
-		{ format = "x {2:output:FireTakenDotMult}", 
-			{ breakdown = "FireTakenDotMult" }, 
-			{ modName = { "DamageTaken", "DamageTakenOverTime", "FireDamageTaken", "FireDamageTakenOverTime", "ElementalDamageTaken", "ElementalDamageTakenOverTime", "SelfIgnoreFireResistance" }  } 
-		},
-		{ format = "x {2:output:ChaosTakenDotMult}",
-			{ breakdown = "ChaosTakenDotMult" }, 
-			{ modName = { "DamageTaken", "DamageTakenOverTime", "ChaosDamageTaken", "ChaosDamageTakenOverTime", "SelfIgnoreChaosResistance" } }
-		},
-	},
-	{ label = "Total Pool",
-		{ format = "{0:output:PhysicalTotalPool}", 
-			{ breakdown = "PhysicalTotalPool" },			
-		},
-		{ format = "{0:output:LightningTotalPool}",
-			{ breakdown = "LightningTotalPool" }, 	
-		},
-		{ format = "{0:output:ColdTotalPool}",
-			{ breakdown = "ColdTotalPool" },
-		},
-		{ format = "{0:output:FireTotalPool}", 
-			{ breakdown = "FireTotalPool" }, 
-		},
-		{ format = "{0:output:ChaosTotalPool}",
-			{ breakdown = "ChaosTotalPool" }, 
-		},
-	}, 
-	{ label = "Effective DoT Pool",
-		{ format = "{0:output:PhysicalDotEHP}", 
-			{ breakdown = "PhysicalDotEHP" }, 
-		},
-		{ format = "{0:output:LightningDotEHP}",
-			{ breakdown = "LightningDotEHP" }, 
-		},
-		{ format = "{0:output:ColdDotEHP}",
-			{ breakdown = "ColdDotEHP" }, 
-		},
-		{ format = "{0:output:FireDotEHP}", 
-			{ breakdown = "FireDotEHP" }, 
-		},
-		{ format = "{0:output:ChaosDotEHP}",
-			{ breakdown = "ChaosDotEHP" }, 
-		},
-	},
-	{ label = "Degens", haveOutput = "TotalDegen",
-		{ format = "{0:output:PhysicalDegen}", 
-			{ breakdown = "PhysicalDegen" }, 
-			{ modName = "PhysicalDegen", }
-		},
-		{ format = "{0:output:LightningDegen}",
-			{ breakdown = "LightningDegen" }, 
-			{ modName = "LightningDegen", }
-		},
-		{ format = "{0:output:ColdDegen}",
-			{ breakdown = "ColdDegen" }, 
-			{ modName = "ColdDegen", }
-		},
-		{ format = "{0:output:FireDegen}", 
-			{ breakdown = "FireDegen" }, 
-			{ modName = "FireDegen", }
-		},
-		{ format = "{0:output:ChaosDegen}",
-			{ breakdown = "ChaosDegen" }, 
-			{ modName = "ChaosDegen", }
-		},
-	},
-	{ label = "Total Degen", haveOutput = "TotalDegen", { format = "{1:output:TotalDegen}", 
-		{ breakdown = "TotalDegen" },
-		{ label = "Sources", modName = { "PhysicalDegen", "FireDegen", "ColdDegen", "LightningDegen", "ChaosDegen" }, modType = "BASE" },
-	}, },
-	{ label = "Total Net Recovery", haveOutput = "TotalNetRegen", { format = "{1:output:TotalNetRegen}",
-		{ breakdown = "TotalNetRegen" },
-	}, },
-	{ label = "Net Life Recovery", color = colorCodes.LIFE, haveOutput = "NetLifeRegen", { format = "{1:output:NetLifeRegen}", { breakdown = "NetLifeRegen" }, }, },
-	{ label = "Net Mana Recovery", color = colorCodes.MANA, haveOutput = "NetManaRegen", { format = "{1:output:NetManaRegen}", { breakdown = "NetManaRegen" }, }, },
-	{ label = "Net ES Recovery", color = colorCodes.ES, haveOutput = "NetEnergyShieldRegen", { format = "{1:output:NetEnergyShieldRegen}", { breakdown = "NetEnergyShieldRegen" }, }, }
 } }, { defaultCollapsed = true, label = "Recoup and Hit Taken Over Time", data = {
 	colWidth = 190,
 	extra = colorCodes.LIFE.."{0:output:netLifeRecoupAndLossLostOverTimeAvg}, "..colorCodes.MANA.."{0:output:ManaRecoupRecoveryAvg}, "..colorCodes.ES.."{0:output:EnergyShieldRecoupRecoveryAvg}",
@@ -2201,6 +2104,132 @@ return {
 			{ breakdown = "netLifeRecoupAndLossLostOverTimeAvg" },
 		},
 	},
+} }, { defaultCollapsed = false, label = "Dots and Build Degens", data = {
+	colWidth = 114,
+	{
+		{ format = colorCodes.PHYS.."Physical:" },
+		{ format = colorCodes.LIGHTNING.."Lightning:" },
+		{ format = colorCodes.COLD.."Cold:" },
+		{ format = colorCodes.FIRE.."Fire:" },
+		{ format = colorCodes.CHAOS.."Chaos:" },
+	},
+	{ label = "DoT taken",
+		{ format = "x {2:output:PhysicalTakenDotMult}", 
+			{ breakdown = "PhysicalTakenDotMult" }, 
+			{ modName = { "DamageTaken", "DamageTakenOverTime", "PhysicalDamageTaken", "PhysicalDamageTakenOverTime" } } 
+		},
+		{ format = "x {2:output:LightningTakenDotMult}",
+			{ breakdown = "LightningTakenDotMult" }, 
+			{ modName = { "DamageTaken", "DamageTakenOverTime", "LightningDamageTaken", "LightningDamageTakenOverTime", "ElementalDamageTaken", "ElementalDamageTakenOverTime", "SelfIgnoreLightningResistance" } }
+		},
+		{ format = "x {2:output:ColdTakenDotMult}",
+			{ breakdown = "ColdTakenDotMult" }, 
+			{ modName = { "DamageTaken", "DamageTakenOverTime", "ColdDamageTaken", "ColdDamageTakenOverTime", "ElementalDamageTaken", "ElementalDamageTakenOverTime", "SelfIgnoreColdResistance" } }
+		},
+		{ format = "x {2:output:FireTakenDotMult}", 
+			{ breakdown = "FireTakenDotMult" }, 
+			{ modName = { "DamageTaken", "DamageTakenOverTime", "FireDamageTaken", "FireDamageTakenOverTime", "ElementalDamageTaken", "ElementalDamageTakenOverTime", "SelfIgnoreFireResistance" }  } 
+		},
+		{ format = "x {2:output:ChaosTakenDotMult}",
+			{ breakdown = "ChaosTakenDotMult" }, 
+			{ modName = { "DamageTaken", "DamageTakenOverTime", "ChaosDamageTaken", "ChaosDamageTakenOverTime", "SelfIgnoreChaosResistance" } }
+		},
+	},
+	{ label = "Total Pool",
+		{ format = "{0:output:PhysicalTotalPool}", 
+			{ breakdown = "PhysicalTotalPool" },			
+		},
+		{ format = "{0:output:LightningTotalPool}",
+			{ breakdown = "LightningTotalPool" }, 	
+		},
+		{ format = "{0:output:ColdTotalPool}",
+			{ breakdown = "ColdTotalPool" },
+		},
+		{ format = "{0:output:FireTotalPool}", 
+			{ breakdown = "FireTotalPool" }, 
+		},
+		{ format = "{0:output:ChaosTotalPool}",
+			{ breakdown = "ChaosTotalPool" }, 
+		},
+	}, 
+	{ label = "Effective DoT Pool",
+		{ format = "{0:output:PhysicalDotEHP}", 
+			{ breakdown = "PhysicalDotEHP" }, 
+		},
+		{ format = "{0:output:LightningDotEHP}",
+			{ breakdown = "LightningDotEHP" }, 
+		},
+		{ format = "{0:output:ColdDotEHP}",
+			{ breakdown = "ColdDotEHP" }, 
+		},
+		{ format = "{0:output:FireDotEHP}", 
+			{ breakdown = "FireDotEHP" }, 
+		},
+		{ format = "{0:output:ChaosDotEHP}",
+			{ breakdown = "ChaosDotEHP" }, 
+		},
+	},
+	{ label = "Degens", haveOutput = "TotalBuildDegen",
+		{ format = "{0:output:PhysicalBuildDegen}", 
+			{ breakdown = "PhysicalBuildDegen" }, 
+			{ modName = "PhysicalDegen", }
+		},
+		{ format = "{0:output:LightningBuildDegen}",
+			{ breakdown = "LightningBuildDegen" }, 
+			{ modName = "LightningDegen", }
+		},
+		{ format = "{0:output:ColdBuildDegen}",
+			{ breakdown = "ColdBuildDegen" }, 
+			{ modName = "ColdDegen", }
+		},
+		{ format = "{0:output:FireBuildDegen}", 
+			{ breakdown = "FireBuildDegen" }, 
+			{ modName = "FireDegen", }
+		},
+		{ format = "{0:output:ChaosBuildDegen}",
+			{ breakdown = "ChaosBuildDegen" }, 
+			{ modName = "ChaosDegen", }
+		},
+	},
+	{ label = "Total Degen", haveOutput = "TotalBuildDegen", { format = "{1:output:TotalBuildDegen}", 
+		{ breakdown = "TotalBuildDegen" },
+		{ label = "Sources", modName = { "PhysicalDegen", "FireDegen", "ColdDegen", "LightningDegen", "ChaosDegen" }, modType = "BASE" },
+	}, },
+	{ label = "Total Net Recovery", haveOutput = "TotalNetRegen", { format = "{1:output:TotalNetRegen}",
+		{ breakdown = "TotalNetRegen" },
+	}, },
+	{ label = "Net Life Recovery", color = colorCodes.LIFE, haveOutput = "NetLifeRegen", { format = "{1:output:NetLifeRegen}", { breakdown = "NetLifeRegen" }, }, },
+	{ label = "Net Mana Recovery", color = colorCodes.MANA, haveOutput = "NetManaRegen", { format = "{1:output:NetManaRegen}", { breakdown = "NetManaRegen" }, }, },
+	{ label = "Net ES Recovery", color = colorCodes.ES, haveOutput = "NetEnergyShieldRegen", { format = "{1:output:NetEnergyShieldRegen}", { breakdown = "NetEnergyShieldRegen" }, }, }
+} }, { defaultCollapsed = true, label = "Enemy Degens", data = {
+	colWidth = 114,
+	{
+		{ format = colorCodes.PHYS.."Physical:" },
+		{ format = colorCodes.LIGHTNING.."Lightning:" },
+		{ format = colorCodes.COLD.."Cold:" },
+		{ format = colorCodes.FIRE.."Fire:" },
+		{ format = colorCodes.CHAOS.."Chaos:" },
+	},
+	{ label = "Degens", haveOutput = "TotalDegen",
+		{ format = "{0:output:PhysicalEnemyDegen}", 
+			{ breakdown = "PhysicalEnemyDegen" }, 
+		},
+		{ format = "{0:output:LightningEnemyDegen}",
+			{ breakdown = "LightningEnemyDegen" }, 
+		},
+		{ format = "{0:output:ColdEnemyDegen}",
+			{ breakdown = "ColdEnemyDegen" }, 
+		},
+		{ format = "{0:output:FireEnemyDegen}", 
+			{ breakdown = "FireEnemyDegen" }, 
+		},
+		{ format = "{0:output:ChaosEnemyDegen}",
+			{ breakdown = "ChaosEnemyDegen" }, 
+		},
+	},
+	{ label = "Total Degen", haveOutput = "TotalDegen", { format = "{1:output:TotalDegen}", 
+		{ breakdown = "TotalDegen" },
+	}, },
 } }
 } },
 }

PathOfBuildingCommunity/PathOfBuilding

src/Modules/CalcSections.lua
@@ -2155,7 +2155,77 @@ return {
 			{ breakdown = "ChaosMaximumHitTaken" }, 
 		},
 	}
-} }, { defaultCollapsed = false, label = "Dots and Degens", data = {
+} }, { defaultCollapsed = true, label = "Recoup and Hit Taken Over Time", data = {
+	colWidth = 190,
+	extra = colorCodes.LIFE.."{0:output:netLifeRecoupAndLossLostOverTimeAvg}, "..colorCodes.MANA.."{0:output:ManaRecoupRecoveryAvg}, "..colorCodes.ES.."{0:output:EnergyShieldRecoupRecoveryAvg}",
+	{
+		{ format = colorCodes.LIFE.."Life:" },
+		{ format = colorCodes.MANA.."Mana:" },
+		{ format = colorCodes.ES.."Energy Shield:" },
+	},
+	{ label = "Recoup Max.", haveOutput = "anyRecoup", 
+		{ format = "{0:output:LifeRecoupRecoveryMax}",
+			{ breakdown = "LifeRecoupRecoveryMax" },
+			{ label = "Sources", modName = { "LifeRecoup", "PhysicalLifeRecoup", "LightningLifeRecoup", "ColdLifeRecoup", "FireLifeRecoup", "ChaosLifeRecoup" } },
+			{ label = "Recovery modifiers", modName = "LifeRecoveryRate" },
+			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		},
+		{ format = "{0:output:ManaRecoupRecoveryMax}",
+			{ breakdown = "ManaRecoupRecoveryMax" },
+			{ label = "Sources", modName = "ManaRecoup" },
+			{ label = "Recovery modifiers", modName = "ManaRecoveryRate" },
+			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		},
+		{ format = "{0:output:EnergyShieldRecoupRecoveryMax}",
+			{ breakdown = "EnergyShieldRecoupRecoveryMax" },
+			{ label = "Sources", modName = { "EnergyShieldRecoup", "ElementalEnergyShieldRecoup" } },
+			{ label = "Recovery modifiers", modName = "EnergyShieldRecoveryRate" },
+			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		},
+	},
+	{ label = "Recoup Avg.", haveOutput = "anyRecoup", 
+		{ format = "{0:output:LifeRecoupRecoveryAvg}",
+			{ breakdown = "LifeRecoupRecoveryAvg" },
+			{ label = "Sources", modName = { "LifeRecoup", "PhysicalLifeRecoup", "LightningLifeRecoup", "ColdLifeRecoup", "FireLifeRecoup", "ChaosLifeRecoup" } },
+			{ label = "Recovery modifiers", modName = "LifeRecoveryRate" },
+			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		},
+		{ format = "{0:output:ManaRecoupRecoveryAvg}",
+			{ breakdown = "ManaRecoupRecoveryAvg" },
+			{ label = "Sources", modName = "ManaRecoup" },
+			{ label = "Recovery modifiers", modName = "ManaRecoveryRate" },
+			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		},
+		{ format = "{0:output:EnergyShieldRecoupRecoveryAvg}",
+			{ breakdown = "EnergyShieldRecoupRecoveryAvg" },
+			{ label = "Sources", modName = { "EnergyShieldRecoup", "ElementalEnergyShieldRecoup" } },
+			{ label = "Recovery modifiers", modName = "EnergyShieldRecoveryRate" },
+			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		},
+	},
+	{ label = "Hit Over Time Max.", haveOutput = "preventedLifeLossTotal", 
+		{ format = "{0:output:LifeLossLostMax}",
+			{ breakdown = "LifeLossLostMax" },
+			{ modName = { "LifeLossPrevented", "LifeLossBelowHalfPrevented", "LifeLossBelowHalfLost" } },
+		},
+	},
+	{ label = "Hit Over Time Avg.", haveOutput = "preventedLifeLossTotal", 
+		{ format = "{0:output:LifeLossLostAvg}",
+			{ breakdown = "LifeLossLostAvg" },
+			{ modName = { "LifeLossPrevented", "LifeLossBelowHalfPrevented", "LifeLossBelowHalfLost" } },
+		},
+	},
+	{ label = "Net Max.", haveOutput = "showNetRecoup", 
+		{ format = "{0:output:netLifeRecoupAndLossLostOverTimeMax}",
+			{ breakdown = "netLifeRecoupAndLossLostOverTimeMax" },
+		},
+	},
+	{ label = "Net Avg.", haveOutput = "showNetRecoup", 
+		{ format = "{0:output:netLifeRecoupAndLossLostOverTimeAvg}",
+			{ breakdown = "netLifeRecoupAndLossLostOverTimeAvg" },
+		},
+	},
+} }, { defaultCollapsed = false, label = "Dots and Build Degens", data = {
 	colWidth = 114,
 	{
 		{ format = colorCodes.PHYS.."Physical:" },
@@ -2285,30 +2355,30 @@ return {
 			{ breakdown = "ChaosDotEHP" }, 
 		},
 	},
-	{ label = "Degens", haveOutput = "TotalDegen",
-		{ format = "{0:output:PhysicalDegen}", 
-			{ breakdown = "PhysicalDegen" }, 
+	{ label = "Degens", haveOutput = "TotalBuildDegen",
+		{ format = "{0:output:PhysicalBuildDegen}", 
+			{ breakdown = "PhysicalBuildDegen" }, 
 			{ modName = "PhysicalDegen", }
 		},
-		{ format = "{0:output:LightningDegen}",
-			{ breakdown = "LightningDegen" }, 
+		{ format = "{0:output:LightningBuildDegen}",
+			{ breakdown = "LightningBuildDegen" }, 
 			{ modName = "LightningDegen", }
 		},
-		{ format = "{0:output:ColdDegen}",
-			{ breakdown = "ColdDegen" }, 
+		{ format = "{0:output:ColdBuildDegen}",
+			{ breakdown = "ColdBuildDegen" }, 
 			{ modName = "ColdDegen", }
 		},
-		{ format = "{0:output:FireDegen}", 
-			{ breakdown = "FireDegen" }, 
+		{ format = "{0:output:FireBuildDegen}", 
+			{ breakdown = "FireBuildDegen" }, 
 			{ modName = "FireDegen", }
 		},
-		{ format = "{0:output:ChaosDegen}",
-			{ breakdown = "ChaosDegen" }, 
+		{ format = "{0:output:ChaosBuildDegen}",
+			{ breakdown = "ChaosBuildDegen" }, 
 			{ modName = "ChaosDegen", }
 		},
 	},
-	{ label = "Total Degen", haveOutput = "TotalDegen", { format = "{1:output:TotalDegen}", 
-		{ breakdown = "TotalDegen" },
+	{ label = "Total Degen", haveOutput = "TotalBuildDegen", { format = "{1:output:TotalBuildDegen}", 
+		{ breakdown = "TotalBuildDegen" },
 		{ label = "Sources", modName = { "PhysicalDegen", "FireDegen", "ColdDegen", "LightningDegen", "ChaosDegen" }, modType = "BASE" },
 	}, },
 	{ label = "Total Net Recovery", haveOutput = "TotalNetRegen", { format = "{1:output:TotalNetRegen}",
@@ -2317,76 +2387,35 @@ return {
 	{ label = "Net Life Recovery", color = colorCodes.LIFE, haveOutput = "NetLifeRegen", { format = "{1:output:NetLifeRegen}", { breakdown = "NetLifeRegen" }, }, },
 	{ label = "Net Mana Recovery", color = colorCodes.MANA, haveOutput = "NetManaRegen", { format = "{1:output:NetManaRegen}", { breakdown = "NetManaRegen" }, }, },
 	{ label = "Net ES Recovery", color = colorCodes.ES, haveOutput = "NetEnergyShieldRegen", { format = "{1:output:NetEnergyShieldRegen}", { breakdown = "NetEnergyShieldRegen" }, }, }
-} }, { defaultCollapsed = true, label = "Recoup and Hit Taken Over Time", data = {
-	colWidth = 190,
-	extra = colorCodes.LIFE.."{0:output:netLifeRecoupAndLossLostOverTimeAvg}, "..colorCodes.MANA.."{0:output:ManaRecoupRecoveryAvg}, "..colorCodes.ES.."{0:output:EnergyShieldRecoupRecoveryAvg}",
+} }, { defaultCollapsed = true, label = "Enemy Degens", data = {
+	colWidth = 114,
 	{
-		{ format = colorCodes.LIFE.."Life:" },
-		{ format = colorCodes.MANA.."Mana:" },
-		{ format = colorCodes.ES.."Energy Shield:" },
+		{ format = colorCodes.PHYS.."Physical:" },
+		{ format = colorCodes.LIGHTNING.."Lightning:" },
+		{ format = colorCodes.COLD.."Cold:" },
+		{ format = colorCodes.FIRE.."Fire:" },
+		{ format = colorCodes.CHAOS.."Chaos:" },
 	},
-	{ label = "Recoup Max.", haveOutput = "anyRecoup", 
-		{ format = "{0:output:LifeRecoupRecoveryMax}",
-			{ breakdown = "LifeRecoupRecoveryMax" },
-			{ label = "Sources", modName = { "LifeRecoup", "PhysicalLifeRecoup", "LightningLifeRecoup", "ColdLifeRecoup", "FireLifeRecoup", "ChaosLifeRecoup" } },
-			{ label = "Recovery modifiers", modName = "LifeRecoveryRate" },
-			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+	{ label = "Degens", haveOutput = "TotalDegen",
+		{ format = "{0:output:PhysicalEnemyDegen}", 
+			{ breakdown = "PhysicalEnemyDegen" }, 
 		},
-		{ format = "{0:output:ManaRecoupRecoveryMax}",
-			{ breakdown = "ManaRecoupRecoveryMax" },
-			{ label = "Sources", modName = "ManaRecoup" },
-			{ label = "Recovery modifiers", modName = "ManaRecoveryRate" },
-			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		{ format = "{0:output:LightningEnemyDegen}",
+			{ breakdown = "LightningEnemyDegen" }, 
 		},
-		{ format = "{0:output:EnergyShieldRecoupRecoveryMax}",
-			{ breakdown = "EnergyShieldRecoupRecoveryMax" },
-			{ label = "Sources", modName = { "EnergyShieldRecoup", "ElementalEnergyShieldRecoup" } },
-			{ label = "Recovery modifiers", modName = "EnergyShieldRecoveryRate" },
-			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		{ format = "{0:output:ColdEnemyDegen}",
+			{ breakdown = "ColdEnemyDegen" }, 
 		},
-	},
-	{ label = "Recoup Avg.", haveOutput = "anyRecoup", 
-		{ format = "{0:output:LifeRecoupRecoveryAvg}",
-			{ breakdown = "LifeRecoupRecoveryAvg" },
-			{ label = "Sources", modName = { "LifeRecoup", "PhysicalLifeRecoup", "LightningLifeRecoup", "ColdLifeRecoup", "FireLifeRecoup", "ChaosLifeRecoup" } },
-			{ label = "Recovery modifiers", modName = "LifeRecoveryRate" },
-			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		{ format = "{0:output:FireEnemyDegen}", 
+			{ breakdown = "FireEnemyDegen" }, 
 		},
-		{ format = "{0:output:ManaRecoupRecoveryAvg}",
-			{ breakdown = "ManaRecoupRecoveryAvg" },
-			{ label = "Sources", modName = "ManaRecoup" },
-			{ label = "Recovery modifiers", modName = "ManaRecoveryRate" },
-			{ label = "Faster Recoup", modName = "3SecondRecoup" },
-		},
-		{ format = "{0:output:EnergyShieldRecoupRecoveryAvg}",
-			{ breakdown = "EnergyShieldRecoupRecoveryAvg" },
-			{ label = "Sources", modName = { "EnergyShieldRecoup", "ElementalEnergyShieldRecoup" } },
-			{ label = "Recovery modifiers", modName = "EnergyShieldRecoveryRate" },
-			{ label = "Faster Recoup", modName = "3SecondRecoup" },
+		{ format = "{0:output:ChaosEnemyDegen}",
+			{ breakdown = "ChaosEnemyDegen" }, 
 		},
 	},
-	{ label = "Hit Over Time Max.", haveOutput = "preventedLifeLossTotal", 
-		{ format = "{0:output:LifeLossLostMax}",
-			{ breakdown = "LifeLossLostMax" },
-			{ modName = { "LifeLossPrevented", "LifeLossBelowHalfPrevented", "LifeLossBelowHalfLost" } },
-		},
-	},
-	{ label = "Hit Over Time Avg.", haveOutput = "preventedLifeLossTotal", 
-		{ format = "{0:output:LifeLossLostAvg}",
-			{ breakdown = "LifeLossLostAvg" },
-			{ modName = { "LifeLossPrevented", "LifeLossBelowHalfPrevented", "LifeLossBelowHalfLost" } },
-		},
-	},
-	{ label = "Net Max.", haveOutput = "showNetRecoup", 
-		{ format = "{0:output:netLifeRecoupAndLossLostOverTimeMax}",
-			{ breakdown = "netLifeRecoupAndLossLostOverTimeMax" },
-		},
-	},
-	{ label = "Net Avg.", haveOutput = "showNetRecoup", 
-		{ format = "{0:output:netLifeRecoupAndLossLostOverTimeAvg}",
-			{ breakdown = "netLifeRecoupAndLossLostOverTimeAvg" },
-		},
-	},
+	{ label = "Total Degen", haveOutput = "TotalDegen", { format = "{1:output:TotalDegen}", 
+		{ breakdown = "TotalDegen" },
+	}, },
 } }
 } },
 }