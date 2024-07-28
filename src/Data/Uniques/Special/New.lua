e3e92bfc6a2bcb1940005782dcfa99570a74fb90
Nostrademous <nostrademous@hotmail.com>
2024-07-28 10:03:32 -0400

    New Uniques

--- src/Data/Uniques/Special/New.lua
+++ src/Data/Uniques/Special/New.lua
@@ -98,7 +98,80 @@ Implicits: 1
 {tags:jewellery_resistance}+21% to Fire Resistance
 {tags:jewellery_resistance}+21% to Cold Resistance
 Gain Ward Instead of 50% of Armour and Evasion Rating From Equipped Body Armour
-]]
-
+]],[[
+Serle's Masterwork
+Phantom Mace
+League: Settlers of Kalguur
+Requires Level 53, 170 Str
+Implicits: 1
+10% reduced Enemy Stun Threshold
++(30-40) to Strength 
++(30-40) to Dexterity
+(150-250)% increased Physical Damage
++(400-500) to Accuracy Rating
+Can have 2 additional Runesmithing Enchantments
+Can be Enchanted by a Kalguuran Runesmith
+]],[[
+Kaom's Command
+Siege Helmet
+League: Settlers of Kalguur
+Requires Level 48, 101 Str
++(60-80) to maximum Life
+(25-35)% increased Warcry Speed
+Nearby corpses Explode when you Warcry, dealing (5-10)% of their Life as Physical Damage
+Warcry Skills have (25-35)% increased Area of Effect
+]],[[
+Grasphing Nightshade
+Sporebloom Tincture
+League: Settlers of Kalguur
+Requires Level 52
+Implicits: 2
+25% chance to Blind Enemies on Hit with Melee Weapons
+(25-35)% increased Effect of Blind from Melee Weapons
+Melee Weapon Hits Inflict (2-3) Withered Debuffs for 2 seconds
+(20-30)% chance to inflict a Grasping Vine on Melee Weapon Hit
+]],[[
+Wildfire Phloem
+Ashbark Tincture
+League: Settlers of Kalguur
+Requires Level 32
+Implicits: 2
+25% chance to Ignite with Melee Weapons
+(60-90)% increased Damage with Ignite from Melee Weapons
+-1 Fire Damage taken from Hits per Mana Burn
+(15-25)% chance to refresh Ignite Duration on Melee Weapon Hit
+Cover Full Life Enemies in Ash for (4-10) seconds on Melee Weapon hit
+]],[[
+Mightblood Ire
+Ironwood Tincture
+League: Settlers of Kalguur
+Requires Level 18
+Implicits: 2
+40% reduced Enemy Stun Threshold with Melee Weapons
+(15-25)% increased Stun Duration with Melee Weapons
+Melee Strike Skills deal Splash Damage to surrounding targets
+(25-15)% reduced Mana Burn rate
+]],[[
+Sap of the Seasons
+Prismatic Tincture
+League: Settlers of Kalguur
+Requires Level 18
+Implicits: 1
+(70-100)% increased Elemental Damage with Melee Weapons
+Melee Weapon Damage Penetrates 1% Elemental Resistance per Mana Burn
+(20-40)% increased Cooldown Recovery Rate
+(35--35)% increased Mana Burn rate
+]],[[
+The Battle Within
+Oakbranch Tincture
+League: Settlers of Kalguur
+Requires Level 18
+Implicits: 1
+Gain 3 Rage on Melee Weapon Hit
+Does not inflict Mana Burn over time
+Inflicts Mana Burn on you when you Hit an Enemy with a Melee Weapon
+(1-5)% increased Rarity of Items found per Mana Burn, up to a maximum of 100%
+Melee Weapon Attacks have Culling Strik
+]],
 
 }
