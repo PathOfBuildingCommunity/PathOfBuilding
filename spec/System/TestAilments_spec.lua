684bd3a6d5b1757ac7b80d090a56d260a35d43e8
Nighty <Nightblade@users.noreply.github.com>
2024-07-31 14:07:24 +1000

    indent space -> tab (#8058)

--- spec/System/TestAilments_spec.lua
+++ spec/System/TestAilments_spec.lua
@@ -1,39 +1,39 @@
 describe("TestAilments", function()
-    before_each(function()
-        newBuild()
-    end)
+	before_each(function()
+		newBuild()
+	end)
 
-    teardown(function()
-        -- newBuild() takes care of resetting everything in setup()
-    end)
+	teardown(function()
+		-- newBuild() takes care of resetting everything in setup()
+	end)
 
-    it("maximum shock value", function()
-        -- Shock Nova
-        build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nShock Nova 4/0 Default  1\n")
-        runCallback("OnFrame")
-        assert.are.equals(round(50 + 10), build.calcsTab.mainOutput.MaximumShock)
+	it("maximum shock value", function()
+		-- Shock Nova
+		build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nShock Nova 4/0 Default  1\n")
+		runCallback("OnFrame")
+		assert.are.equals(round(50 + 10), build.calcsTab.mainOutput.MaximumShock)
 
-        -- Voltaxic Rift
-        build.itemsTab:CreateDisplayItemFromRaw("New Item\nAssassin Bow\n+40% to Maximum Effect of Shock")
-        build.itemsTab:AddDisplayItem()
-        runCallback("OnFrame")
-        assert.are.equals(round(50 + 10 + 40), build.calcsTab.mainOutput.MaximumShock)
-    end)
+		-- Voltaxic Rift
+		build.itemsTab:CreateDisplayItemFromRaw("New Item\nAssassin Bow\n+40% to Maximum Effect of Shock")
+		build.itemsTab:AddDisplayItem()
+		runCallback("OnFrame")
+		assert.are.equals(round(50 + 10 + 40), build.calcsTab.mainOutput.MaximumShock)
+	end)
 
 	it("bleed is buffed by bleed chance", function()
 		build.itemsTab:CreateDisplayItemFromRaw("New Item\nKarui Chopper")
 		build.itemsTab:AddDisplayItem()
 		build.skillsTab:PasteSocketGroup("Slot: Weapon 1\nHeavy Strike 1/0 Default  1\n")
 		build.configTab.input.customMods = "\z
-        attacks have 10% chance to cause bleeding\n\z
-        "
+		attacks have 10% chance to cause bleeding\n\z
+		"
 		build.configTab:BuildModList()
 		runCallback("OnFrame")
 		local badDps = build.calcsTab.mainOutput.BleedDPS
 
 		build.configTab.input.customMods = "\z
-        attacks have 100% chance to cause bleeding\n\z
-        "
+		attacks have 100% chance to cause bleeding\n\z
+		"
 		build.configTab:BuildModList()
 		runCallback("OnFrame")
 		local goodDps = build.calcsTab.mainOutput.BleedDPS
