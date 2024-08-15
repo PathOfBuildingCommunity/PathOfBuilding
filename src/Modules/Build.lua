102300a54ee3b54a9eb05648b8fba699f847c9a8
Programmist <niko4pok@gmail.com>
2024-08-15 10:23:17 +0300

    predefine build name on import

--- src/Modules/Build.lua
+++ src/Modules/Build.lua
@@ -1450,7 +1450,7 @@ function buildMode:OpenSaveAsPopup()
 	end
 	controls.label = new("LabelControl", nil, 0, 20, 0, 16, "^7Enter new build name:")
 	controls.edit = new("EditControl", nil, 0, 40, 450, 20,
-	(self.buildName or self.dbFileName):gsub("[\\/:%*%?\"<>|%c]", "-"), nil, "\\/:%*%?\"<>|%c", 100, function(buf)
+	main.predefinedBuildName or (self.buildName or self.dbFileName):gsub("[\\/:%*%?\"<>|%c]", "-"), nil, "\\/:%*%?\"<>|%c", 100, function(buf)
 		updateBuildName()
 	end)
 	controls.folderLabel = new("LabelControl", {"TOPLEFT",nil,"TOPLEFT"}, 10, 70, 0, 16, "^7Folder:")
