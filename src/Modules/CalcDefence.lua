34d6d098e50b6140ce0496a30562c86365b25475
Edvinas-Smita <36479307+Edvinas-Smita@users.noreply.github.com>
2025-02-14 05:17:28 +0200

    PoB2 #694 backport (#8459)

--- src/Modules/CalcDefence.lua
+++ src/Modules/CalcDefence.lua
@@ -258,33 +258,32 @@ function calcs.reducePoolsByDamage(poolTable, damageTable, actor)
 		end
 		local esBypass = output[damageType.."EnergyShieldBypass"] / 100 or 0
 		local lifeHitPool = calcLifeHitPoolWithLossPrevention(life, output.Life, output.preventedLifeLoss, lifeLossBelowHalfPrevented)
-		if energyShield > 0 and (not modDB:Flag(nil, "EnergyShieldProtectsMana")) and (esBypass) < 1 then
-			local esPool = esBypass > 0 and m_min(lifeHitPool / esBypass - lifeHitPool, energyShield) or energyShield
+		local MoMEffect = m_min(output.sharedMindOverMatter + output[damageType.."MindOverMatter"], 100) / 100
+		local MoMPool = MoMEffect < 1 and m_min(lifeHitPool / (1 - MoMEffect) - lifeHitPool, mana) or mana
+		local lifePlusMoMHitPool = lifeHitPool + MoMPool
+		if energyShield > 0 and not modDB:Flag(nil, "EnergyShieldProtectsMana") and esBypass < 1 then
+			local esPool = esBypass > 0 and m_min(lifePlusMoMHitPool / esBypass - lifePlusMoMHitPool, energyShield) or energyShield
 			local tempDamage = m_min(damageRemainder * (1 - esBypass), esPool)
 			esPoolRemaining = m_min(esPoolRemaining, esPool - tempDamage)
 			energyShield = energyShield - tempDamage
 			damageRemainder = damageRemainder - tempDamage
+		elseif esBypass == 1 then
+			esPoolRemaining = 0
 		end
-		if (output.sharedMindOverMatter + output[damageType.."MindOverMatter"]) > 0 then
-			local MoMEffect = m_min(output.sharedMindOverMatter + output[damageType.."MindOverMatter"], 100) / 100
-			local MoMPool = MoMEffect < 1 and m_min(lifeHitPool / (1 - MoMEffect) - lifeHitPool, mana) or mana
+		if MoMEffect > 0 and mana > 0 then
 			local MoMDamage = damageRemainder * MoMEffect
 			if modDB:Flag(nil, "EnergyShieldProtectsMana") and energyShield > 0 and esBypass < 1 then
 				local MoMEBPool = esBypass > 0 and m_min(MoMPool / esBypass - MoMPool, energyShield) or energyShield
 				local tempDamage = m_min(MoMDamage * (1 - esBypass), MoMEBPool)
 				esPoolRemaining = m_min(esPoolRemaining, MoMEBPool - tempDamage)
 				energyShield = energyShield - tempDamage
-				MoMDamage = MoMDamage - tempDamage
-				local tempDamage2 = m_min(MoMDamage, MoMPool)
-				MoMPoolRemaining = m_min(MoMPoolRemaining, MoMPool - tempDamage2)
-				mana = mana - tempDamage2
-				damageRemainder = damageRemainder - tempDamage - tempDamage2
-			elseif mana > 0 then
-				local tempDamage = m_min(MoMDamage, MoMPool)
-				MoMPoolRemaining = m_min(MoMPoolRemaining, MoMPool - tempDamage)
-				mana = mana - tempDamage
 				damageRemainder = damageRemainder - tempDamage
+				MoMDamage = MoMDamage - tempDamage
 			end
+			local tempDamage = m_min(MoMDamage, MoMPool)
+			MoMPoolRemaining = m_min(MoMPoolRemaining, MoMPool - tempDamage)
+			mana = mana - tempDamage
+			damageRemainder = damageRemainder - tempDamage
 		else
 			MoMPoolRemaining = 0
 		end
