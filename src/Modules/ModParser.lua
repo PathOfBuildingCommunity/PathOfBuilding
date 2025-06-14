d01b9895eae1e711895ccbbbff531294ab782e77
Peechey <92683202+Peechey@users.noreply.github.com>
2025-06-13 20:52:11 -0500

    add support for cold dot multi per overcapped cold res mastery

--- src/Modules/ModParser.lua
+++ src/Modules/ModParser.lua
@@ -2104,6 +2104,7 @@ local specialModList = {
 	["energy shield protects mana instead of life"] = { flag("EnergyShieldProtectsMana") },
 	["modifiers to critical strike multiplier also apply to damage over time multiplier for ailments from critical strikes at (%d+)%% of their value"] = function(num) return { mod("CritMultiplierAppliesToDegen", "BASE", num) } end,
 	["damage over time multiplier for ailments is equal to critical strike multiplier"] = { flag("DotMultiplierIsCritMultiplier") },
+	["%+(%d+)%% to cold damage over time multiplier for each (%d+)%% overcapped cold resistance"] = function(num, _, val) return { mod("ColdDotMultiplier", "BASE", num, { type = "PerStat", stat = "ColdResistOverCap", div = val }) } end,
 	["your bleeding does not deal extra damage while the enemy is moving"] = { flag("Condition:NoExtraBleedDamageToMovingEnemy") },
 	["your bleeding does not deal extra damage while the enemy is moving and cannot be aggravated"] = { flag("Condition:NoExtraBleedDamageToMovingEnemy"), flag("Condition:CannotAggravate") },
 	["you and enemies in your presence count as moving while affected by elemental ailments"] = {
