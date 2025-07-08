cc3d574a12c698bffd5e37cb6c87b48f1ed97bac
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2025-07-08 20:36:40 +1000

    Fix Brand attachment duration not being scaled by Temporal Chains (#8796)
    The Brand attachment time is affected by temportal chains so this affects the maximum number of ticks a brand can do. More relevant for Wintertide Brand as we uses the attachment duration for DPS calcs
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Modules/CalcOffence.lua
+++ src/Modules/CalcOffence.lua
@@ -2297,7 +2297,7 @@ function calcs.offence(env, actor, activeSkill)
 			output.HitSpeed = 1 / output.HitTime
 			--Brands always have hitTimeOverride
 			if activeSkill.skillTypes[SkillType.Brand] and not skillModList:Flag(nil, "UnlimitedBrandDuration") then
-				output.BrandTicks = m_floor(output.Duration * output.HitSpeed)
+				output.BrandTicks = m_floor(output.Duration * output.HitSpeed * debuffDurationMult)
 			end
 		elseif skillData.hitTimeMultiplier and output.Time and not skillData.triggeredOnDeath then
 			output.HitTime = output.Time * skillData.hitTimeMultiplier
