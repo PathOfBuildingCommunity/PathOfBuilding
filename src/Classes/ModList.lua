PathOfBuildingCommunity/PathOfBuilding

commit 0ed1dd8bb482bf776867e34c9d0de6f8e404780c

Fix rounding of scaled mods being incorrect in some cases (#8862)
* Fixes #8860: Rounding of scaled mods was incorrect in some cases

* Don't include boolean or table mods

src/Classes/ModList.lua
@@ -46,7 +46,7 @@ function ModListClass:ReplaceModInternal(mod)
 end
 
 function ModListClass:MergeMod(mod)
-	if mod.type == "BASE" or mod.type == "INC" then
+	if mod.type == "BASE" or mod.type == "INC" or mod.type == "MORE" then
 		for i = 1, #self do
 			if modLib.compareModParams(self[i], mod) then
 				self[i] = copyTable(self[i], true)

src/Modules/CalcSetup.lua
@@ -1051,19 +1051,29 @@ function calcs.initEnv(build, mode, override, specEnv)
 					end	
 					env.itemModDB:ScaleAddList(combinedList, scale)
 				elseif item.type == "Gloves" and calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromGloves") ~=1 then
-					scale = calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromGloves")
+					scale = calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromGloves") - 1
 					local combinedList = new("ModList")
 					for _, mod in ipairs(srcList) do
 						combinedList:MergeMod(mod)
 					end
-					env.itemModDB:ScaleAddList(combinedList, scale)
+					local scaledList = new("ModList")
+					scaledList:ScaleAddList(combinedList, scale)
+					for _, mod in ipairs(scaledList) do
+						combinedList:MergeMod(mod)
+					end
+					env.itemModDB:AddList(combinedList)
 				elseif item.type == "Boots" and calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromBoots") ~= 1 then
-					scale = calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromBoots")
+					scale = calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromBoots") - 1
 					local combinedList = new("ModList")
 					for _, mod in ipairs(srcList) do
 						combinedList:MergeMod(mod)
 					end
-					env.itemModDB:ScaleAddList(combinedList, scale)
+					local scaledList = new("ModList")
+					scaledList:ScaleAddList(combinedList, scale)
+					for _, mod in ipairs(scaledList) do
+						combinedList:MergeMod(mod)
+					end
+					env.itemModDB:AddList(combinedList)
 				else
 					env.itemModDB:ScaleAddList(srcList, scale)
 				end

commit 81e83fbd70cb818f45a8fd08c80943625c274448

FIX: LIST type mods being duplicated
Most likely caused by https://github.com/PathOfBuildingCommunity/PathOfBuilding/pull/8862

src/Classes/ModList.lua
@@ -45,7 +45,7 @@ function ModListClass:ReplaceModInternal(mod)
 	return false
 end
 
-function ModListClass:MergeMod(mod)
+function ModListClass:MergeMod(mod, skipNonAdditive)
 	if mod.type == "BASE" or mod.type == "INC" or mod.type == "MORE" then
 		for i = 1, #self do
 			if modLib.compareModParams(self[i], mod) then
@@ -55,7 +55,9 @@ function ModListClass:MergeMod(mod)
 			end
 		end
 	end
-	self:AddMod(mod)
+	if not skipNonAdditive then
+		self:AddMod(mod)
+	end
 end
 
 function ModListClass:AddList(modList)

src/Modules/CalcSetup.lua
@@ -1067,7 +1067,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 					local scaledList = new("ModList")
 					scaledList:ScaleAddList(combinedList, scale)
 					for _, mod in ipairs(scaledList) do
-						combinedList:MergeMod(mod)
+						combinedList:MergeMod(mod, true)
 					end
 					env.itemModDB:AddList(combinedList)
 				elseif item.type == "Boots" and calcLib.mod(env.initialNodeModDB, nil, "EffectOfBonusesFromBoots") ~= 1 then
@@ -1079,7 +1079,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 					local scaledList = new("ModList")
 					scaledList:ScaleAddList(combinedList, scale)
 					for _, mod in ipairs(scaledList) do
-						combinedList:MergeMod(mod)
+						combinedList:MergeMod(mod, true)
 					end
 					env.itemModDB:AddList(combinedList)
 				else