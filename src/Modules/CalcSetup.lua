43147e1f3117a2da3093aeedc53c760aa4120984
Wires77 <Wires77@users.noreply.github.com>
2025-10-30 00:53:33 -0500

    Kalandra's Touch doesn't copy if it's in the 3rd slot.  See PoB2#1487

--- src/Modules/CalcSetup.lua
+++ src/Modules/CalcSetup.lua
@@ -1021,7 +1021,7 @@ function calcs.initEnv(build, mode, override, specEnv)
 						end
 					end
 				elseif item.name:match("Kalandra's Touch") then
-					local otherRing = items[(slotName == "Ring 1" and "Ring 2") or (slotName == "Ring 2" and "Ring 1") or (slotName == "Ring 3" and "Ring 2")]
+					local otherRing = items[(slotName == "Ring 1" and "Ring 2") or (slotName == "Ring 2" and "Ring 1")]
 					if otherRing and not otherRing.name:match("Kalandra's Touch") then
 						for _, mod in ipairs(otherRing.modList or otherRing.slotModList[slot.slotNum] or {}) do
 							-- Filter out SocketedIn type mods
