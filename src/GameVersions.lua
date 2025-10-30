abafbf757bf759e16bdefc16e6dbcbb49cf42608
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2025-10-31 06:14:39 +1100

    Add 3.27 Skill Tree (#9068)
    * Add 3.27 Skill Tree
    
    The new skill tree uses the old wildwood ascendancies data type so we'll need a way to filter just the Wildwood nodes out
    We also currently rely on assets from the 3.19 tree to draw the wildwood ascendancy art so would need to change it to use the sprite file for the bloodline ones
    
    * Use alphabetical order for bloodline ascendancies
    
    * Load Bloodline sprites
    
    * Count towards regular ascendancy points
    
    * Add back alt ascendancy dropdown
    
    * Remove legacy wildwood nodes from the tree
    
    * Use bloodline art assets from sprite sheet
    
    * Remove PNG assets and use web-p ones instead
    
    * Add ruthless tree, clean up unnecessary code
    
    * Update PoE Planner versions
    
    * ModCache
    
    ---------
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>
    Co-authored-by: Wires77 <Wires77@users.noreply.github.com>

--- src/GameVersions.lua
+++ src/GameVersions.lua
@@ -7,7 +7,9 @@ liveTargetVersion = "3_0"
 -- Skill tree versions
 ---Added for convenient indexing of skill tree versions.
 ---@type string[]
-treeVersionList = { "2_6", "3_6", "3_7", "3_8", "3_9", "3_10", "3_11", "3_12", "3_13", "3_14", "3_15", "3_16", "3_17", "3_18", "3_19", "3_20", "3_21", "3_22_ruthless", "3_22", "3_23_ruthless", "3_23", "3_24_ruthless", "3_24", "3_25_ruthless", "3_25_ruthless_alternate", "3_25_alternate", "3_25", "3_26_ruthless", "3_26_ruthless_alternate", "3_26_alternate", "3_26" }
+treeVersionList = { "2_6", "3_6", "3_7", "3_8", "3_9", "3_10", "3_11", "3_12", "3_13", "3_14", "3_15", "3_16", "3_17", "3_18", "3_19", "3_20", "3_21",
+					"3_22_ruthless", "3_22", "3_23_ruthless", "3_23", "3_24_ruthless", "3_24", "3_25_ruthless", "3_25_ruthless_alternate", "3_25_alternate", "3_25",
+					"3_26_ruthless", "3_26_ruthless_alternate", "3_26_alternate", "3_26", "3_27_ruthless", "3_27" }
 --- Always points to the latest skill tree version.
 latestTreeVersion = treeVersionList[#treeVersionList]
 ---Tree version where multiple skill trees per build were introduced to PoBC.
@@ -169,11 +171,23 @@ treeVersions = {
 		display = "3.26",
 		num = 3.26,
 		url = "https://www.pathofexile.com/passive-skill-tree/3.26.0/",
-	}
+	},
+	["3_27_ruthless"] = {
+		display = "3.27 (ruthless)",
+		num = 3.27,
+		url = "https://www.pathofexile.com/passive-skill-tree/ruthless/",
+	},
+	["3_27"] = {
+		display = "3.27",
+		num = 3.27,
+		url = "https://www.pathofexile.com/passive-skill-tree/3.27.0/",
+	},
 }
 
----Mapping PoEPlanner.com version when importing trees from there
+---Mapping PoEPlanner.com version when importing trees from there (https://cdn.poeplanner.com/json/versions.json)
 poePlannerVersions = {
+	[43] = "3_27",
+	[42] = "3_27_ruthless",
 	[37] = "3_26",
 	[36] = "3_26_ruthless",
 	[35] = "3_25_alternate",
