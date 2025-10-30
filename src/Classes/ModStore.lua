eca55405e13e2e4c7b76d0c38782abedfcf0ba15
LocalIdentity <localidentity2@gmail.com>
2025-10-31 03:36:36 +1100

    Fix Threshold using wrong actor
    The change was not using target as a backup when ThresholdActor was not set

--- src/Classes/ModStore.lua
+++ src/Classes/ModStore.lua
@@ -373,7 +373,7 @@ function ModStoreClass:EvalMod(mod, cfg, globalLimits)
 			else
 				mult = GetMultiplier(target, tag.var, cfg)
 			end
-			local threshold = tag.threshold or GetMultiplier(thresholdTarget, tag.thresholdVar, cfg)
+			local threshold = tag.threshold or GetMultiplier(tag.thresholdActor and thresholdTarget or target, tag.thresholdVar, cfg)
 			if (tag.upper and mult > threshold) or (tag.equals and mult ~= threshold) or (not (tag.upper and tag.exact) and mult < threshold) then
 				return
 			end
