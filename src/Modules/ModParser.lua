18736464c9c224b328497d5b366232ca6d8ad947
Никита Троянов <36129181+n1tr0xs@users.noreply.github.com>
2025-11-03 00:03:05 +0300

    Fix Intuitive Leap, Thread of Hope and Impossible Escape not working (#9135)
    * fix #9125
    
    * Update parsing + update unique mod lines
    
    ---------
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Modules/ModParser.lua
+++ src/Modules/ModParser.lua
@@ -4752,11 +4752,11 @@ local specialModList = {
 		flag("LifeFlaskEffectNotRemoved")
 	},
 	-- Jewels
-	["passives in radius of ([%a%s']+) can be allocated without being connected to your tree"] = function(_, name) return {
+	["passives ?s?k?i?l?l?s? in radius of ([%a%s']+) can be allocated without being connected to your tree"] = function(_, name) return {
 		mod("JewelData", "LIST", { key = "impossibleEscapeKeystone", value = name }),
 		mod("ImpossibleEscapeKeystones", "LIST", { key = name, value = true }),
 	} end,
-	["passives in radius can be allocated without being connected to your tree"] = { mod("JewelData", "LIST", { key = "intuitiveLeapLike", value = true }) },
+	["passives ?s?k?i?l?l?s? in radius can be allocated without being connected to your tree"] = { mod("JewelData", "LIST", { key = "intuitiveLeapLike", value = true }) },
 	["affects passives in small ring"] = { mod("JewelData", "LIST", { key = "radiusIndex", value = 6 }) },
 	["affects passives in medium ring"] = { mod("JewelData", "LIST", { key = "radiusIndex", value = 7 }) },
 	["affects passives in large ring"] = { mod("JewelData", "LIST", { key = "radiusIndex", value = 8 }) },
