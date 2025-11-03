725840e9c3723fb81900804f47d62da16c6422d2
LocalIdentity <31035929+LocalIdentity@users.noreply.github.com>
2025-11-03 12:03:56 +1100

    Fix Overexertion and Echoes of Creation counting duplicate Warcries (#9144)
    The count for the number of Warcries exerting a skill was not counting the number of unique warcries but instead the number of warcries in the build. We now make a list of warcries that are affecting the skill and don't add a count to the multiplier if the buff has already been counted once
    
    Co-authored-by: LocalIdentity <localidentity2@gmail.com>

--- src/Modules/CalcPerform.lua
+++ src/Modules/CalcPerform.lua
@@ -1945,6 +1945,7 @@ function calcs.perform(env, skipEHP)
 	end
 
 	local appliedCombustion = false
+	local warcryList = { }
 	for _, activeSkill in ipairs(env.player.activeSkillList) do
 		local skillModList = activeSkill.skillModList
 		local skillCfg = activeSkill.skillCfg
@@ -2008,7 +2009,10 @@ function calcs.perform(env, skipEHP)
 						local extraExertions = modStore:Sum("BASE", nil, "ExtraExertedAttacks") or 0
 						local exertMultiplier = modStore:More(nil, "ExtraExertedAttacks")
 						env.player.modDB:NewMod("Num"..warcryName.."Exerts", "BASE", m_floor((baseExerts + extraExertions) * exertMultiplier))
-						env.player.modDB:NewMod("Multiplier:ExertingWarcryCount", "BASE", 1)
+						if not warcryList[buff.name] then
+							env.player.modDB:NewMod("Multiplier:ExertingWarcryCount", "BASE", 1, buff.name)
+							warcryList[buff.name] = true
+						end
 					end
 					if not activeSkill.skillModList:Flag(nil, "CannotShareWarcryBuffs") then
 						local warcryPower = modDB:Override(nil, "WarcryPower") or m_max((modDB:Sum("BASE", nil, "WarcryPower") or 0) * (1 + (modDB:Sum("INC", nil, "WarcryPower") or 0)/100), (modDB:Sum("BASE", nil, "MinimumWarcryPower") or 0))
