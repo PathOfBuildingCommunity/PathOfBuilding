69904e621c378073e4b3854cedeb2144400b041b
Wires77 <Wires77@users.noreply.github.com>
2025-11-25 00:02:16 -0600

    Fix mod export logic to keep using functions as intended

--- src/Export/Scripts/mods.lua
+++ src/Export/Scripts/mods.lua
@@ -33,8 +33,6 @@ local function writeMods(outName, condFunc)
 					print("[Jewel]: Skipping '" .. mod.Id .. "'")
 					goto continue
 				end
-			elseif mod.Family[1] and mod.Family[1].Id ~= "AuraBonus" and mod.Family[1].Id ~= "ArbalestBonus" and mod.GenerationType == 3 and not (mod.Domain == 16 or (mod.Domain == 1 and mod.Id:match("^Synthesis") or mod.Id:match("^MutatedUnique") or (mod.Family[2] and mod.Family[2].Id:match("MatchedInfluencesTier")))) then
-				goto continue
 			end
 			local stats, orders = describeMod(mod)
 			if #orders > 0 then
@@ -137,8 +135,10 @@ end
 
 writeMods("../Data/ModItem.lua", function(mod)
 	return (mod.Domain == 1 or mod.Domain == 16)
-			and (mod.GenerationType == 1 or mod.GenerationType == 2 or (mod.GenerationType == 3 and (not mod.Id:match("^MutatedUnique")) and (mod.Id:match("^Synthesis") or (mod.Family[1].Id ~= "AuraBonus" and mod.Family[1].Id ~= "ArbalestBonus") and not (mod.Family[2] and mod.Family[2].Id:match("MatchedInfluencesTier")))) or mod.GenerationType == 5
-			 or mod.GenerationType == 25 or mod.GenerationType == 24 or mod.GenerationType == 28 or mod.GenerationType == 29)
+			and (mod.GenerationType == 1 or mod.GenerationType == 2
+			or (mod.GenerationType == 3 and mod.Domain == 1 and mod.Id:match("^Synthesis"))
+			or (mod.GenerationType == 3 and mod.Domain == 16)
+			or mod.GenerationType == 5 or mod.GenerationType == 25 or mod.GenerationType == 24 or mod.GenerationType == 28 or mod.GenerationType == 29)
 			and not mod.Id:match("^Hellscape[UpDown]+sideMap") -- Exclude Scourge map mods
 			and not mod.Id:match("Royale")
 			and #mod.AuraFlags == 0
@@ -147,7 +147,7 @@ writeMods("../Data/ModFlask.lua", function(mod)
 	return mod.Domain == 2 and (mod.GenerationType == 1 or mod.GenerationType == 2)
 end)
 writeMods("../Data/ModTincture.lua", function(mod)
-	return (mod.Domain == 34) and (mod.GenerationType == 1 or mod.GenerationType == 2 or mod.GenerationType == 3)
+	return (mod.Domain == 34) and (mod.GenerationType == 1 or mod.GenerationType == 2)
 end)
 writeMods("../Data/ModJewel.lua", function(mod)
 	return (mod.Domain == 10 or mod.Domain == 16) and (mod.GenerationType == 1 or mod.GenerationType == 2 or mod.GenerationType == 5)
@@ -174,8 +174,11 @@ writeMods("../Data/ModNecropolis.lua", function(mod)
 	return mod.Domain == 1 and mod.Id:match("^NecropolisCrafting")
 end)
 writeMods("../Data/ModItemExclusive.lua", function(mod) -- contains primarily uniques and items implicits but also other mods only available on a single base or unique.
-	return (mod.Domain == 1 or mod.Domain == 2 or mod.Domain == 11 or mod.Domain == 22) and mod.GenerationType == 3
-	and (mod.Family[1] and mod.Family[1].Id ~= "AuraBonus" or not mod.Family[1])
+if mod.Id == "StrengthUniqueHelmetDexInt1" then
+		return true
+	end	
+return (mod.Domain == 1 or mod.Domain == 2 or mod.Domain == 10 or mod.Domain == 21 or mod.Domain == 34) and mod.GenerationType == 3
+	and (mod.Family[1] and mod.Family[1].Id ~= "AuraBonus")
 	and not mod.Id:match("^Synthesis") and not mod.Id:match("Royale") and not mod.Id:match("Cowards") and not mod.Id:match("Map") and not mod.Id:match("Ultimatum")
 end)
 writeMods("../Data/ModGraft.lua", function(mod)
