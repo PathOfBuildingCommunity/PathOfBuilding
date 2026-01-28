089aee947ade4a46311c022beaef8871d8b95358
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:13:00 -0300

    Update belt.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/belt.lua
+++ src/Data/Uniques/Foulborn/belt.lua
@@ -3,7 +3,7 @@
 return {
 -- Belt
 [[
-Maligaro's Restraint Foulborn 1
+Foulborn Maligaro's Restraint 1
 Chain Belt
 Upgrade: Removed Mod: Adds 1 to (30—50) Lightning Damage to Attacks
 Upgrade: Added Mod: 40% of Lightning Damage Converted to Chaos Damage
@@ -14,9 +14,9 @@ Implicits: 1
 Shocks you cause are reflected back to you
 60% increased Damage while Shocked
 {tags:speed}15% increased Movement Speed while Shocked
-40% of Lightning Damage Converted to Chaos Damage
-]],[[ 
-Belt of the Deceiver Foulborn 1
+40% of Lightning Damage Converted to Chaos Damage (mutated)
+]],[[
+Foulborn Belt of the Deceiver 1
 Heavy Belt
 Upgrade: Removed Mod: You take 30% reduced Extra Damage from Critical Strikes
 Upgrade: Added Mod: 20% reduced Effect of Curses on you
@@ -27,9 +27,9 @@ Implicits: 1
 {tags:life}+(30-40) to maximum Life
 {tags:jewellery_resistance}+(10-15)% to all Elemental Resistances
 Nearby Enemies are Intimidated
-20% reduced Effect of Curses on you
-]],[[ 
-Belt of the Deceiver Foulborn 2
+20% reduced Effect of Curses on you (mutated)
+]],[[
+Foulborn Belt of the Deceiver 2
 Heavy Belt
 Upgrade: Removed Mod: Nearby Enemies are Intimidated
 Upgrade: Added Mod: Nearby Enemies are Unnerved
@@ -40,9 +40,9 @@ Implicits: 1
 {tags:life}+(30-40) to maximum Life
 {tags:jewellery_resistance}+(10-15)% to all Elemental Resistances
 {variant:2}{tags:critical}You take 30% reduced Extra Damage from Critical Strikes
-Nearby Enemies are Unnerved
-]],[[ 
-Belt of the Deceiver Foulborn 3
+Nearby Enemies are Unnerved (mutated)
+]],[[
+Foulborn Belt of the Deceiver 3
 Heavy Belt
 Upgrade: Removed Mod: You take 30% reduced Extra Damage from Critical Strikes
 Upgrade: Removed Mod: Nearby Enemies are Intimidated
@@ -54,10 +54,10 @@ Implicits: 1
 {tags:physical_damage}(15-25)% increased Global Physical Damage
 {tags:life}+(30-40) to maximum Life
 {tags:jewellery_resistance}+(10-15)% to all Elemental Resistances
-20% reduced Effect of Curses on you
-Nearby Enemies are Unnerved
-]],[[ 
-Dyadian Dawn Foulborn 1
+20% reduced Effect of Curses on you (mutated)
+Nearby Enemies are Unnerved (mutated)
+]],[[
+Foulborn Dyadian Dawn 1
 Heavy Belt
 Upgrade: Removed Mod: Ignites you inflict with Attacks deal Damage 35% faster
 Upgrade: Added Mod: All Damage can Ignite
@@ -69,9 +69,9 @@ Implicits: 1
 {tags:jewellery_resistance}+(20-40)% to Cold Resistance
 Deal no Physical Damage
 {tags:attack,life}1% of Attack Damage Leeched as Life against Chilled enemies
-All Damage can Ignite
-]],[[ 
-Dyadian Dawn Foulborn 2
+All Damage can Ignite (mutated)
+]],[[
+Foulborn Dyadian Dawn 2
 Heavy Belt
 Upgrade: Removed Mod: 1% of Attack Damage Leeched as Life against Chilled Enemies
 Upgrade: Added Mod: Recover 2% of Life when you Chill a non-Chilled Enemy
@@ -83,9 +83,9 @@ Implicits: 1
 {tags:jewellery_resistance}+(20-40)% to Cold Resistance
 Deal no Physical Damage
 {tags:jewellery_elemental,attack}Ignites you inflict with Attacks deal Damage 35% faster
-Recover 2% of Life when you Chill a non-Chilled Enemy
-]],[[ 
-Dyadian Dawn Foulborn 3
+Recover 2% of Life when you Chill a non-Chilled Enemy (mutated)
+]],[[
+Foulborn Dyadian Dawn 3
 Heavy Belt
 Upgrade: Removed Mod: Ignites you inflict with Attacks deal Damage 35% faster
 Upgrade: Removed Mod: 1% of Attack Damage Leeched as Life against Chilled Enemies
@@ -98,10 +98,10 @@ Implicits: 1
 {tags:jewellery_resistance}+(20-40)% to Fire Resistance
 {tags:jewellery_resistance}+(20-40)% to Cold Resistance
 Deal no Physical Damage
-All Damage can Ignite
-Recover 2% of Life when you Chill a non-Chilled Enemy
-]],[[ 
-Umbilicus Immortalis Foulborn 1
+All Damage can Ignite (mutated)
+Recover 2% of Life when you Chill a non-Chilled Enemy (mutated)
+]],[[
+Foulborn Umbilicus Immortalis 1
 Leather Belt
 Upgrade: Removed Mod: Regenerate 2% of Life per second
 Upgrade: Added Mod: Flasks gain a Charge every 3 seconds
@@ -111,9 +111,9 @@ Implicits: 1
 {tags:life}(8-12)% increased maximum Life
 Flasks you Use apply to your Raised Zombies and Spectres
 Flasks do not apply to you
-Flasks gain a Charge every 3 seconds
-]],[[ 
-Umbilicus Immortalis Foulborn 2
+Flasks gain a Charge every 3 seconds (mutated)
+]],[[
+Foulborn Umbilicus Immortalis 2
 Leather Belt
 Upgrade: Removed Mod: Flasks do not apply to you
 Upgrade: Added Mod: 100% increased Flask Charges used
@@ -123,9 +123,9 @@ Implicits: 1
 {tags:life}(8-12)% increased maximum Life
 Flasks you Use apply to your Raised Zombies and Spectres
 Regenerate 2% of Life per second
-100% increased Flask Charges used
-]],[[ 
-Umbilicus Immortalis Foulborn 3
+100% increased Flask Charges used (mutated)
+]],[[
+Foulborn Umbilicus Immortalis 3
 Leather Belt
 Upgrade: Removed Mod: Regenerate 2% of Life per second
 Upgrade: Removed Mod: Flasks do not apply to you
@@ -136,10 +136,10 @@ Implicits: 1
 {tags:life}+(25-40) to maximum Life
 {tags:life}(8-12)% increased maximum Life
 Flasks you Use apply to your Raised Zombies and Spectres
-Flasks gain a Charge every 3 seconds
-100% increased Flask Charges used
-]],[[ 
-Soul Tether Foulborn 1
+Flasks gain a Charge every 3 seconds (mutated)
+100% increased Flask Charges used (mutated)
+]],[[
+Foulborn Soul Tether 1
 Cloth Belt
 Upgrade: Removed Mod: Gain (4—6)% of Maximum Life as Extra Maximum Energy Shield
 Upgrade: Added Mod: (8—12)% of Leech is Instant
@@ -148,10 +148,9 @@ Implicits: 1
 (15-25)% increased Stun and Block Recovery
 {tags:jewellery_attribute}+(20-40) to Intelligence
 Immortal Ambition
-(8—12)% of Leech is Instant
-
-]],[[ 
-Soul Tether Foulborn 2
+(8—12)% of Leech is Instant (mutated)
+]],[[
+Foulborn Soul Tether 2
 Cloth Belt
 Upgrade: Removed Mod: Immortal Ambition
 Upgrade: Added Mod: Everlasting Sacrifice
@@ -160,10 +159,9 @@ Implicits: 1
 (15-25)% increased Stun and Block Recovery
 {tags:jewellery_attribute}+(20-40) to Intelligence
 Gain (4—6)% of Maximum Life as Extra Maximum Energy Shield
-Everlasting Sacrifice
-
-]],[[ 
-Soul Tether Foulborn 3
+Everlasting Sacrifice (mutated)
+]],[[
+Foulborn Soul Tether 3
 Cloth Belt
 Upgrade: Removed Mod: Gain (4—6)% of Maximum Life as Extra Maximum Energy Shield
 Upgrade: Removed Mod: Immortal Ambition
@@ -173,10 +171,10 @@ LevelReq: 48
 Implicits: 1
 (15-25)% increased Stun and Block Recovery
 {tags:jewellery_attribute}+(20-40) to Intelligence
-(8—12)% of Leech is Instant
-Everlasting Sacrifice
-]],[[ 
-Meginord's Girdle Foulborn 1
+(8—12)% of Leech is Instant (mutated)
+Everlasting Sacrifice (mutated)
+]],[[
+Foulborn Meginord's Girdle 1
 Heavy Belt
 Upgrade: Removed Mod: 25% increased Flask Life Recovery rate
 Upgrade: Added Mod: (5—15)% increased Strength
@@ -186,9 +184,9 @@ Implicits: 1
 {tags:attack,physical_damage}Adds 5 to 15 Physical Damage to Attacks
 {tags:life}10% increased maximum Life
 {tags:jewellery_resistance}+(10-20)% to Cold Resistance
-(5—15)% increased Strength
-]],[[ 
-Headhunter Foulborn 1
+(5—15)% increased Strength (mutated)
+]],[[
+Foulborn Headhunter 1
 Leather Belt
 Upgrade: Removed Mod: (20—30)% increased Damage with Hits against Rare monsters
 Upgrade: Added Mod: Culling Strike
@@ -199,9 +197,9 @@ Implicits: 1
 {tags:life}+(50-60) to maximum Life
 When you Kill a Rare monster, you gain its Modifiers for 60 seconds
 {tags:jewellery_attribute}+(40-55) to Strength
-Culling Strike
-]],[[ 
-Headhunter Foulborn 2
+Culling Strike (mutated)
+]],[[
+Foulborn Headhunter 2
 Leather Belt
 Upgrade: Removed Mod: When you Kill a Rare monster, you gain its Modifiers for 60 seconds
 Upgrade: Added Mod: Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds
@@ -212,9 +210,9 @@ Implicits: 1
 {tags:life}+(50-60) to maximum Life
 (20—30)% increased Damage with Hits against Rare monsters
 {tags:jewellery_attribute}+(40-55) to Strength
-Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds
-]],[[ 
-Headhunter Foulborn 3
+Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds (mutated)
+]],[[
+Foulborn Headhunter 3
 Leather Belt
 Upgrade: Removed Mod: {tags:jewellery_attribute}+(40-55) to Strength
 Upgrade: Added Mod: Rare and Unique Enemies within 120 metres have Minimap Icons
@@ -225,9 +223,9 @@ Implicits: 1
 {tags:life}+(50-60) to maximum Life
 (20—30)% increased Damage with Hits against Rare monsters
 When you Kill a Rare monster, you gain its Modifiers for 60 seconds
-Rare and Unique Enemies within 120 metres have Minimap Icons
-]],[[ 
-Headhunter Foulborn 4
+Rare and Unique Enemies within 120 metres have Minimap Icons (mutated)
+]],[[
+Foulborn Headhunter 4
 Leather Belt
 Upgrade: Removed Mod: (20—30)% increased Damage with Hits against Rare monsters
 Upgrade: Removed Mod: When you Kill a Rare monster, you gain its Modifiers for 60 seconds
@@ -239,10 +237,10 @@ Implicits: 1
 {tags:jewellery_attribute}+(40-55) to Dexterity
 {tags:life}+(50-60) to maximum Life
 {tags:jewellery_attribute}+(40-55) to Strength
-Culling Strike
-Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds
-]],[[ 
-Headhunter Foulborn 5
+Culling Strike (mutated)
+Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds (mutated)
+]],[[
+Foulborn Headhunter 5
 Leather Belt
 Upgrade: Removed Mod: (20—30)% increased Damage with Hits against Rare monsters
 Upgrade: Removed Mod: {tags:jewellery_attribute}+(40-55) to Strength
@@ -254,10 +252,10 @@ Implicits: 1
 {tags:jewellery_attribute}+(40-55) to Dexterity
 {tags:life}+(50-60) to maximum Life
 When you Kill a Rare monster, you gain its Modifiers for 60 seconds
-Culling Strike
-Rare and Unique Enemies within 120 metres have Minimap Icons
-]],[[ 
-Headhunter Foulborn 6
+Culling Strike (mutated)
+Rare and Unique Enemies within 120 metres have Minimap Icons (mutated)
+]],[[
+Foulborn Headhunter 6
 Leather Belt
 Upgrade: Removed Mod: When you Kill a Rare monster, you gain its Modifiers for 60 seconds
 Upgrade: Removed Mod: {tags:jewellery_attribute}+(40-55) to Strength
@@ -269,10 +267,10 @@ Implicits: 1
 {tags:jewellery_attribute}+(40-55) to Dexterity
 {tags:life}+(50-60) to maximum Life
 (20—30)% increased Damage with Hits against Rare monsters
-Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds
-Rare and Unique Enemies within 120 metres have Minimap Icons
-]],[[ 
-Headhunter Foulborn 7
+Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds (mutated)
+Rare and Unique Enemies within 120 metres have Minimap Icons (mutated)
+]],[[
+Foulborn Headhunter 7
 Leather Belt
 Upgrade: Removed Mod: (20—30)% increased Damage with Hits against Rare monsters
 Upgrade: Removed Mod: When you Kill a Rare monster, you gain its Modifiers for 60 seconds
@@ -285,11 +283,11 @@ Implicits: 1
 {tags:life}+(25-40) to maximum Life
 {tags:jewellery_attribute}+(40-55) to Dexterity
 {tags:life}+(50-60) to maximum Life
-Culling Strike
-Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds
-Rare and Unique Enemies within 120 metres have Minimap Icons
-]],[[ 
-Soulthirst Foulborn 1
+Culling Strike (mutated)
+Eat a Soul when you Hit a Rare or Unique Enemy, no more than once every 0.25 seconds (mutated)
+Rare and Unique Enemies within 120 metres have Minimap Icons (mutated)
+]],[[
+Foulborn Soulthirst 1
 Cloth Belt
 Upgrade: Removed Mod: (20—30)% reduced Flask Effect Duration
 Upgrade: Added Mod: 50% reduced Mana Recovery rate
@@ -301,9 +299,9 @@ Implicits: 1
 {tags:mana}(20-30)% increased Mana Recovery from Flasks
 Gain Soul Eater during any Flask Effect
 Lose Souls gained from Soul Eater when you use a Flask
-50% reduced Mana Recovery rate
-]],[[ 
-The Magnate Foulborn 1
+50% reduced Mana Recovery rate (mutated)
+]],[[
+Foulborn The Magnate 1
 Studded Belt
 Upgrade: Removed Mod: 50% increased Flask Charges gained
 Upgrade: Added Mod: Flasks applied to you have (10—15)% increased Effect
@@ -314,9 +312,9 @@ Implicits: 1
 {tags:jewellery_resistance}+(20-25)% to all Elemental Resistances
 10% chance to deal Double Damage while you have at least 200 Strength
 5% chance to deal Triple Damage while you have at least 400 Strength
-Flasks applied to you have (10—15)% increased Effect
-]],[[ 
-Mageblood Foulborn 1
+Flasks applied to you have (10—15)% increased Effect (mutated)
+]],[[
+Foulborn Mageblood 1
 Heavy Belt
 Upgrade: Removed Mod: Leftmost (2-4) Magic Utility Flasks constantly apply their Flask Effects to you
 Upgrade: Added Mod: Rightmost (2-4) Magic Utility Flasks constantly apply their Flask Effects to you
@@ -328,9 +326,9 @@ Implicits: 1
 {tags:jewellery_resistance}+(15-25)% to Cold Resistance
 Magic Utility Flask cannot be Used
 Magic Utility Flask Effects cannot be removed
-Rightmost (2-4) Magic Utility Flasks constantly apply their Flask Effects to you
-]],[[ 
-Sunblast Foulborn 1
+Rightmost (2-4) Magic Utility Flasks constantly apply their Flask Effects to you (mutated)
+]],[[
+Foulborn Sunblast 1
 Cloth Belt
 Upgrade: Removed Mod: (50—75)% reduced Trap Duration
 Upgrade: Added Mod: Skills used by Traps have (40—60)% increased Area of Effect
@@ -342,10 +340,10 @@ Implicits: 1
 Skills which Throw Traps throw up to 2 additional Traps
 Traps cannot be triggered by Enemies
 Traps from Skills are thrown randomly around targeted location
-Skills used by Traps have (40—60)% increased Area of Effect
+Skills used by Traps have (40—60)% increased Area of Effect (mutated)
 
-]],[[ 
-Auxium Foulborn 1
+]],[[
+Foulborn Auxium 1
 Crystal Belt
 Upgrade: Removed Mod: 0.2% of Attack Damage Leeched as Mana per Power Charge
 Upgrade: Added Mod: (4—6)% increased Energy Shield per Power Charge
@@ -356,7 +354,7 @@ Implicits: 1
 {tags:mana}+(45-55) to maximum Mana
 Chill Effect and Freeze Duration on you are based on 100% of Energy Shield
 {tags:attack,jewellery_elemental}(20-25)% increased Elemental Damage with Attack Skills per Power Charge
-(4—6)% increased Energy Shield per Power Charge
+(4—6)% increased Energy Shield per Power Charge (mutated)
 ]],
 
 }
