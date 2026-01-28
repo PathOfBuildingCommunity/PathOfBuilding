ab12df37ad745e3e3d2bb0cb25399549047467bb
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:13:08 -0300

    Update bow.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/bow.lua
+++ src/Data/Uniques/Foulborn/bow.lua
@@ -2,9 +2,8 @@
 
 return {
 -- Weapon: Bow
-
 [[
-Null's Inclination Foulborn 1
+Foulborn Null's Inclination 1
 Ranger Bow
 Upgrade: Removed Mod: Trigger Socketed Minion Spells on Kill with this Weapon Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
 Upgrade: Removed Mod: Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
@@ -15,14 +14,14 @@ Adds (50-80) to (130-180) Chaos Damage
 +(7-11)% to Chaos Resistance
 Minions deal 2% increased Damage per 5 Dexterity
 +212 Intelligence Requirement
-Socketed Gems are Supported by Level 20 Summon Phantasm
+Socketed Gems are Supported by Level 20 Summon Phantasm (mutated)
 ]],[[
-Null's Inclination Foulborn 2
+Foulborn Null's Inclination 2
 Ranger Bow
 Upgrade: Removed Mod: Adds (50—80) to (130—180) Chaos Damage
 Upgrade: Added Mod: Minions deal (25—35) to (50—65) additional Chaos Damage
 Requires Level 60, 212 Dex, 212 Int
-Minions deal (25—35) to (50—65) additional Chaos Damage
+Minions deal (25—35) to (50—65) additional Chaos Damage (mutated)
 (7-12)% increased Attack Speed
 +(7-11)% to Chaos Resistance
 Minions deal 2% increased Damage per 5 Dexterity
@@ -30,20 +29,20 @@ Minions deal 2% increased Damage per 5 Dexterity
 Trigger Socketed Minion Spells on Kill with this Weapon
 Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
 ]],[[
-Null's Inclination Foulborn 3
+Foulborn Null's Inclination 3
 Ranger Bow
 Upgrade: Removed Mod: +(7—11)% to Chaos Resistance
 Upgrade: Added Mod: An Enemy Writhing Worm spawns every 2 seconds
 Requires Level 60, 212 Dex, 212 Int
 Adds (50-80) to (130-180) Chaos Damage
 (7-12)% increased Attack Speed
-An Enemy Writhing Worm spawns every 2 seconds
+An Enemy Writhing Worm spawns every 2 seconds (mutated)
 Minions deal 2% increased Damage per 5 Dexterity
 +212 Intelligence Requirement
 Trigger Socketed Minion Spells on Kill with this Weapon
 Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
 ]],[[
-Null's Inclination Foulborn 4
+Foulborn Null's Inclination 4
 Ranger Bow
 Upgrade: Removed Mod: Trigger Socketed Minion Spells on Kill with this Weapon
 Upgrade: Removed Mod: Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
@@ -51,14 +50,14 @@ Upgrade: Removed Mod: Adds (50—80) to (130—180) Chaos Damage
 Upgrade: Added Mod: Socketed Gems are Supported by Level 20 Summon Phantasm
 Upgrade: Added Mod: Minions deal (25—35) to (50—65) additional Chaos Damage
 Requires Level 60, 212 Dex, 212 Int
-Minions deal (25—35) to (50—65) additional Chaos Damage
+Minions deal (25—35) to (50—65) additional Chaos Damage (mutated)
 (7-12)% increased Attack Speed
 +(7-11)% to Chaos Resistance
 Minions deal 2% increased Damage per 5 Dexterity
 +212 Intelligence Requirement
-Socketed Gems are Supported by Level 20 Summon Phantasm
+Socketed Gems are Supported by Level 20 Summon Phantasm (mutated)
 ]],[[
-Null's Inclination Foulborn 5
+Foulborn Null's Inclination 5
 Ranger Bow
 Upgrade: Removed Mod: Trigger Socketed Minion Spells on Kill with this Weapon
 Upgrade: Removed Mod: Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
@@ -68,27 +67,27 @@ Upgrade: Added Mod: An Enemy Writhing Worm spawns every 2 seconds
 Requires Level 60, 212 Dex, 212 Int
 Adds (50-80) to (130-180) Chaos Damage
 (7-12)% increased Attack Speed
-An Enemy Writhing Worm spawns every 2 seconds
+An Enemy Writhing Worm spawns every 2 seconds (mutated)
 Minions deal 2% increased Damage per 5 Dexterity
 +212 Intelligence Requirement
-Socketed Gems are Supported by Level 20 Summon Phantasm
+Socketed Gems are Supported by Level 20 Summon Phantasm (mutated)
 ]],[[
-Null's Inclination Foulborn 6
+Foulborn Null's Inclination 6
 Ranger Bow
 Upgrade: Removed Mod: Adds (50—80) to (130—180) Chaos Damage
 Upgrade: Removed Mod: +(7—11)% to Chaos Resistance
 Upgrade: Added Mod: Minions deal (25—35) to (50—65) additional Chaos Damage
 Upgrade: Added Mod: An Enemy Writhing Worm spawns every 2 seconds
 Requires Level 60, 212 Dex, 212 Int
-Minions deal (25—35) to (50—65) additional Chaos Damage
+Minions deal (25—35) to (50—65) additional Chaos Damage (mutated)
 (7-12)% increased Attack Speed
-An Enemy Writhing Worm spawns every 2 seconds
+An Enemy Writhing Worm spawns every 2 seconds (mutated)
 Minions deal 2% increased Damage per 5 Dexterity
 +212 Intelligence Requirement
 Trigger Socketed Minion Spells on Kill with this Weapon
 Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
 ]],[[
-Null's Inclination Foulborn 7
+Foulborn Null's Inclination 7
 Ranger Bow
 Upgrade: Removed Mod: Trigger Socketed Minion Spells on Kill with this Weapon
 Upgrade: Removed Mod: Minion Spells Triggered by this Item have a 0.25 second Cooldown with 5 Uses
@@ -98,16 +97,15 @@ Upgrade: Added Mod: Socketed Gems are Supported by Level 20 Summon Phantasm
 Upgrade: Added Mod: Minions deal (25—35) to (50—65) additional Chaos Damage
 Upgrade: Added Mod: An Enemy Writhing Worm spawns every 2 seconds
 Requires Level 60, 212 Dex, 212 Int
-Minions deal (25—35) to (50—65) additional Chaos Damage
+Minions deal (25—35) to (50—65) additional Chaos Damage (mutated)
 (7-12)% increased Attack Speed
-+(7-11)% to Chaos Resistance
+An Enemy Writhing Worm spawns every 2 seconds (mutated)
 Minions deal 2% increased Damage per 5 Dexterity
 +212 Intelligence Requirement
-Socketed Gems are Supported by Level 20 Summon Phantasm
+Socketed Gems are Supported by Level 20 Summon Phantasm (mutated)
 ]],
-
 [[
-Xoph's Inception Foulborn 1
+Foulborn Xoph's Inception 1
 Citadel Bow
 Upgrade: Removed Mod: Projectiles Pierce all Burning Enemies
 Upgrade: Added Mod: Socketed Gems are Supported by Level 20 Returning Projectiles
@@ -116,22 +114,22 @@ Requires Level 23, 80 Dex
 Gain (200-300) Life per Ignited Enemy Killed
 Gain 20% of Physical Damage as Extra Fire Damage
 10% chance to Ignite
-Socketed Gems are Supported by Level 20 Returning Projectiles
+Socketed Gems are Supported by Level 20 Returning Projectiles (mutated)
 Arrows deal 30 to 50 Added Fire Damage for each time they've Pierced
 ]],[[
-Xoph's Inception Foulborn 2
+Foulborn Xoph's Inception 2
 Citadel Bow
 Upgrade: Removed Mod: Gain 20% of Physical Damage as Extra Fire Damage
 Upgrade: Added Mod: Ignites you inflict deal Damage (20—40)% faster
 Requires Level 23, 80 Dex
 (160-190)% increased Physical Damage
 Gain (200-300) Life per Ignited Enemy Killed
-Ignites you inflict deal Damage (20—40)% faster
+Ignites you inflict deal Damage (20—40)% faster (mutated)
 10% chance to Ignite
 Projectiles Pierce all Burning Enemies
 Arrows deal 30 to 50 Added Fire Damage for each time they've Pierced
 ]],[[
-Xoph's Inception Foulborn 3
+Foulborn Xoph's Inception 3
 Citadel Bow
 Upgrade: Removed Mod: Projectiles Pierce all Burning Enemies
 Upgrade: Removed Mod: Gain 20% of Physical Damage as Extra Fire Damage
@@ -140,48 +138,48 @@ Upgrade: Added Mod: Ignites you inflict deal Damage (20—40)% faster
 Requires Level 23, 80 Dex
 (160-190)% increased Physical Damage
 Gain (200-300) Life per Ignited Enemy Killed
-Ignites you inflict deal Damage (20—40)% faster
+Ignites you inflict deal Damage (20—40)% faster (mutated)
 10% chance to Ignite
-Socketed Gems are Supported by Level 20 Returning Projectiles
+Socketed Gems are Supported by Level 20 Returning Projectiles (mutated)
 Arrows deal 30 to 50 Added Fire Damage for each time they've Pierced
 ]],[[
-Xoph's Nurture Foulborn 1
+Foulborn Xoph's Nurture 1
 Citadel Bow
 Upgrade: Removed Mod: 50% of Physical Damage Converted to Fire Damage
 Upgrade: Added Mod: All Damage can Ignite
 Requires Level 64, 185 Dex
 Socketed Gems are Supported by Level 20 Ignite Proliferation
 (165-195)% increased Physical Damage
-All Damage can Ignite
+All Damage can Ignite (mutated)
 10% chance to Ignite
 Recover (40-60) Life when you Ignite an Enemy
 ]],[[
-Xoph's Nurture Foulborn 2
+Foulborn Xoph's Nurture 2
 Citadel Bow
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 20 Ignite Proliferation
 Upgrade: Added Mod: Socketed Gems are Supported by Level 30 Immolate
 Requires Level 64, 185 Dex
-Socketed Gems are Supported by Level 30 Immolate
+Socketed Gems are Supported by Level 30 Immolate (mutated)
 (165-195)% increased Physical Damage
 50% of Physical Damage Converted to Fire Damage
 10% chance to Ignite
 Recover (40-60) Life when you Ignite an Enemy
 ]],[[
-Xoph's Nurture Foulborn 3
+Foulborn Xoph's Nurture 3
 Citadel Bow
 Upgrade: Removed Mod: 50% of Physical Damage Converted to Fire Damage
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 20 Ignite Proliferation
 Upgrade: Added Mod: All Damage can Ignite
 Upgrade: Added Mod: Socketed Gems are Supported by Level 30 Immolate
 Requires Level 64, 185 Dex
-Socketed Gems are Supported by Level 30 Immolate
+Socketed Gems are Supported by Level 30 Immolate (mutated)
 (165-195)% increased Physical Damage
-All Damage can Ignite
+All Damage can Ignite (mutated)
 10% chance to Ignite
 Recover (40-60) Life when you Ignite an Enemy
 ]],
 [[
-Quill Rain Foulborn 1
+Foulborn Quill Rain 1
 Short Bow
 Upgrade: Removed Mod: (50—100)% increased Projectile Speed
 Upgrade: Added Mod: (40—60)% increased Area of Effect
@@ -190,11 +188,11 @@ Requires Level 5, 26 Dex
 100% increased Physical Damage
 100% increased Attack Speed
 +(25-50) to Accuracy Rating
-(40—60)% increased Area of Effect
+(40—60)% increased Area of Effect (mutated)
 30% less Damage
 Gain 2 Mana per Enemy Hit with Attacks
 ]],[[
-Quill Rain Foulborn 2
+Foulborn Quill Rain 2
 Short Bow
 Upgrade: Removed Mod: Grants 2 Mana per Enemy Hit
 Upgrade: Added Mod: Insufficient Mana doesn't prevent your Bow Attacks
@@ -205,9 +203,9 @@ Requires Level 5, 26 Dex
 +(25-50) to Accuracy Rating
 (50-100)% increased Projectile Speed
 30% less Damage
-Insufficient Mana doesn't prevent your Bow Attacks
+Insufficient Mana doesn't prevent your Bow Attacks (mutated)
 ]],[[
-Quill Rain Foulborn 3
+Foulborn Quill Rain 3
 Short Bow
 Upgrade: Removed Mod: (50—100)% increased Projectile Speed
 Upgrade: Removed Mod: Grants 2 Mana per Enemy Hit
@@ -218,12 +216,12 @@ Requires Level 5, 26 Dex
 100% increased Physical Damage
 100% increased Attack Speed
 +(25-50) to Accuracy Rating
-(40—60)% increased Area of Effect
+(40—60)% increased Area of Effect (mutated)
 30% less Damage
-Insufficient Mana doesn't prevent your Bow Attacks
+Insufficient Mana doesn't prevent your Bow Attacks (mutated)
 ]],
 [[
-Chin Sol Foulborn 1
+Foulborn Chin Sol 1
 Assassin Bow
 Upgrade: Removed Mod: 50% more Damage with Arrow Hits at Close Range
 Upgrade: Added Mod: 50% more Damage with Arrow Hits not at Close Range
@@ -234,10 +232,10 @@ Implicits: 2
 (100-140)% increased Physical Damage
 Adds 25 to 50 Fire Damage
 (10-14)% increased Attack Speed
-50% more Damage with Arrow Hits not at Close Range
+50% more Damage with Arrow Hits not at Close Range (mutated)
 Bow Knockback at Close Range
 ]],[[
-Chin Sol Foulborn 2
+Foulborn Chin Sol 2
 Assassin Bow
 Upgrade: Removed Mod: Bow Knockback at Close Range
 Upgrade: Added Mod: Projectiles Pierce all nearby Targets
@@ -249,9 +247,9 @@ Implicits: 2
 Adds 25 to 50 Fire Damage
 (10-14)% increased Attack Speed
 50% More Damage with Arrow Hits at Close Range
-Projectiles Pierce all nearby Targets
+Projectiles Pierce all nearby Targets (mutated)
 ]],[[
-Chin Sol Foulborn 3
+Foulborn Chin Sol 3
 Assassin Bow
 Upgrade: Removed Mod: 50% more Damage with Arrow Hits at Close Range
 Upgrade: Removed Mod: Bow Knockback at Close Range
@@ -264,11 +262,11 @@ Implicits: 2
 (100-140)% increased Physical Damage
 Adds 25 to 50 Fire Damage
 (10-14)% increased Attack Speed
-50% more Damage with Arrow Hits not at Close Range
-Projectiles Pierce all nearby Targets
+50% more Damage with Arrow Hits not at Close Range (mutated)
+Projectiles Pierce all nearby Targets (mutated)
 ]],
 [[
-Doomfletch Foulborn 1
+Foulborn Doomfletch 1
 Royal Bow
 Upgrade: Removed Mod: (10—14)% increased Attack Speed
 Upgrade: Added Mod: Socketed Gems are Supported by Level 25 Prismatic Burst
@@ -276,25 +274,25 @@ Requires Level 28, 95 Dex
 Implicits: 2
 (20-24)% increased Elemental Damage with Attack Skills
 Adds (12-16) to (20-24) Physical Damage
-Socketed Gems are Supported by Level 25 Prismatic Burst
+Socketed Gems are Supported by Level 25 Prismatic Burst (mutated)
 60% increased Mana Regeneration Rate
 Gain 100% of Weapon Physical Damage as Extra Damage of each Element
 ]],
 [[
-Death's Harp Foulborn 1
+Foulborn Death's Harp 1
 Death Bow
 Upgrade: Removed Mod: (90—105)% increased Physical Damage
 Upgrade: Added Mod: Gain (67—83)% of Physical Damage as Extra Chaos Damage
 Requires Level 32, 107 Dex
 Implicits: 1
 (30-50)% increased Critical Strike Chance
-Gain (67—83)% of Physical Damage as Extra Chaos Damage
+Gain (67—83)% of Physical Damage as Extra Chaos Damage (mutated)
 10% increased Attack Speed
 +50% to Global Critical Strike Multiplier
 Bow Attacks fire 2 additional Arrows
 ]],
 [[
-Darkscorn Foulborn 
+Foulborn Darkscorn 1
 Assassin Bow
 Upgrade: Removed Mod: 25% of Physical Damage Converted to Chaos Damage
 Upgrade: Added Mod: 25% chance to gain Unholy Might for 4 seconds on Critical Strike
@@ -304,7 +302,7 @@ Implicits: 2
 (60-80)% increased Physical Damage
 Adds (15-20) to (25-30) Physical Damage
 20% increased Attack Speed
-25% chance to gain Unholy Might for 4 seconds on Critical Strike
+25% chance to gain Unholy Might for 4 seconds on Critical Strike (mutated)
 (15-30)% increased Accuracy Rating
 25% of Physical Damage from Hits taken as Chaos Damage
 20% chance for Poisons inflicted with this Weapon to deal 300% more Damage
