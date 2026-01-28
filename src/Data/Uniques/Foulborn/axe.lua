c66e842aa36218f543c916a3e3ed03195a3bc8da
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:12:53 -0300

    Update axe.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/axe.lua
+++ src/Data/Uniques/Foulborn/axe.lua
@@ -2,9 +2,8 @@
 
 return {
 -- Weapon: One Handed Axe
-
 [[
-Moonbender's Wing Foulborn 1
+Foulborn Moonbender's Wing 1
 Tomahawk
 Upgrade: Removed Mod: (30—50)% increased Physical Damage
 Upgrade: Added Mod: (30—50)% increased Attack Speed
@@ -12,10 +11,10 @@ Implicits: 0
 Trigger Level 15 Lightning Warp on Hit with this Weapon
 (30-50)% increased Critical Strike Chance
 Hits with this Weapon gain (75-100)% of Physical Damage as Extra Cold or Lightning Damage
-(30—50)% increased Attack Speed
+(30—50)% increased Attack Speed (mutated)
 ]],
 [[
-Rigwald's Savagery Foulborn 1
+Foulborn Rigwald's Savagery 1
 Royal Axe
 Upgrade: Removed Mod: 40% increased Physical Attack Damage while Dual Wielding
 Upgrade: Added Mod: Your Melee Hits can't be Evaded while wielding a Sword
@@ -23,9 +22,9 @@ Implicits: 0
 Adds (50-70) to (135-165) Physical Damage
 (10-15)% increased Attack Speed
 +10 to Maximum Rage while wielding a Sword
-Your Melee Hits can't be Evaded while wielding a Sword
+Your Melee Hits can't be Evaded while wielding a Sword (mutated)
 ]],[[
-Uul-Netol's Kiss Foulborn 1
+Foulborn Uul-Netol's Kiss 1
 Vaal Axe
 Upgrade: Removed Mod: Curse Enemies with Vulnerability on Hit
 Upgrade: Added Mod: Warcries Exert 1 additional Attack
@@ -35,9 +34,9 @@ Implicits: 1
 15% reduced Attack Speed
 Exerted Attacks deal 200% increased Damage
 Exerted Attacks Knock Enemies Back on Hit
-Warcries Exert 1 additional Attack
+Warcries Exert 1 additional Attack (mutated)
 ]],[[
-Uul-Netol's Kiss Foulborn 2
+Foulborn Uul-Netol's Kiss 2
 Vaal Axe
 Upgrade: Removed Mod: Exerted Attacks deal 200% increased Damage
 Upgrade: Added Mod: 500% increased Warcry Cooldown Recovery Rate
@@ -47,9 +46,9 @@ Implicits: 1
 15% reduced Attack Speed
 Curse Enemies with Vulnerability on Hit
 Exerted Attacks Knock Enemies Back on Hit
-500% increased Warcry Cooldown Recovery Rate
+500% increased Warcry Cooldown Recovery Rate (mutated)
 ]],[[
-Uul-Netol's Kiss Foulborn 3
+Foulborn Uul-Netol's Kiss 3
 Vaal Axe
 Upgrade: Removed Mod: Curse Enemies with Vulnerability on Hit
 Upgrade: Removed Mod: Exerted Attacks deal 200% increased Damage
@@ -60,11 +59,11 @@ Implicits: 1
 (230-270)% increased Physical Damage
 15% reduced Attack Speed
 Exerted Attacks Knock Enemies Back on Hit
-Warcries Exert 1 additional Attack
-500% increased Warcry Cooldown Recovery Rate
+Warcries Exert 1 additional Attack (mutated)
+500% increased Warcry Cooldown Recovery Rate (mutated)
 ]],
 [[
-Uul-Netol's Embrace Foulborn 1
+Foulborn Uul-Netol's Embrace 1
 Vaal Axe
 Upgrade: Removed Mod: 25% chance to cause Bleeding on Hit
 Upgrade: Added Mod: Bleeding you inflict deals Damage (20—40)% faster
@@ -73,9 +72,9 @@ Implicits: 1
 Trigger Level 20 Bone Nova when you Hit a Bleeding Enemy
 (280-320)% increased Physical Damage
 (30-25)% reduced Attack Speed
-Bleeding you inflict deals Damage (20—40)% faster
+Bleeding you inflict deals Damage (20—40)% faster (mutated)
 ]],[[
-Uul-Netol's Embrace Foulborn 2
+Foulborn Uul-Netol's Embrace 2
 Vaal Axe
 Upgrade: Removed Mod: (25—30)% reduced Attack Speed
 Upgrade: Added Mod: 80% reduced Intelligence
@@ -84,9 +83,9 @@ Implicits: 1
 Trigger Level 20 Bone Nova when you Hit a Bleeding Enemy
 (280-320)% increased Physical Damage
 Attacks have 25% chance to inflict Bleeding
-80% reduced Intelligence
+80% reduced Intelligence (mutated)
 ]],[[
-Uul-Netol's Embrace Foulborn 3
+Foulborn Uul-Netol's Embrace 3
 Vaal Axe
 Upgrade: Removed Mod: 25% chance to cause Bleeding on Hit
 Upgrade: Removed Mod: (25—30)% reduced Attack Speed
@@ -96,12 +95,11 @@ Implicits: 1
 25% chance to Maim on Hit
 Trigger Level 20 Bone Nova when you Hit a Bleeding Enemy
 (280-320)% increased Physical Damage
-Bleeding you inflict deals Damage (20—40)% faster
-80% reduced Intelligence
+Bleeding you inflict deals Damage (20—40)% faster (mutated)
+80% reduced Intelligence (mutated)
 ]],
-
 [[
-The Harvest Foulborn 1
+Foulborn The Harvest 1
 Jasper Chopper
 Upgrade: Removed Mod: 120—140)% increased Physical Damage
 Upgrade: Added Mod: (120—140)% increased Spell Damage
@@ -109,10 +107,10 @@ Implicits: 0
 1.2% of Damage Leeched as Life on Critical Strike
 3% increased Global Critical Strike Chance per Level
 Gain a Flask Charge when you deal a Critical Strike
-(120—140)% increased Spell Damage
+(120—140)% increased Spell Damage (mutated)
 ]],
 [[
-Ngamahu's Flame Foulborn 1
+Foulborn Ngamahu's Flame 1
 Abyssal Axe
 Upgrade: Removed Mod: Damage Penetrates 20% Fire Resistance
 Upgrade: Added Mod: Attacks fire 3 additional Projectiles
@@ -121,10 +119,10 @@ Implicits: 0
 (170-190)% increased Physical Damage
 (8-12)% increased Attack Speed
 60% of Physical Damage Converted to Fire Damage
-Attacks fire 3 additional Projectiles
+Attacks fire 3 additional Projectiles (mutated)
 ]],
 [[
-Kaom's Primacy Foulborn 1
+Foulborn Kaom's Primacy 1
 Karui Chopper
 Upgrade: Removed Mod: Culling Strike
 Upgrade: Added Mod: +1 to Maximum Endurance Charges
@@ -134,7 +132,6 @@ Gain 20 Life per Enemy Killed
 +(150-250) to Accuracy Rating
 Gain 5 Rage on Melee Hit
 Every Rage also grants 1% of Physical Damage as Extra Fire Damage
-+1 to Maximum Endurance Charges
++1 to Maximum Endurance Charges (mutated)
 ]],
-
 }
