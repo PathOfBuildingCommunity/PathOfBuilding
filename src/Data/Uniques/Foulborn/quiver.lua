36faceb38e971348b54be6a21404eb1747612f54
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:13:33 -0300

    Update quiver.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/quiver.lua
+++ src/Data/Uniques/Foulborn/quiver.lua
@@ -2,26 +2,22 @@
 
 return {
 -- Quiver
-
-
-
-
 [[
-Drillneck Foulborn 1
+Foulborn Drillneck 1
 Penetrating Arrow Quiver
 Upgrade: Removed Mod: (8—12)% increased Attack Speed
 Upgrade: Added Mod: (20—30)% increased Impale Effect
 Requires Level 36
 Implicits: 1
 Arrows Pierce an additional Target
-(20—30)% increased Impale Effect
+(20—30)% increased Impale Effect (mutated)
 +350 to Evasion Rating
 +(40-50) to maximum Life
 Adds (10-14) to (19-24) Physical Damage to Attacks with Bows
 Arrows deal 50% increased Damage with Hits and Ailments to Targets they Pierce
 ]],
 [[
-Rearguard Foulborn 1
+Foulborn Rearguard 1
 Blunt Arrow Quiver
 Upgrade: Removed Mod: (20—30)% increased Projectile Speed
 Upgrade: Added Mod: 50% reduced Enemy Stun Threshold with Bows
@@ -30,12 +26,12 @@ Implicits: 1
 (20-24)% Chance to Block Attack Damage
 (12-15)% Chance to Block Spell Damage
 +(400-450) to Armour
-50% reduced Enemy Stun Threshold with Bows
+50% reduced Enemy Stun Threshold with Bows (mutated)
 50% increased Stun and Block Recovery
 (15-20)% increased Projectile Damage
 ]],
 [[
-Soul Strike Foulborn 1
+Foulborn Soul Strike 1
 Spike-Point Arrow Quiver
 Upgrade: Removed Mod: +(100—120) to maximum Energy Shield
 Upgrade: Added Mod: (10—15)% of Maximum Life Converted to Energy Shield
@@ -45,11 +41,11 @@ Implicits: 1
 +30 to Dexterity
 Adds (13-18) to (26-32) Chaos Damage to Attacks
 (8-12)% increased Attack Speed
-(10—15)% of Maximum Life Converted to Energy Shield
+(10—15)% of Maximum Life Converted to Energy Shield (mutated)
 40% reduced Energy Shield Recharge Rate
 80% faster start of Energy Shield Recharge
 ]],[[
-Soul Strike Foulborn 2
+Foulborn Soul Strike 2
 Spike-Point Arrow Quiver
 Upgrade: Removed Mod: 80% faster start of Energy Shield Recharge
 Upgrade: Added Mod: (10—15)% chance for Energy Shield Recharge to start when you use a Skill
@@ -61,9 +57,9 @@ Adds (13-18) to (26-32) Chaos Damage to Attacks
 (8-12)% increased Attack Speed
 +(100-120) to maximum Energy Shield
 40% reduced Energy Shield Recharge Rate
-(10—15)% chance for Energy Shield Recharge to start when you use a Skill
+(10—15)% chance for Energy Shield Recharge to start when you use a Skill (mutated)
 ]],[[
-Soul Strike Foulborn 3
+Foulborn Soul Strike 3
 Spike-Point Arrow Quiver
 Upgrade: Removed Mod: +(100—120) to maximum Energy Shield
 Upgrade: Removed Mod: 80% faster start of Energy Shield Recharge
@@ -75,12 +71,12 @@ Implicits: 1
 +30 to Dexterity
 Adds (13-18) to (26-32) Chaos Damage to Attacks
 (8-12)% increased Attack Speed
-(10—15)% of Maximum Life Converted to Energy Shield
+(10—15)% of Maximum Life Converted to Energy Shield (mutated)
 40% reduced Energy Shield Recharge Rate
-(10—15)% chance for Energy Shield Recharge to start when you use a Skill
+(10—15)% chance for Energy Shield Recharge to start when you use a Skill (mutated)
 ]],
 [[
-Rigwald's Quills Foulborn 1
+Foulborn Rigwald's Quills 1
 Two-Point Arrow Quiver
 Upgrade: Removed Mod: (30—50)% increased Projectile Damage
 Upgrade: Added Mod: (30—50)% chance to Aggravate Bleeding on targets you Hit with Attacks
@@ -91,11 +87,11 @@ Implicits: 1
 +(20-30)% to Lightning Resistance
 Gain 7 Life per Enemy Hit with Attacks
 20% reduced Projectile Speed
-(30—50)% chance to Aggravate Bleeding on targets you Hit with Attacks
+(30—50)% chance to Aggravate Bleeding on targets you Hit with Attacks (mutated)
 Arrows Fork
 ]],
 [[
-Maloney's Mechanism Foulborn 1
+Foulborn Maloney's Mechanism 1
 Ornate Quiver
 Upgrade: Removed Mod: (7—12)% increased Attack Speed
 Upgrade: Added Mod: Socketed Gems are Supported by Level 25 Arrow Nova
@@ -104,7 +100,7 @@ Implicits: 1
 Has 1 Socket
 Has 2 Sockets
 Trigger a Socketed Bow Skill when you Attack with a Bow, with a 1 second Cooldown
-Socketed Gems are Supported by Level 25 Arrow Nova
+Socketed Gems are Supported by Level 25 Arrow Nova (mutated)
 +(50-70) to maximum Life
 5% chance to Blind Enemies on Hit with Attacks
 ]],
