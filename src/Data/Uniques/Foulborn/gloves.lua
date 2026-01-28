63fb573752fcd9ba5995560c4ecee2e467114c1d
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:13:21 -0300

    Update gloves.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/gloves.lua
+++ src/Data/Uniques/Foulborn/gloves.lua
@@ -1,10 +1,8 @@
 -- Item data (c) Grinding Gear Games
 
 return {
-
-
 [[
-Maligaro's Virtuosity Foulborn 1
+Foulborn Maligaro's Virtuosity 1
 Deerskin Gloves
 Upgrade: Removed Mod: Your Critical Strike Multiplier is 300%
 Upgrade: Added Mod: Your Action Speed is at least 90% of base value
@@ -12,21 +10,21 @@ Requires Level 21, 33 Dex
 +(20-30) to Dexterity
 5% increased Attack Speed
 50% increased Global Critical Strike Chance
-Your Action Speed is at least 90% of base value
+Your Action Speed is at least 90% of base value (mutated)
 (60-80)% increased Evasion Rating
 ]],[[
-Maligaro's Virtuosity Foulborn 2
+Foulborn Maligaro's Virtuosity 2
 Deerskin Gloves
 Upgrade: Removed Mod: 50% increased Global Critical Strike Chance
 Upgrade: Added Mod: 50% increased Effect of non-Damaging Ailments you inflict with Critical Strikes
 Requires Level 21, 33 Dex
 +(20-30) to Dexterity
 5% increased Attack Speed
-50% increased Effect of non-Damaging Ailments you inflict with Critical Strikes
+50% increased Effect of non-Damaging Ailments you inflict with Critical Strikes (mutated)
 Your Critical Strike Multiplier is 300%
 (60-80)% increased Evasion Rating
 ]],[[
-Maligaro's Virtuosity Foulborn 3
+Foulborn Maligaro's Virtuosity 3
 Deerskin Gloves
 Upgrade: Removed Mod: Your Critical Strike Multiplier is 300%
 Upgrade: Removed Mod: 50% increased Global Critical Strike Chance
@@ -35,29 +33,29 @@ Upgrade: Added Mod: 50% increased Effect of non-Damaging Ailments you inflict wi
 Requires Level 21, 33 Dex
 +(20-30) to Dexterity
 5% increased Attack Speed
-50% increased Effect of non-Damaging Ailments you inflict with Critical Strikes
-Your Action Speed is at least 90% of base value
+50% increased Effect of non-Damaging Ailments you inflict with Critical Strikes (mutated)
+Your Action Speed is at least 90% of base value (mutated)
 (60-80)% increased Evasion Rating
 ]],
 [[
-Facebreaker Foulborn 1
+Foulborn Facebreaker 1
 Strapped Mitts
 Upgrade: Removed Mod: +30% to Global Critical Strike Multiplier
 Upgrade: Added Mod: (20—30)% increased Area of Effect while Unarmed
 Requires Level 16, 14 Dex, 14 Int
-(20—30)% increased Area of Effect while Unarmed
+(20—30)% increased Area of Effect while Unarmed (mutated)
 10% reduced Enemy Stun Threshold
 (600-1000)% more Physical Damage with Unarmed Melee Attacks
 Extra gore
 ]],
 [[
-Snakebite Foulborn 1
+Foulborn Snakebite 1
 Assassin's Mitts
 Upgrade: Removed Mod: +(60—70) to maximum Life
 Upgrade: Added Mod: +(100—130) to maximum Energy Shield
 Requires Level 58, 45 Dex, 45 Int
 (150-180)% increased Evasion Rating
-+(100—130) to maximum Energy Shield
++(100—130) to maximum Energy Shield (mutated)
 2% increased Attack Speed per Frenzy Charge
 6% increased Accuracy Rating per Frenzy Charge
 10% reduced Frenzy Charge Duration per Frenzy Charge
@@ -65,7 +63,7 @@ Attacks have 60% chance to Poison while at maximum Frenzy Charges
 +5% to Damage over Time Multiplier for Poison per Frenzy Charge
 ]],
 [[
-Shadows and Dust Foulborn 1
+Foulborn Shadows and Dust 1
 Clasped Mitts
 League: Rampage
 Upgrade: Removed Mod: 0.2% of Attack Damage Leeched as Mana
@@ -74,24 +72,24 @@ Requires Level 31, 25 Dex, 25 Int
 (20-30)% increased Global Critical Strike Chance
 +(20-30)% to Global Critical Strike Multiplier
 (100-130)% increased Evasion and Energy Shield
-(20—30)% increased Blind Effect
+(20—30)% increased Blind Effect (mutated)
 Creates a Smoke Cloud on Rampage
 Gain Unholy Might for 3 seconds on Rampage
 Rampage
 ]],
 [[
-The Embalmer Foulborn 1
+Foulborn The Embalmer 1
 Carnal Mitts
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 20 Vile Toxins
 Upgrade: Added Mod: Poisons you inflict deal Damage (15—20)% faster
 Requires Level 50, 39 Dex, 39 Int
-Poisons you inflict deal Damage (15—20)% faster
+Poisons you inflict deal Damage (15—20)% faster (mutated)
 Adds (13-17) to (23-29) Chaos Damage
 +(50-70) to maximum Life
 +(17-29)% to Chaos Resistance
 (20-25)% increased Poison Duration
 ]],[[
-The Embalmer Foulborn 2
+Foulborn The Embalmer 2
 Carnal Mitts
 Upgrade: Removed Mod: (20—25)% increased Poison Duration
 Upgrade: Added Mod: On Killing a Poisoned Enemy, nearby Enemies are Poisoned
@@ -100,23 +98,23 @@ Socketed Gems are Supported by Level 20 Vile Toxins
 Adds (13-17) to (23-29) Chaos Damage
 +(50-70) to maximum Life
 +(17-29)% to Chaos Resistance
-On Killing a Poisoned Enemy, nearby Enemies are Poisoned
+On Killing a Poisoned Enemy, nearby Enemies are Poisoned (mutated)
 ]],[[
-The Embalmer Foulborn 3
+Foulborn The Embalmer 3
 Carnal Mitts
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 20 Vile Toxins
 Upgrade: Removed Mod: (20—25)% increased Poison Duration
 Upgrade: Added Mod: Poisons you inflict deal Damage (15—20)% faster
 Upgrade: Added Mod: On Killing a Poisoned Enemy, nearby Enemies are Poisoned
 Requires Level 50, 39 Dex, 39 Int
-Poisons you inflict deal Damage (15—20)% faster
+Poisons you inflict deal Damage (15—20)% faster (mutated)
 Adds (13-17) to (23-29) Chaos Damage
 +(50-70) to maximum Life
 +(17-29)% to Chaos Resistance
-On Killing a Poisoned Enemy, nearby Enemies are Poisoned
+On Killing a Poisoned Enemy, nearby Enemies are Poisoned (mutated)
 ]],
 [[
-Sadima's Touch Foulborn 1
+Foulborn Sadima's Touch 1
 Wool Gloves
 Upgrade: Removed Mod: (5—15)% increased Rarity of Items found
 Upgrade: Added Mod: (5—15)% increased Quantity of Gold Dropped by Slain Enemies
@@ -124,21 +122,21 @@ Requires Level 11
 Adds 4 to 8 Fire Damage to Attacks
 Adds 1 to 13 Lightning Damage to Attacks
 +18 to maximum Energy Shield
-(5—15)% increased Quantity of Gold Dropped by Slain Enemies
+(5—15)% increased Quantity of Gold Dropped by Slain Enemies (mutated)
 ]],
 [[
-Kalisa's Grace Foulborn 1
+Foulborn Kalisa's Grace 1
 Samite Gloves
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 18 Faster Casting
 Upgrade: Added Mod: Socketed Gems are Supported by Level 18 Focused Channelling
 Requires Level 55, 68 Int
-Socketed Gems are Supported by Level 18 Focused Channelling
+Socketed Gems are Supported by Level 18 Focused Channelling (mutated)
 +(20-30) to Intelligence
 +(50-80) to maximum Energy Shield
 +(50-70) to maximum Life
 Gain +2% to Critical Strike Chance for 2 seconds after Spending a total of 800 Mana
 ]],[[
-Kalisa's Grace Foulborn 2
+Foulborn Kalisa's Grace 2
 Samite Gloves
 Upgrade: Removed Mod: Gain +2% to Critical Strike Chance for 2 seconds after Spending a total of 800 Mana
 Upgrade: Added Mod: 50% reduced Mana Cost of Skills for 2 seconds after Spending a total of 800 Mana
@@ -147,47 +145,47 @@ Socketed Gems are Supported by Level 18 Faster Casting
 +(20-30) to Intelligence
 +(50-80) to maximum Energy Shield
 +(50-70) to maximum Life
-50% reduced Mana Cost of Skills for 2 seconds after Spending a total of 800 Mana
+50% reduced Mana Cost of Skills for 2 seconds after Spending a total of 800 Mana (mutated)
 ]],[[
-Kalisa's Grace Foulborn 3
+Foulborn Kalisa's Grace 3
 Samite Gloves
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 18 Faster Casting
 Upgrade: Removed Mod: Gain +2% to Critical Strike Chance for 2 seconds after Spending a total of 800 Mana
 Upgrade: Added Mod: Socketed Gems are Supported by Level 18 Focused Channelling
 Upgrade: Added Mod: 50% reduced Mana Cost of Skills for 2 seconds after Spending a total of 800 Mana
 Requires Level 55, 68 Int
-Socketed Gems are Supported by Level 18 Focused Channelling
+Socketed Gems are Supported by Level 18 Focused Channelling (mutated)
 +(20-30) to Intelligence
 +(50-80) to maximum Energy Shield
 +(50-70) to maximum Life
-50% reduced Mana Cost of Skills for 2 seconds after Spending a total of 800 Mana
+50% reduced Mana Cost of Skills for 2 seconds after Spending a total of 800 Mana (mutated)
 ]],
 [[
-Voidbringer Foulborn 1
+Foulborn Voidbringer 1
 Conjurer Gloves
 Upgrade: Removed Mod: (125—150)% increased Spell Critical Strike Chance
 Upgrade: Added Mod: (10—15)% increased Chaos Damage for each Corrupted Item Equipped
 Requires Level 55, 79 Int
 +1 to Level of Socketed Elemental Gems
-(10—15)% increased Chaos Damage for each Corrupted Item Equipped
+(10—15)% increased Chaos Damage for each Corrupted Item Equipped (mutated)
 (180-250)% increased Energy Shield
 Lose (40-80) Mana when you use a Skill
 Gain (15-20) Energy Shield per Enemy Killed
 ]],
 [[
-Kaom's Spirit Foulborn 1
+Foulborn Kaom's Spirit 1
 Titan Gauntlets
 Upgrade: Removed Mod: +(20—30)% to Fire Resistance
 Upgrade: Added Mod: Inherent Rage Loss starts 2 seconds later
 +(50-70) to Maximum Life
-Inherent Rage Loss starts 2 seconds later
+Inherent Rage Loss starts 2 seconds later (mutated)
 (0.3-0.5)% of Physical Attack Damage Leeched as Life
 Life Recovery from Regeneration is not applied
 Regenerate 1 Rage per second for every 200 Life Recovery per second from Regeneration
 Does not delay Inherent Loss of Rage
 ]],
 [[
-Flesh and Spirit Foulborn 1
+Foulborn Flesh and Spirit 1
 Ironscale Gauntlets
 Upgrade: Removed Mod: Recover 20% of Life on Rampage
 Upgrade: Added Mod: Vaal Skills have (20—40)% increased Skill Effect Duration
@@ -195,17 +193,17 @@ Requires Level 15
 (6-9)% increased Attack Speed
 (80-120)% increased Armour and Evasion
 Regenerate (3-4) Life per second
-Vaal Skills have (20—40)% increased Skill Effect Duration
+Vaal Skills have (20—40)% increased Skill Effect Duration (mutated)
 Kills grant an additional Vaal Soul if you have Rampaged Recently
 Rampage
 ]],
 [[
-Wyrmsign Foulborn 1
+Foulborn Wyrmsign 1
 Wyrmscale Gauntlets
 Upgrade: Removed Mod: Socketed Gems are Supported by Level 5 Concentrated Effect
 Upgrade: Added Mod: Socketed Gems are Supported by Level 5 Manaforged Arrows
 Requires Level 49, 38 Str, 38 Dex
-Socketed Gems are Supported by Level 5 Manaforged Arrows
+Socketed Gems are Supported by Level 5 Manaforged Arrows (mutated)
 (120-160)% increased Armour and Evasion
 +(50-70) to maximum Life
 4% reduced Mana Cost per Endurance Charge
@@ -213,12 +211,12 @@ Gain Rampage while at Maximum Endurance Charges
 Lose all Endurance Charges when Rampage ends
 ]],
 [[
-Shackles of the Wretched Foulborn 1
+Foulborn Shackles of the Wretched 1
 Chain Gloves
 Upgrade: Removed Mod: (40—60)% increased Stun and Block Recovery
 Upgrade: Added Mod: (-30—30)% reduced Duration of Curses on you
 Requires Level 7, 17 Dex
-(-30—30)% reduced Duration of Curses on you
+(-30—30)% reduced Duration of Curses on you (mutated)
 Hexes applied by Socketed Curse Skills are Reflected back to you
 You cannot be Chilled for 3 seconds after being Chilled
 You cannot be Frozen for 3 seconds after being Frozen
@@ -227,7 +225,7 @@ You cannot be Shocked for 3 seconds after being Shocked
 You grant (4-6) Frenzy Charges to allies on Death
 ]],
 [[
-Null and Void Foulborn 1
+Foulborn Null and Void 1
 Legion Gloves
 Upgrade: Removed Mod: (20—40)% increased Mana Regeneration Rate
 Upgrade: Added Mod: (15—25)% increased Life Regeneration rate
@@ -235,13 +233,13 @@ Requires Level 57, 44 Str, 44 Int
 +(20-30) to Strength
 (150-180)% increased Armour and Energy Shield
 +(50-70) to maximum Life
-(15—25)% increased Life Regeneration rate
+(15—25)% increased Life Regeneration rate (mutated)
 Dispels Elemental Ailments on Rampage
 Gain Immunity to Physical Damage for 1.5 seconds on Rampage
 Rampage
 ]],
 [[
-Thunderfist Foulborn 1
+Foulborn Thunderfist 1
 Murder Mitts
 Upgrade: Removed Mod: 100% increased Effect of Lightning Ailments
 Upgrade: Added Mod: Herald of Thunder has 100% increased Buff Effect
@@ -249,10 +247,10 @@ Requires Level 67, 51 Dex, 51 Int
 Socketed Gems are Supported by Level 30 Added Lightning Damage
 10% increased Attack Speed
 (150-200)% increased Evasion and Energy Shield
-Herald of Thunder has 100% increased Buff Effect
+Herald of Thunder has 100% increased Buff Effect (mutated)
 ]],
 [[
-Asenath's Gentle Touch Foulborn 1
+Foulborn Asenath's Gentle Touch 1
 Silk Gloves
 Upgrade: Removed Mod: Curse Enemies with Temporal Chains on Hit
 Upgrade: Added Mod: Curse Enemies with Punishment on Hit
@@ -260,14 +258,14 @@ Requires Level 25, 39 Int
 +(20-30) to Intelligence
 +(60-80) to maximum Life
 +(60-80) to maximum Mana
-Curse Enemies with Punishment on Hit
+Curse Enemies with Punishment on Hit (mutated)
 Non-Aura Curses you inflict are not removed from Dying Enemies
 Enemies near Corpses affected by your Curses are Blinded
 Enemies killed near Corpses affected by your Curses explode, dealing
 3% of their Life as Physical Damage
 ]],
 [[
-Meginord's Vise Foulborn 1
+Foulborn Meginord's Vise 1
 Steel Gauntlets
 Upgrade: Removed Mod: +50 to Strength
 Upgrade: Added Mod: +700 Strength Requirement
@@ -275,27 +273,27 @@ Upgrade: Added Mod: (10—15)% chance to deal Triple Damage
 Requires Level 35, 52 Str
 Melee Strike Skills deal Splash Damage to surrounding targets
 +700 Strength Requirement
-(10—15)% chance to deal Triple Damage
+(10—15)% chance to deal Triple Damage (mutated)
 (150-200)% increased Armour
 100% increased Knockback Distance
 Melee Hits with Strike Skills always Knockback
 ]],
 [[
-Doryani's Fist Foulborn 1
+Foulborn Doryani's Fist 1
 Vaal Gauntlets
 Upgrade: Removed Mod: 30% chance to Shock
 Upgrade: Added Mod: 30% chance to Sap Enemies
 Requires Level 63, 100 Str
 Grants Level 20 Doryani's Touch Skill
 +(80-100) to maximum Energy Shield
-30% chance to Sap Enemies
+30% chance to Sap Enemies (mutated)
 Adds (150-225) to (525-600) Lightning Damage to Unarmed Melee Hits
 Adds (90-135) to (315-360) Lightning Damage to Spells while Unarmed
 +(200-250) Energy Shield gained on Killing a Shocked Enemy
 30% increased Effect of Lightning Ailments
 ]],
 [[
-Veruso's Battering Rams Foulborn 1
+Foulborn Veruso's Battering Rams 1
 Titan Gauntlets
 Upgrade: Removed Mod: You cannot be Shocked while at maximum Endurance Charges
 Upgrade: Added Mod: Cannot be Ignited while at maximum Endurance Charges
@@ -305,21 +303,21 @@ Requires Level 69, 98 Str
 (3-5)% increased Movement Speed
 50% increased Stun Duration on you
 4% increased Melee Damage per Endurance Charge
-Cannot be Ignited while at maximum Endurance Charges
+Cannot be Ignited while at maximum Endurance Charges (mutated)
 ]],
 [[
-Aurseize Foulborn 1
+Foulborn Aurseize 1
 Steelscale Gauntlets
 Upgrade: Removed Mod: +15% to all Elemental Resistances
 Upgrade: Added Mod: 15% increased Quantity of Gold Dropped by Slain Enemies
 Requires Level 36, 29 Str, 29 Dex
 (40-60)% increased Armour and Evasion
-15% increased Quantity of Gold Dropped by Slain Enemies
+15% increased Quantity of Gold Dropped by Slain Enemies (mutated)
 (40-50)% increased Rarity of Items found
 5% reduced Movement Speed
 ]],
 [[
-Vaal Caress Foulborn 1
+Foulborn Vaal Caress 1
 Bronzescale Gauntlets
 Upgrade: Removed Mod: You gain Onslaught for 20 seconds on using a Vaal Skill
 Upgrade: Added Mod: You gain Adrenaline for 3 seconds on using a Vaal Skill
@@ -328,16 +326,16 @@ Upgrade: Added Mod: You gain Adrenaline for 3 seconds on using a Vaal Skill
 (120-140)% increased Armour and Evasion
 +(50-70) to maximum Life
 +40% to Cold Resistance
-You gain Adrenaline for 3 seconds on using a Vaal Skill
+You gain Adrenaline for 3 seconds on using a Vaal Skill (mutated)
 ]],
 [[
-Repentance Foulborn 1
+Foulborn Repentance 1
 Crusader Gloves
 Upgrade: Removed Mod: (6—12)% increased Strength
 Upgrade: Added Mod: (12—16)% increased Intelligence
 Requires Level 66, 306 Str, 306 Int
 500% increased Attribute Requirements
-(12—16)% increased Intelligence
+(12—16)% increased Intelligence (mutated)
 (400-500)% increased Armour and Energy Shield
 Iron Will
 ]],
