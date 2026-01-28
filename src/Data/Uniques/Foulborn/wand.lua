cd3c5883edaf31468961241ebb05d43e1b0e1269
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:13:49 -0300

    Update wand.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/wand.lua
+++ src/Data/Uniques/Foulborn/wand.lua
@@ -2,10 +2,8 @@
 
 return {
 -- Weapon: Wand
-
-
 [[
-Ashcaller Foulborn 
+Foulborn Ashcaller 1
 Goat's Horn
 Upgrade: Removed Mod: Adds (20—24) to (38—46) Fire Damage to Spells
 Upgrade: Added Mod: Minions gain (20—40)% of Physical Damage as Extra Fire Damage
@@ -13,33 +11,33 @@ Implicits: 1
 Adds (1-2) to (3-4) Fire Damage to Spells and Attacks
 25% chance to Trigger Level 10 Summon Raging Spirit on Kill
 Adds (20-24) to (38-46) Fire Damage
-Minions gain (20—40)% of Physical Damage as Extra Fire Damage
+Minions gain (20—40)% of Physical Damage as Extra Fire Damage (mutated)
 10% chance to Cover Enemies in Ash on Hit
 ]],
 [[
-Tulborn Foulborn 1
+Foulborn Tulborn 1
 Opal Wand
 Upgrade: Removed Mod: Adds (120—140) to (150—170) Cold Damage to Spells
 Upgrade: Added Mod: +(2—4) to Level of all Cold Spell Skill Gems
 Implicits: 1
 Adds (14-29) to (42-47) Cold Damage to Spells and Attacks
-+(2—4) to Level of all Cold Spell Skill Gems
++(2—4) to Level of all Cold Spell Skill Gems (mutated)
 Gain a Power Charge on Killing a Frozen Enemy
 Cold Exposure you inflict applies an extra -12% to Cold Resistance
 +(20-25) Mana gained on Killing a Frozen Enemy
 ]],[[
-Tulborn Foulborn 2
+Foulborn Tulborn 2
 Opal Wand
 Upgrade: Removed Mod: Gain a Power Charge on Killing a Frozen Enemy
 Upgrade: Added Mod: Gain a Power Charge after Spending a total of 200 Mana
 Implicits: 1
 Adds (14-29) to (42-47) Cold Damage to Spells and Attacks
 Adds (120-140) to (150-170) Cold Damage to Spells
-Gain a Power Charge after Spending a total of 200 Mana
+Gain a Power Charge after Spending a total of 200 Mana (mutated)
 Cold Exposure you inflict applies an extra -12% to Cold Resistance
 +(20-25) Mana gained on Killing a Frozen Enemy
 ]],[[
-Tulborn Foulborn 3
+Foulborn Tulborn 3
 Opal Wand
 Upgrade: Removed Mod: Adds (120—140) to (150—170) Cold Damage to Spells
 Upgrade: Removed Mod: Gain a Power Charge on Killing a Frozen Enemy
@@ -47,13 +45,13 @@ Upgrade: Added Mod: +(2—4) to Level of all Cold Spell Skill Gems
 Upgrade: Added Mod: Gain a Power Charge after Spending a total of 200 Mana
 Implicits: 1
 Adds (14-29) to (42-47) Cold Damage to Spells and Attacks
-+(2—4) to Level of all Cold Spell Skill Gems
-Gain a Power Charge after Spending a total of 200 Mana
++(2—4) to Level of all Cold Spell Skill Gems (mutated)
+Gain a Power Charge after Spending a total of 200 Mana (mutated)
 Cold Exposure you inflict applies an extra -12% to Cold Resistance
 +(20-25) Mana gained on Killing a Frozen Enemy
 ]],
 [[
-Tulfall Foulborn 
+Foulborn Tulfall 1
 Opal Wand
 Upgrade: Removed Mod: Adds 50 to 70 Cold Damage to Spells per Power Charge
 Upgrade: Added Mod: +(15—20)% to Cold Damage over Time Multiplier per Power Charge
@@ -61,12 +59,40 @@ Implicits: 1
 Adds (14-29) to (42-47) Cold Damage to Spells and Attacks
 (10-20)% increased Cast Speed
 Gain a Power Charge on Killing a Frozen Enemy
-+(15—20)% to Cold Damage over Time Multiplier per Power Charge
++(15—20)% to Cold Damage over Time Multiplier per Power Charge (mutated)
 Lose all Power Charges on reaching Maximum Power Charges
 Gain a Frenzy Charge on reaching Maximum Power Charges
 ]],
 [[
-Midnight Bargain Foulborn 1
+Foulborn Tulfall 2
+Opal Wand
+Upgrade: Removed Mod: Gain a Power Charge on Killing a Frozen Enemy
+Upgrade: Added Mod:	Gain a Power Charge after Spending a total of 200 Mana
+Implicits: 1
+Adds (14-29) to (42-47) Cold Damage to Spells and Attacks
+(10-20)% increased Cast Speed
+Gain a Power Charge after Spending a total of 200 Mana (mutated)
+Adds 50 to 70 Cold Damage to Spells per Power Charge
+Lose all Power Charges on reaching Maximum Power Charges
+Gain a Frenzy Charge on reaching Maximum Power Charges
+]],
+[[
+Foulborn Tulfall 3
+Opal Wand
+Upgrade: Removed Mod: Adds 50 to 70 Cold Damage to Spells per Power Charge
+Upgrade: Removed Mod:Gain a Power Charge on Killing a Frozen Enemy
+Upgrade: Added Mod: +(15—20)% to Cold Damage over Time Multiplier per Power Charge
+Upgrade: Added Mod:Gain a Power Charge after Spending a total of 200 Mana
+Implicits: 1
+Adds (14-29) to (42-47) Cold Damage to Spells and Attacks
+(10-20)% increased Cast Speed
+Gain a Power Charge after Spending a total of 200 Mana (mutated)
++(15—20)% to Cold Damage over Time Multiplier per Power Charge (mutated)
+Lose all Power Charges on reaching Maximum Power Charges
+Gain a Frenzy Charge on reaching Maximum Power Charges
+]],
+[[
+Foulborn Midnight Bargain 1
 Calling Wand
 Upgrade: Removed Mod: Cannot be used with Chaos Inoculation
 Upgrade: Removed Mod: Reserves 30% of Life
@@ -79,9 +105,9 @@ Minions deal (50-70)% increased Damage
 +1 to Maximum number of Raised Zombies
 +1 to Maximum number of Spectres
 +1 to Maximum number of Skeletons
-Lose 0.5% Life and Energy Shield per Second per Minion
+Lose 0.5% Life and Energy Shield per Second per Minion (mutated)
 ]],[[
-Midnight Bargain Foulborn 2
+Foulborn Midnight Bargain 2
 Calling Wand
 Upgrade: Removed Mod: +1 to maximum number of Raised Zombies
 Upgrade: Removed Mod: +1 to maximum number of Spectres
@@ -93,10 +119,10 @@ Cannot be used with Chaos Inoculation
 +(10-20) to Intelligence
 Minions have (20-30)% increased Movement Speed
 Minions deal (50-70)% increased Damage
-+2 to maximum number of Summoned Golems
++2 to maximum number of Summoned Golems (mutated)
 Reserves 30% of Life
 ]],[[
-Midnight Bargain Foulborn 3
+Foulborn Midnight Bargain 3
 Calling Wand
 Upgrade: Removed Mod: Cannot be used with Chaos Inoculation
 Upgrade: Removed Mod: Reserves 30% of Life
@@ -110,11 +136,11 @@ Minions deal (12-16)% increased Damage
 +(10-20) to Intelligence
 Minions have (20-30)% increased Movement Speed
 Minions deal (50-70)% increased Damage
-+2 to maximum number of Summoned Golems
-Lose 0.5% Life and Energy Shield per Second per Minion
++2 to maximum number of Summoned Golems (mutated)
+Lose 0.5% Life and Energy Shield per Second per Minion (mutated)
 ]],
 [[
-Piscator's Vigil Foulborn 1
+Foulborn Piscator's Vigil 1
 Kinetic Wand
 Upgrade: Removed Mod: (20—30)% increased Critical Strike Chance
 Upgrade: Added Mod: (30—50)% increased effect of Wishes granted by Ancient Fish
@@ -122,25 +148,25 @@ Implicits: 1
 Cannot roll Caster Modifiers
 No Physical Damage
 (10-18)% increased Attack Speed
-(30—50)% increased effect of Wishes granted by Ancient Fish
+(30—50)% increased effect of Wishes granted by Ancient Fish (mutated)
 +(340-400) to Accuracy Rating
 Attacks with this Weapon have (100-115)% increased Elemental Damage
 Damage with Weapons Penetrates 5% Elemental Resistances
 ]],
 [[
-The Poet's Pen Foulborn 1
+Foulborn The Poet's Pen 1
 Somatic Wand
 Upgrade: Removed Mod: Adds 3 to 5 Physical Damage to Attacks with this Weapon per 3 Player Levels
 Upgrade: Added Mod: Adds 3 to 5 Physical Damage to Spells per 3 Player Levels
 Implicits: 1
 Cannot roll Caster Modifiers
 +1 to Level of Socketed Active Skill Gems per 25 Player Levels
-Adds 3 to 5 Physical Damage to Spells per 3 Player Levels
+Adds 3 to 5 Physical Damage to Spells per 3 Player Levels (mutated)
 (8-12)% increased Attack Speed
 Trigger a Socketed Spell when you Attack with this Weapon, with a 0.25 second Cooldown
 ]],
 [[
-Void Battery Foulborn 1
+Foulborn Void Battery 1
 Prophecy Wand
 Upgrade: Removed Mod: (50—65)% increased Global Critical Strike Chance
 Upgrade: Added Mod: 3% increased Area of Effect per Power Charge
@@ -148,32 +174,32 @@ Implicits: 1
 (36-40)% increased Spell Damage
 80% reduced Spell Damage
 (10-20)% increased Cast Speed
-3% increased Area of Effect per Power Charge
+3% increased Area of Effect per Power Charge (mutated)
 +(40-50) to maximum Mana
 +1 to Maximum Power Charges
 25% increased Spell Damage per Power Charge
 ]],
 [[
-Apep's Rage Foulborn 1
+Foulborn Apep's Rage 1
 Omen Wand
 Upgrade: Removed Mod: Adds (90—130) to (140—190) Chaos Damage to Spells
 Upgrade: Added Mod: Skills gain Added Chaos Damage equal to (20—25)% of Mana Cost, if Mana Cost is not higher than the maximum you could spend
 Implicits: 1
 (27-31)% increased Spell Damage
-Skills gain Added Chaos Damage equal to (20—25)% of Mana Cost, if Mana Cost is not higher than the maximum you could spend
+Skills gain Added Chaos Damage equal to (20—25)% of Mana Cost, if Mana Cost is not higher than the maximum you could spend (mutated)
 (25-30)% increased Cast Speed
 +(5-10)% to Chaos Resistance
 Poisons you inflict deal Damage (30-50)% faster
 Lose 40 Mana when you use a Skill
 ]],
 [[
-Reverberation Rod Foulborn 1
+Foulborn Reverberation Rod 1
 Spiraled Wand
 Upgrade: Removed Mod: +2 to Level of Socketed Gems
 Upgrade: Added Mod: Socketed Gems are Supported by Level 1 Awakened Spell Cascade
 Implicits: 1
 Adds (1-2) to (9-11) Lightning Damage to Spells and Attacks
-Socketed Gems are Supported by Level 1 Awakened Spell Cascade
+Socketed Gems are Supported by Level 1 Awakened Spell Cascade (mutated)
 Socketed Gems are Supported by Level 10 Arcane Surge
 Socketed Gems are Supported by Level 10 Spell Echo
 Socketed Gems are Supported by Level 10 Controlled Destruction
