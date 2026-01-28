8cadacf81cd8d73ee5319277d7dcbef0aba1fe07
felinista-ux <elielgesing@gmail.com>
2026-01-28 17:13:10 -0300

    Update claw.lua
    Updated name description and mutated mod tag

--- src/Data/Uniques/Foulborn/claw.lua
+++ src/Data/Uniques/Foulborn/claw.lua
@@ -2,23 +2,22 @@
 
 return {
 -- Weapon: Claw
-
 [[
-Al Dhih Foulborn 1
+Foulborn Al Dhih 1
 Timeworn Claw
 Upgrade: Removed Mod: (100—120)% increased Physical Damage
 Upgrade: Added Mod: (100—120)% increased Chaos Damage
 Requires Level 26, 39 Dex, 56 Int
 Implicits: 1
 Grants 19 Life per Enemy Hit
-(100—120)% increased Chaos Damage
+(100—120)% increased Chaos Damage (mutated)
 3% of Physical Attack Damage Leeched as Life
 Enemies Taunted by your Warcries Explode on death, dealing 8% of their maximum Life as Chaos Damage
 25% increased Warcry Buff Effect
 Warcry Skills' Cooldown Time is 4 seconds
 ]],
 [[
-Rive Foulborn 1
+Foulborn Rive 1
 Terror Claw
 Upgrade: Removed Mod: 25% chance to cause Bleeding on Hit
 Upgrade: Added Mod: 25% chance to Crush on Hit
@@ -27,12 +26,12 @@ Implicits: 1
 2% of Physical Attack Damage Leeched as Life
 (60-80)% increased Physical Damage
 Adds (25-35) to (50-65) Physical Damage
-25% chance to Crush on Hit
+25% chance to Crush on Hit (mutated)
 2% increased Physical Damage over time per 10 Dexterity
 1% increased Bleed Duration per 12 Intelligence
 30% Chance to cause Bleeding Enemies to Flee on hit
 ]],[[
-Rive Foulborn 2
+Foulborn Rive 2
 Terror Claw
 Upgrade: Removed Mod: 1% increased Bleeding Duration per 12 Intelligence
 Upgrade: Added Mod: Physical Skills have 1% increased Duration per 12 Intelligence
@@ -43,10 +42,10 @@ Implicits: 1
 Adds (25-35) to (50-65) Physical Damage
 25% chance to cause Bleeding on Hit
 2% increased Physical Damage over time per 10 Dexterity
-Physical Skills have 1% increased Duration per 12 Intelligence
+Physical Skills have 1% increased Duration per 12 Intelligence (mutated)
 30% Chance to cause Bleeding Enemies to Flee on hit
 ]],[[
-Rive Foulborn 3
+Foulborn Rive 3
 Terror Claw
 Upgrade: Removed Mod:25% chance to cause Bleeding on Hit
 Upgrade: Removed Mod: 1% increased Bleeding Duration per 12 Intelligence
@@ -57,13 +56,13 @@ Implicits: 1
 2% of Physical Attack Damage Leeched as Life
 (60-80)% increased Physical Damage
 Adds (25-35) to (50-65) Physical Damage
-25% chance to Crush on Hit
+25% chance to Crush on Hit (mutated)
 2% increased Physical Damage over time per 10 Dexterity
-Physical Skills have 1% increased Duration per 12 Intelligence
+Physical Skills have 1% increased Duration per 12 Intelligence (mutated)
 30% Chance to cause Bleeding Enemies to Flee on hit
 ]],
 [[
-Hand of Thought and Motion Foulborn 1
+Foulborn Hand of Thought and Motion 1
 Imperial Claw
 Upgrade: Removed Mod: 5% increased Critical Strike Chance per 25 Intelligence
 Upgrade: Added Mod: 5% increased Accuracy Rating per 25 Intelligence
@@ -73,9 +72,9 @@ Grants 46 Life per Enemy Hit
 (8-12)% increased Intelligence
 Recover 1% of Life on Kill
 {variant:5}Adds 1 to 12 Lightning Damage to Attacks with this Weapon per 10 Dexterity
-5% increased Accuracy Rating per 25 Intelligence
+5% increased Accuracy Rating per 25 Intelligence (mutated)
 ]],[[
-Hand of Thought and Motion Foulborn 2
+Foulborn Hand of Thought and Motion 2
 Imperial Claw
 Upgrade: Removed Mod: Recover 1% of Life on Kill
 Upgrade: Added Mod: (8—12)% increased Strength
@@ -83,11 +82,11 @@ Implicits: 1
 Grants 46 Life per Enemy Hit
 (8-12)% increased Dexterity
 (8-12)% increased Intelligence
-(8—12)% increased Strength
+(8—12)% increased Strength (mutated)
 {variant:5}Adds 1 to 12 Lightning Damage to Attacks with this Weapon per 10 Dexterity
 5% increased Critical Strike Chance per 25 Intelligence
 ]],[[
-Hand of Thought and Motion Foulborn 3
+Foulborn Hand of Thought and Motion 3
 Imperial Claw
 Upgrade: Removed Mod: 5% increased Critical Strike Chance per 25 Intelligence
 Upgrade: Removed Mod: Recover 1% of Life on Kill
@@ -97,11 +96,11 @@ Implicits: 1
 Grants 46 Life per Enemy Hit
 (8-12)% increased Dexterity
 (8-12)% increased Intelligence
-(8—12)% increased Strength
+(8—12)% increased Strength (mutated)
 {variant:5}Adds 1 to 12 Lightning Damage to Attacks with this Weapon per 10 Dexterity
-5% increased Accuracy Rating per 25 Intelligence
+5% increased Accuracy Rating per 25 Intelligence (mutated)
 ]],[[
-Hand of Wisdom and Action Foulborn 1
+Foulborn Hand of Wisdom and Action 1
 Imperial Claw
 Upgrade: Removed Mod: 1% increased Attack Speed per 25 Dexterity
 Upgrade: Added Mod: +3% to Critical Strike Multiplier per 25 Dexterity
@@ -112,9 +111,9 @@ Grants 46 Life per Enemy Hit
 (8-12)% increased Intelligence
 1% of Attack Damage Leeched as Life
 {variant:5}Adds 1 to 12 Lightning Damage to Attacks with this Weapon per 10 Intelligence
-+3% to Critical Strike Multiplier per 25 Dexterity
++3% to Critical Strike Multiplier per 25 Dexterity (mutated)
 ]],[[
-Hand of Wisdom and Action Foulborn 2
+Foulborn Hand of Wisdom and Action 2
 Imperial Claw
 Upgrade: Removed Mod: 1% of Attack Damage Leeched as Life
 Upgrade: Added Mod: (8—12)% increased Strength
@@ -123,11 +122,11 @@ Implicits: 1
 Grants 46 Life per Enemy Hit
 (8-12)% increased Dexterity
 (8-12)% increased Intelligence
-(8—12)% increased Strength
+(8—12)% increased Strength (mutated)
 {variant:5}Adds 1 to 12 Lightning Damage to Attacks with this Weapon per 10 Intelligence
 1% increased Attack Speed per 25 Dexterity
 ]],[[
-Hand of Wisdom and Action Foulborn 3
+Foulborn Hand of Wisdom and Action 3
 Imperial Claw
 Upgrade: Removed Mod: 1% increased Attack Speed per 25 Dexterity
 Upgrade: Removed Mod: 1% of Attack Damage Leeched as Life
@@ -138,12 +137,12 @@ Implicits: 1
 Grants 46 Life per Enemy Hit
 (8-12)% increased Dexterity
 (8-12)% increased Intelligence
-(8—12)% increased Strength
+(8—12)% increased Strength (mutated)
 {variant:5}Adds 1 to 12 Lightning Damage to Attacks with this Weapon per 10 Intelligence
-+3% to Critical Strike Multiplier per 25 Dexterity
++3% to Critical Strike Multiplier per 25 Dexterity (mutated)
 ]],
 [[
-Cybil's Paw Foulborn 1
+Foulborn Cybil's Paw 1
 Thresher Claw
 Upgrade: Removed Mod: Gain (15—20) Life per Enemy Hit with Spells
 Upgrade: Added Mod: Recover Energy Shield equal to 1% of Evasion Rating when you Block
@@ -152,11 +151,7 @@ Implicits: 1
 Grants 25 Life per Enemy Hit
 (15-20)% increased Cast Speed
 +(30-40) to maximum Mana
-Recover Energy Shield equal to 1% of Evasion Rating when you Block
+Recover Energy Shield equal to 1% of Evasion Rating when you Block (mutated)
 8% increased Spell Damage per 5% Chance to Block Attack Damage
 ]],
-
-
-
-
 }
