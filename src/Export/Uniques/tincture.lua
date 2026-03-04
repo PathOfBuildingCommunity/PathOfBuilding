5ae94e3e09206dcc9e59848d2c1797e393e524d7
PJacek <PJacek@users.noreply.github.com>
2026-03-05 00:37:32 +0100

    Tinctures

--- src/Export/Uniques/tincture.lua
+++ src/Export/Uniques/tincture.lua
@@ -9,7 +9,6 @@ Requires Level 18
 Implicits: 1
 TinctureRageOnHitImplicit1
 TinctureToxicityOnHitUnique__1
-Inflicts Mana Burn on you when you Hit an Enemy with a Melee Weapon
 TinctureRarityPerToxicityUnique__1
 TinctureCullingStrikeUnique__1
 ]],[[
@@ -17,9 +16,8 @@ Grasping Nightshade
 Sporebloom Tincture
 League: Settlers of Kalguur
 Requires Level 52
-Implicits: 2
+Implicits: 1
 TinctureChanceToBlindImplicit1
-(25-35)% increased Effect of Blind from Melee Weapons
 TinctureApplyWitherStacksOnHitUnique__1
 TinctureGraspingVineOnWeaponHitUnique__1
 ]],[[
@@ -27,11 +25,10 @@ Mightblood Ire
 Ironwood Tincture
 League: Settlers of Kalguur
 Requires Level 18
-Implicits: 2
+Implicits: 1
 TinctureStunThresholdImplicit1
-(15-25)% increased Stun Duration with Melee Weapons
 TinctureMeleeSplashOnWeaponHitUnique__1
-(25-15)% reduced Mana Burn rate
+TinctureToxicityRateUnique__1
 ]],[[
 Sap of the Seasons
 Prismatic Tincture
@@ -39,7 +36,7 @@ League: Settlers of Kalguur
 Requires Level 18
 Implicits: 1
 TinctureElementalDamageImplicit1
-Melee Weapon Damage Penetrates 1% Elemental Resistance per Mana Burn, up to a maximum of 200%
+TincturePenetrationPerToxicityUnique__1
 TinctureCooldownRecoveryUnique__1
 (35--35)% increased Mana Burn rate
 ]],[[
@@ -47,11 +44,10 @@ Wildfire Phloem
 Ashbark Tincture
 League: Settlers of Kalguur
 Requires Level 32
-Implicits: 2
+Implicits: 1
 TinctureChanceToIgniteImplicit1
-(60-90)% increased Damage with Ignite from Melee Weapons
 TinctureFireDamageTakenPerToxicityUnique__1
 TinctureRefreshIgniteDurationUnique__1
-Cover Full Life Enemies in Ash for (4-10) seconds on Melee Weapon hit
+TinctureCoverInAshOnFullLifeUnique__1
 ]]
 }
