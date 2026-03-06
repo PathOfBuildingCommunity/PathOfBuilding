bf4d2deac6a46a250386ea22f94c7824026ca33d
LocalIdentity <localidentity2@gmail.com>
2026-03-07 06:51:25 +1100

    Fix StatDescription extract

--- src/Export/Classes/GGPKData.lua
+++ src/Export/Classes/GGPKData.lua
@@ -88,7 +88,7 @@ function GGPKClass:ExtractFiles(reExport)
 
 		-- non-regex chunk: dat files + txtList + itList
 		for i = 1, #txtList do
-			datFiles[#datFiles + 1] = itList[i]
+			datFiles[#datFiles + 1] = txtList[i]
 		end
 		for i = 1, #itList do
 			datFiles[#datFiles + 1] = itList[i]
