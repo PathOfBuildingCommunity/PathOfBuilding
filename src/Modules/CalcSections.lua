cc3c3fad3275bcffe7463431cb3ebd8cf5d13f35
majochem <77203255+majochem@users.noreply.github.com>
2026-03-10 15:45:47 +0100

    Fix doubled breakdown section

--- src/Modules/CalcSections.lua
+++ src/Modules/CalcSections.lua
@@ -925,7 +925,6 @@ return {
 		{ label = "inflict # additional poisons (Main Hand)", flag = "weapon1Attack", modName = { "AdditionalPoisonStacks" }, modType = "BASE", cfg = "weapon1" },
 		{ label = "inflict # additional poisons (Off Hand)", flag = "weapon2Attack", modName = { "AdditionalPoisonStacks" }, modType = "BASE", cfg = "weapon2" },
 		{ label = "Poison Stack Limits", modName = { "PoisonStackLimit", "CannotMultiplePoison", "Condition:NonPoisonedOnly" }, cfg = "skill" },
-		{ label = "Poison Stack Limits", modName = { "Condition:NonPoisonedOnly" } },
 	}, },
 	{ label = "Total Increased", { format = "{0:mod:1}%", { modName = { "Damage", "ChaosDamage" }, modType = "INC", cfg = "poison" }, }, },
 	{ label = "Total More", { format = "{0:mod:1}%", { modName = { "Damage", "ChaosDamage" }, modType = "MORE", cfg = "poison" }, }, },
