759247dd176fb53d90485d9480fd974e3214bd12
Mickael Cagnion <cagnionm@gmail.com>
2026-03-19 14:49:46 +0100

    feat: add radius jewel finder entry point

--- src/Classes/TreeTab.lua
+++ src/Classes/TreeTab.lua
@@ -188,11 +188,16 @@ local TreeTabClass = newClass("TreeTab", "ControlHost", function(self, build)
 		self:FindTimelessJewel()
 	end)
 
+	-- Find Radius Jewel Button
+	self.controls.findRadiusJewel = new("ButtonControl", { "LEFT", self.controls.findTimelessJewel, "RIGHT" }, { 8, 0, 160, 20 }, "Find Radius Jewel", function()
+		self:FindRadiusJewel()
+	end)
+
 	--Default index for Tattoos
 	self.defaultTattoo = { }
 
 	-- Show Node Power Checkbox
-	self.controls.treeHeatMap = new("CheckBoxControl", { "LEFT", self.controls.findTimelessJewel, "RIGHT" }, { 130, 0, 20 }, "Show Node Power:", function(state)
+	self.controls.treeHeatMap = new("CheckBoxControl", { "LEFT", self.controls.findRadiusJewel, "RIGHT" }, { 130, 0, 20 }, "Show Node Power:", function(state)
 		self.viewer.showHeatMap = state
 		self.controls.treeHeatMapStatSelect.shown = state
 
@@ -397,6 +402,7 @@ function TreeTabClass:Draw(viewPort, inputEvents)
 	
 	local widthSecondLineControls = self.controls.treeSearch.width + 8
 									+ self.controls.findTimelessJewel.width + self.controls.findTimelessJewel.x
+									+ self.controls.findRadiusJewel.width + self.controls.findRadiusJewel.x
 									+ self.controls.treeHeatMap.width + 130
 									+ self.controls.nodePowerMaxDepthSelect.width + self.controls.nodePowerMaxDepthSelect.x
 									+ (self.isCustomMaxDepth and (self.controls.nodePowerMaxDepthCustom.width + self.controls.nodePowerMaxDepthCustom.x) or 0)
@@ -415,7 +421,7 @@ function TreeTabClass:Draw(viewPort, inputEvents)
 
 	-- Check second line
 	if viewPort.width >= widthSecondLineControls + rightMargin then
-		self.controls.treeHeatMap:SetAnchor("LEFT", self.controls.findTimelessJewel, "RIGHT", 130, 0)
+		self.controls.treeHeatMap:SetAnchor("LEFT", self.controls.findRadiusJewel, "RIGHT", 130, 0)
 	else
 		linesHeight = linesHeight * 2
 		self.controls.treeHeatMap:SetAnchor("TOPLEFT", self.controls.treeSearch, "BOTTOMLEFT", 124, 4)
@@ -2705,4 +2711,8 @@ function TreeTabClass:FindTimelessJewel()
 
 	local panelHeight = 565
 	main:OpenPopup(panelWidth, panelHeight, "Find a Timeless Jewel", controls)
+end
+
+function TreeTabClass:FindRadiusJewel()
+	new("RadiusJewelFinder", self):Open()
 end
