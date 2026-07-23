48825bc2025564931963beb7dacca519bcee3505
majochem <77203255+majochem@users.noreply.github.com>
2026-07-23 19:53:39 +0200

    Add support for "Unhallowed Rite" Occultist Ascendancy node (#9968)
    * Add support for "Spirit Infusion" Occultist Ascendancy node
    
    * Add more flexible mod pattern
    
    * Slightly rename and reposition config option
    
    It is now right below "Spirit Charges" which seemed fitting

--- src/Modules/ConfigOptions.lua
+++ src/Modules/ConfigOptions.lua
@@ -954,6 +954,9 @@ Huge sets the radius to 11.
 	{ var = "overrideSpiritCharges", type = "countAllowZero", label = "# of Spirit Charges:", ifMult = "SpiritCharge", apply = function(val, modList, enemyModList)
 		modList:NewMod("SpiritCharges", "OVERRIDE", val, "Config", { type = "Condition", var = "Combat" })
 	end },
+	{ var = "overrideSpiritInfusion", type = "countAllowZero", label = "# of Spirit Infusions:", ifFlag = "Condition:CanGainSpiritInfusion", tooltip = "Each stack of Spirit Infusion grants:\n30% faster start of Energy Shield Recharge, \nChannelling Spells deal 10% more Damage and \nChannelling Spells have 20% more Cost", apply = function(val, modList, enemyModList)
+		modList:NewMod("SpiritInfusion", "OVERRIDE", val, "Config", { type = "Condition", var = "Combat" } )
+	end },
 	{ var = "minionsUsePowerCharges", type = "check", label = "Do your Minions use Power Charges?", ifFlag = "haveMinion", apply = function(val, modList, enemyModList)
 		modList:NewMod("MinionModifier", "LIST", { mod = modLib.createMod("UsePowerCharges", "FLAG", true, "Config", { type = "Condition", var = "Combat" }) }, "Config")
 	end },
