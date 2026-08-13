2a6352964b14ba573e9ab4b40ed5ad8de8cf716c
vaisest <4550061+vaisest@users.noreply.github.com>
2026-08-13 21:07:54 +0300

    Show fancy aoe image

--- src/Modules/BuildDisplayStats.lua
+++ src/Modules/BuildDisplayStats.lua
@@ -112,7 +112,7 @@ local displayStats = {
 	{ stat = "SealCooldown", label = "Seal Gain Frequency", fmt = ".2fs", lowerIsBetter = true },
 	{ stat = "SealMax", label = "Max Number of Seals", fmt = "d" },
 	{ stat = "TimeMaxSeals", label = "Time to Gain Max Seals", fmt = ".2fs", lowerIsBetter = true },
-	{ stat = "AreaOfEffectRadiusMetres", label = "AoE Radius", fmt = ".1fm", breakdown = "AreaOfEffectMod" },
+	{ stat = "AreaOfEffectRadiusMetres", label = "AoE Radius", fmt = ".1fm", breakdown = "AreaOfEffectRadius", modNames = { "AreaOfEffect" } },
 	{ stat = "BrandAttachmentRangeMetre", label = "Attachment Range", fmt = ".1fm", flag = "brand" },
 	{ stat = "BrandTicks", label = "Activations per Brand", fmt = "d", flag = "brand" },
 	{ stat = "ManaCost", label = "Mana Cost", fmt = "d", color = colorCodes.MANA, pool = "ManaUnreserved", compPercent = true, lowerIsBetter = true, condFunc = function(v,o) return o.ManaHasCost end },
