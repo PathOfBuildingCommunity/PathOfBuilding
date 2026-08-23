7685abcc028e5f0b4fffef3e09d5d0e038162174
LocalIdentity <localidentity2@gmail.com>
2026-08-23 20:17:47 +1000

    Spec update

--- src/Export/spec.lua
+++ src/Export/spec.lua
@@ -12043,10 +12043,10 @@ return {
 		},
 		[8]={
 			list=false,
-			name="",
+			name="RandomizeVisualRotationOnSpawn",
 			refTo="",
 			type="Bool",
-			width=50
+			width=200
 		},
 		[9]={
 			list=false,
@@ -12071,31 +12071,31 @@ return {
 		},
 		[12]={
 			list=false,
-			name="",
-			refTo="",
+			name="MiscAnimated",
+			refTo="MiscAnimated",
 			type="Key",
-			width=40
+			width=220
 		},
 		[13]={
 			list=false,
 			name="",
 			refTo="",
 			type="Int",
-			width=50
+			width=150
 		},
 		[14]={
 			list=false,
-			name="",
+			name="ApplyProjectileSpeedToAuxiliarySpeed",
 			refTo="",
 			type="Bool",
-			width=50
+			width=200
 		},
 		[15]={
 			list=false,
-			name="",
+			name="AuxiliarySpeedCannotBeBelowBase",
 			refTo="",
 			type="Bool",
-			width=50
+			width=190
 		},
 		[16]={
 			list=true,
